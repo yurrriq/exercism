1c874646526ae68582be2201813e76b24558915e
Eric Bailey <eric@ericb.me>
2015-01-03 16:50:06 -0600

    http://exercism.io/submissions/dfd57b22837d41029c721cb017fd1d29

--- anagram/anagram.clj
+++ anagram/anagram.clj
@@ -2,9 +2,7 @@
   (:require [clojure.string :refer [lower-case]]))
 
 (defn anagrams-for [s coll]
-  (letfn [(letters [s] (reduce #(conj %1 (int %2)) [] (lower-case s)))]
-    (filter #(and (not= (lower-case s)
-                        (lower-case %))
-                  (= (sort (letters s))
-                     (sort (letters %))))
-            coll)))
+  (filter #(let [comparison (map lower-case [s %])]
+             (when (apply not= comparison)
+               (apply = (map sort comparison))))
+          coll))
