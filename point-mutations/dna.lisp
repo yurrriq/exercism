07cc4e3f7ff6313ff9f10c6e3539a24987393c1f
Eric Bailey <eric@ericb.me>
2015-01-10 17:44:28 -0600

    http://exercism.io/submissions/482a38bcfb854c57b13723aa63855f7a

--- point-mutations/dna.lisp
+++ point-mutations/dna.lisp
@@ -3,6 +3,12 @@
   (:export #:hamming-distance))
 (in-package #:dna)
 
+;; This is a rather direct port of my Clojure solution. 
 (defun hamming-distance (dna1 dna2)
   "Determine number of mutations between DNA strands by computing the Hamming Distance."
-  )
+  (when (apply '= (mapcar 'length (list dna1 dna2)))
+    (reduce #'(lambda (n different?) (if different? (incf n) n))
+            (mapcar #'(lambda (a b) (not (char= a b)))
+                    (coerce dna1 'list)
+                    (coerce dna2 'list))
+            :initial-value 0)))
