6d99cc5bd0b3290fa9593fa20b33462d98eb8261
Eric Bailey <eric@ericb.me>
2015-04-12 01:09:06 -0500

    http://exercism.io/submissions/3e624cb3d316473abed2f68eb2e43455 fixes #1

--- rna-transcription/rna_transcription.erl
+++ rna-transcription/rna_transcription.erl
@@ -3,10 +3,10 @@
 
 transcribe(Nucleotide) ->
     case Nucleotide of
-	71 -> 67;   
-	67 -> 71;
-	84 -> 65;
-	65 -> 85
+	$G -> $C;
+	$C -> $G;
+	$T -> $A;
+	$A -> $U
     end.
 
 to_rna(Strand) -> lists:map(fun transcribe/1, Strand).
