8c3d8f379af8348f7a34443722d1b4eb4e026580
Eric Bailey <eric@ericb.me>
2015-05-10 01:18:53 -0500

    removed trailing whitespace

--- leap/leap.erl
+++ leap/leap.erl
@@ -3,5 +3,5 @@
 
 leap_year(Year) when Year rem 400 =:= 0 -> true;
 leap_year(Year) when Year rem 100 =:= 0 -> false;
-leap_year(Year) when Year rem   4 =:= 0 -> true; 
+leap_year(Year) when Year rem   4 =:= 0 -> true;
 leap_year(_)                            -> false.
