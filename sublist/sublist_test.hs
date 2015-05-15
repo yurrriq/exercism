84408bb561d63b168215ca1c853678218ce4e39d
Eric Bailey <eric@ericb.me>
2015-05-15 14:46:25 -0500

    removed redundant `do`s and trailing whitespace

--- sublist/sublist_test.hs
+++ sublist/sublist_test.hs
@@ -16,41 +16,41 @@ main = exitProperly $ runTestTT $ TestList
 
 sublistTests :: [Test]
 sublistTests =
-  [ testCase "empty equals empty" $ do
+  [ testCase "empty equals empty" $
     Equal @=? sublist "" ""
-  , testCase "empty is a sublist of anything" $ do
+  , testCase "empty is a sublist of anything" $
     Sublist @=? sublist "" "asdf"
-  , testCase "anything is a superlist of empty" $ do
+  , testCase "anything is a superlist of empty" $
     Superlist @=? sublist "asdf" ""
-  , testCase  "1 is not 2" $ do
+  , testCase  "1 is not 2" $
     Unequal @=? sublist "1" "2"
   , testCase "compare larger equal lists" $ do
     let xs = replicate 1000 'x'
     Equal @=? sublist xs xs
-  , testCase "sublist at start" $ do
+  , testCase "sublist at start" $
     Sublist @=? sublist "123" "12345"
-  , testCase "sublist in middle" $ do
+  , testCase "sublist in middle" $
     Sublist @=? sublist "432" "54321"
-  , testCase "sublist at end" $ do
+  , testCase "sublist at end" $
     Sublist @=? sublist "345" "12345"
-  , testCase "partially matching sublist at start" $ do
+  , testCase "partially matching sublist at start" $
     Sublist @=? sublist "112" "1112"
-  , testCase "sublist early in huge list" $ do
-    Sublist @=? sublist [3, 4, 5] [1 .. 1000000 :: Int] 
-  , testCase "huge sublist not in huge list" $ do
+  , testCase "sublist early in huge list" $
+    Sublist @=? sublist [3, 4, 5] [1 .. 1000000 :: Int]
+  , testCase "huge sublist not in huge list" $
     Unequal @=? sublist [10 .. 1000001] [1 .. 1000000 :: Int]
-  , testCase "superlist at start" $ do
+  , testCase "superlist at start" $
     Superlist @=? sublist "12345" "123"
-  , testCase "superlist in middle" $ do
+  , testCase "superlist in middle" $
     Superlist @=? sublist "54321" "432"
-  , testCase "superlist at end" $ do
+  , testCase "superlist at end" $
     Superlist @=? sublist "12345" "345"
-  , testCase "partially matching superlist at start" $ do
+  , testCase "partially matching superlist at start" $
     Superlist @=? sublist "1112" "112"
-  , testCase "superlist early in huge list" $ do
+  , testCase "superlist early in huge list" $
     Superlist @=? sublist [1 .. 1000000] [3, 4, 5 :: Int]
-  , testCase "recurring values sublist" $ do
+  , testCase "recurring values sublist" $
     Sublist @=? sublist "12123" "1231212321"
-  , testCase "recurring values unequal" $ do
+  , testCase "recurring values unequal" $
     Unequal @=? sublist "12123" "1231232321"
   ]
