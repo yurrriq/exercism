624e811c2408c50bed84b0555c0fecd824aecd9b
Eric Bailey <eric@ericb.me>
2015-05-27 04:33:01 -0500

    :golf: http://exercism.io/submissions/f68466fbe76d402692096566569c403d

--- rna-transcription/rna_transcription.clj
+++ rna-transcription/rna_transcription.clj
@@ -1,17 +1,5 @@
 (ns rna-transcription)
 
-(defn transcribe [nucleotide]
-  (case nucleotide
-    \G \C
-    \C \G
-    \T \A
-    \A \U
-    (throw (AssertionError. "Invalid DNA strand."))))
-
 (defn to-rna [dna]
-  (loop [to-convert dna rna (transient [])]
-    (let [nucleotide (transcribe (first to-convert))
-          rna        (conj! rna nucleotide)]
-      (if-let [remaining (not-empty (rest to-convert))]
-        (recur remaining rna)
-        (apply str (persistent! rna))))))
+  (let [invalid #(throw (AssertionError. "Invalid DNA strand."))]
+    (apply str (map #(case % \G \C, \C \G, \T \A, \A \U, (invalid)) dna))))
