1f477d97443cdd5a0a653b34a397f0191cb62a05
Eric Bailey <eric@ericb.me>
2015-05-30 21:22:30 -0500

    http://exercism.io/submissions/862eeec039ff4921924d150bbd3db992

--- nucleotide-count/DNA.hs
+++ nucleotide-count/DNA.hs
@@ -1,21 +1,33 @@
-module DNA (count, nucleotideCounts) where
+{-|
+Module      : DNA
+Copyright   : (c) Eric Bailey, 2015
+License     : MIT
 
-import Control.Monad (ap)
-import Data.List (find)
-import Data.Map (Map, fromList)
+Maintainer  : Eric Bailey
+Stability   : experimental
+Portability : portable
 
+Calculating nucleotide frequencies in DNA strings.
+-}
+module DNA where
+
+import Data.Map.Strict (Map, fromListWith)
+
+-- | Given a nucleotide and a DNA string, returns the frequency of
+-- the given nucleotide in the given string, throwing an exception for any
+-- invalid nucleotide.
 count :: Char -> String -> Int
-count nucleotide string =
-  maybe (length $ filter (== nucleotide) string)
-  throwInvalid $ find isInvalid (nucleotide:string)
+count c | valid c == c = length . filter ((c ==) . valid)
 
+-- | Given a DNA string, returns a map from a nucleotide to its frequency
+-- in the given string, throwing an exception for any invalid nucleotide.
 nucleotideCounts :: String -> Map Char Int
-nucleotideCounts string =
-  maybe (fromList . ap zip (map (flip count string)) $ "ACGT")
-  throwInvalid $ find isInvalid string
+nucleotideCounts = fromListWith (+) .
+                   -- (zip "ACGT" (repeat 0) ++) .
+                   ([('A', 0), ('C', 0), ('G', 0), ('T', 0)] ++) .
+                   map (flip (,) 1 . valid)
 
-throwInvalid :: Char -> e
-throwInvalid = (error . ("invalid nucleotide " ++) . show)
-
-isInvalid :: Char -> Bool
-isInvalid = not . (`elem` "ACGT")
+-- | Given a nucleotide, returns it if valid, otherwise throws an exception.
+valid :: Char -> Char
+valid c | not $ c `elem` "ACGT" = error $ "invalid nucleotide " ++ show c
+        | otherwise   = c
