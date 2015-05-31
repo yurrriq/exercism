615764b8b4fc1f3a97f3924d5a7abad03729ab90
Eric Bailey <eric@ericb.me>
2015-05-30 21:57:43 -0500

    http://exercism.io/submissions/34016336bad14823a0d37a92251d45f0 type aliases and helper functions

--- nucleotide-count/DNA.hs
+++ nucleotide-count/DNA.hs
@@ -13,21 +13,38 @@ module DNA where
 
 import Data.Map.Strict (Map, fromListWith)
 
+-- | A nucleotide is a character.
+type Nucleotide = Char
+
+-- | A DNA string is a list of nucleotides.
+type DNA = [Nucleotide]
+
+-- | The frequency of a nucleotide in a DNA string is an integer.
+type Frequency  = Int
+
 -- | Given a nucleotide and a DNA string, returns the frequency of
 -- the given nucleotide in the given string, throwing an exception for any
 -- invalid nucleotide.
-count :: Char -> String -> Int
+count :: Nucleotide -> DNA -> Frequency
 count c | valid c == c = length . filter ((c ==) . valid)
 
 -- | Given a DNA string, returns a map from a nucleotide to its frequency
 -- in the given string, throwing an exception for any invalid nucleotide.
-nucleotideCounts :: String -> Map Char Int
+nucleotideCounts :: DNA -> Map Nucleotide Frequency
 nucleotideCounts = fromListWith (+) .
                    -- (zip "ACGT" (repeat 0) ++) .
                    ([('A', 0), ('C', 0), ('G', 0), ('T', 0)] ++) .
                    map (flip (,) 1 . valid)
 
 -- | Given a nucleotide, returns it if valid, otherwise throws an exception.
-valid :: Char -> Char
-valid c | c `elem` "ACGT" = c
-        | otherwise       = error $ "invalid nucleotide " ++ show c
+valid :: Nucleotide -> Nucleotide
+valid c | isValid c = c
+        | otherwise = throwInvalid c
+
+-- | Given a nucleotide, returns @True@ if valid, otherwise @False@.
+isValid :: Nucleotide -> Bool
+isValid = (`elem` "ACGT")
+
+-- | Given an invalid nucleotide, throwns an exception.
+throwInvalid :: Nucleotide -> e
+throwInvalid = error . ("invalid nucleotide " ++) . show
