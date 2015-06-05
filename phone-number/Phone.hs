04c5a1b69a37235a734083944ba00f6a1a56ec1d
Eric Bailey <eric@ericb.me>
2015-06-04 22:01:33 -0500

    http://exercism.io/submissions/3e2bdf2cc8584dda94a04dab6e150515

---
+++ phone-number/Phone.hs
@@ -0,0 +1,20 @@
+module Phone where
+
+import Data.Char (isDigit)
+
+areaCode :: String -> String
+areaCode = take 3 . number
+
+number :: String -> String
+number x
+  | numDigits == 10                    = digits
+  | (numDigits == 11) && (head x == '1') = tail digits
+  | otherwise                         = "0000000000"
+  where digits    = filter isDigit x
+        numDigits = length digits
+
+prettyPrint :: String -> String
+prettyPrint = (\ (area, rest) ->
+                 "(" ++ area ++ ") " ++
+                 ((\(prefix, line) -> prefix ++ "-" ++ line) . splitAt 3) rest) .
+              splitAt 3 . number
