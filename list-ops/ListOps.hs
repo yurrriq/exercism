cfcfc0f462ab13a02ed00788614c85332bf660bd
Eric Bailey <eric@ericb.me>
2015-06-06 11:00:37 -0500

    http://exercism.io/submissions/ef54be5a3deb45ce90b962d44be62cb9

--- list-ops/ListOps.hs
+++ list-ops/ListOps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module ListOps (
   (++),
   concat,
@@ -11,26 +12,37 @@ module ListOps (
 
 import Prelude hiding ((++), concat, filter, foldr, length, map, reverse)
 
-foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' = undefined
-
-foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = undefined
-
-length :: [a] -> Int
-length = undefined
-
-reverse :: [a] -> [a]
-reverse = undefined
-
-map :: (a -> b) -> [a] -> [b]
-map = undefined
-
-filter :: (a -> Bool) -> [a] -> [a]
-filter = undefined
+if' :: Bool -> a -> a -> a
+if' True  x _ = x
+if' False _ y = y
 
 (++) :: [a] -> [a] -> [a]
-xs ++ ys = undefined
+xs ++ ys = foldr (:) ys xs
 
 concat :: [[a]] -> [a]
-concat = undefined
+concat = foldr (++) []
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter p = foldr f []
+  where f x = if' (p x) =<< (x :)
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' f = lgo
+  where lgo z []      = z
+        lgo !z (x:xs) = lgo (f z x) xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f y = go
+  where go [] = y
+        go (x:xs) = f x $ go xs
+
+length :: [a] -> Int
+length = foldr (const (1 +)) 0
+
+map :: (a -> b) -> [a] -> [b]
+map f = foldr g []
+  where g =  (:) . f
+
+reverse :: [a] -> [a]
+reverse xs = foldr f id xs []
+  where f = flip (.) . (:)
