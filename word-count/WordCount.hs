da5e700c023617c683bd0bdf69321261f84c8d4e
Eric Bailey <eric@ericb.me>
2015-06-08 00:35:18 -0500

    http://exercism.io/submissions/ebe43f0ea1b44fe98e8d5bee9ebe0779 tweaks - pulled out some helper functions - use fromListWith instead of foldr

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -1,14 +1,22 @@
-module WordCount where
+module WordCount (wordCount) where
 
 import Data.Char (isAlphaNum, toLower)
+import Data.List.Split (wordsBy)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
+-- | Given a string, returns a string with each character converted
+-- to lowercase. Also found in "Distribution.Simple.Utils".
+lowercase :: String -> String
+lowercase = map toLower
+
+-- | Given a string, returns a list of strings by splitting on (and dropping)
+-- non-alphanumeric characters.
+words' :: String -> [String]
+words' = wordsBy $ not . isAlphaNum
+
+-- | Given a string, returns a Map from word to frequency for each word.
 wordCount :: String -> Map String Int
-wordCount = foldr (f . filter isAlphaNum) Map.empty . words'
-  where f []     = id
-        f x      = Map.insertWith (+) (map toLower x) 1
-        words' s = case dropWhile p s of
-                        "" -> []
-                        s' -> w : words' s'' where (w, s'') = break p s'
-          where p = not . isAlphaNum
+wordCount = Map.fromListWith (+) . toPairs . words'
+  where toPairs :: [String] -> [(String, Int)]
+        toPairs = map $ flip (,) 1 . lowercase
