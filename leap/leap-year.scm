b7d330a0b5f176447ea1dab3b122cc864b43cccd
Eric Bailey <eric@ericb.me>
2015-06-14 00:38:52 -0500

    http://exercism.io/submissions/5c087ec897fd40c58ec30541afa37454

--- leap/leap-year.scm
+++ leap/leap-year.scm
@@ -1,2 +1,7 @@
 (define-module (leap-year)
   #:export (leap-year?))
+
+(define (leap-year? year)
+  (and (zero? (modulo year 4))
+       (or (positive? (modulo year 100))
+           (zero? (modulo year 400)))))
