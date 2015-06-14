f8d92174a8cf79fdd80a8da7360d4b6b6d668513
Eric Bailey <eric@ericb.me>
2015-06-14 02:04:40 -0500

    http://exercism.io/submissions/b3e1cd5de42d4f1ebd5f81ada129c167

--- grains/grains.scm
+++ grains/grains.scm
@@ -3,7 +3,8 @@
   #:autoload (srfi srfi-1) (iota))
 
 (define (square n)
-  )
+  (expt 2 (1- n)))
 
+;; Traverses the list only once.
 (define (total)
-  )
+  (reduce (lambda (n acc) (+ acc (square n))) 0 (iota 64 1)))
