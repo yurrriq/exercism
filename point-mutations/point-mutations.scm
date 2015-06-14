f2a6208b834e05a1b6d7a88dfa7c96de3aad9f64
Eric Bailey <eric@ericb.me>
2015-06-14 00:30:18 -0500

    http://exercism.io/submissions/e05596ec488944578d301c0b0fc47352

--- point-mutations/point-mutations.scm
+++ point-mutations/point-mutations.scm
@@ -1,2 +1,7 @@
 (define-module (point-mutations)
   #:export (hamming-distance))
+
+(define (hamming-distance x y)
+  (if (eqv? (string-length x) (string-length y))
+      (length (filter not (map char=? (string->list x) (string->list y))))
+      (error "Can't calculate distance for strings of unequal length.")))
