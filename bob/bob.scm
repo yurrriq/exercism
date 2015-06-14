1a391ee2f88199df65c040819dfddae295ab80dd
Eric Bailey <eric@ericb.me>
2015-06-14 16:06:57 -0500

    http://exercism.io/submissions/d6b7511430764d9c9dbbe7a5d1450051

--- bob/bob.scm
+++ bob/bob.scm
@@ -1,3 +1,10 @@
 (define-module (bob)
   #:export (response-for))
 
+(define (response-for prompt)
+  (cond
+   ((string-every char-whitespace? prompt)         "Fine. Be that way!")
+   ((and (string-any char-alphabetic? prompt)
+         (string=? prompt (string-upcase prompt))) "Whoa, chill out!")
+   ((string-suffix? "?" prompt)                    "Sure.")
+   (else                                           "Whatever.")))
