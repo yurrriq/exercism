a306e34eafe4c642853de3c7e0abf40be76bfb28
Eric Bailey <eric@ericb.me>
2015-06-14 23:44:44 -0500

    http://exercism.io/submissions/b123f33a3beb4f9e9438c25280d8042c

--- rna-transcription/dna.scm
+++ rna-transcription/dna.scm
@@ -1,2 +1,22 @@
 (define-module (dna)
-  #:export (to-rna))
+  #:export (to-rna)
+  #:use-module (ice-9 hash-table))
+
+(define hash-dna->rna
+  (alist->hash-table
+   '((#\G . #\C)
+     (#\C . #\G)
+     (#\T . #\A)
+     (#\A . #\U))))
+
+(define (complement nucleotide)
+  "Given a DNA nucleotide, returns its RNA complement,
+  or shows an error message when an invalid nucleotide is encountered."
+  (define (invalid nucleotide)
+    (error (format #f "Invalid nucleotide: ~a" nucleotide)))
+  (or (hash-ref hash-dna->rna nucleotide)
+      (invalid nucleotide)))
+
+(define (to-rna dna)
+  "Given a DNA strand, returns its transcribed RNA strand."
+  (list->string (map complement (string->list dna))))
