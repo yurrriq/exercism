00d0206cc1a6dac31acbd5ab7a6d814bf9b7451c
Eric Bailey <eric@ericb.me>
2015-06-14 22:47:24 -0500

    :lipstick:

--- rna-transcription/rna-transcription-test.scm
+++ rna-transcription/rna-transcription-test.scm
@@ -10,24 +10,24 @@
 (test-begin "rna-transcription")
 
 (test-equal "transcribes-cytosine-to-guanine"
-          "G"
-          (to-rna "C"))
+            "G"
+            (to-rna "C"))
 
 (test-equal "transcribes-guanine-to-cytosine"
-          "C"
-          (to-rna "G"))
+            "C"
+            (to-rna "G"))
 
 (test-equal "transcribes-adenine-to-uracil"
-          "U"
-          (to-rna "A"))
+            "U"
+            (to-rna "A"))
 
 (test-equal "transcribes-thymine-to-adenine"
-          "A"
-          (to-rna "T"))
+            "A"
+            (to-rna "T"))
 
 (test-equal "transcribes-all-nucleotides"
-          "UGCACCAGAAUU"
-          (to-rna "ACGTGGTCTTAA"))
+            "UGCACCAGAAUU"
+            (to-rna "ACGTGGTCTTAA"))
 
 
 (test-error "it-validates-dna-strands"
