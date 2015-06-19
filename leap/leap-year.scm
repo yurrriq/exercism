73693cd5a01b75f2919f5fd00d477d11e8fafacf
Eric Bailey <eric@ericb.me>
2015-06-19 02:47:12 -0500

    http://exercism.io/submissions/7ea008777e9748daae07b84736fffeac logic fix

--- leap/leap-year.scm
+++ leap/leap-year.scm
@@ -3,5 +3,5 @@
 
 (define (leap-year? year)
   (and (zero? (modulo year 4))
-       (or (positive? (modulo year 100))
-           (zero? (modulo year 400)))))
+       (or (zero? (modulo year 400))
+           (positive? (modulo year 100)))))
