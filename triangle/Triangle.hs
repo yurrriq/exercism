5972319aa47b4a15f5123b43838bbf453004fa81
Eric Bailey <eric@ericb.me>
2015-06-20 14:21:59 -0500

    http://exercism.io/submissions/ef6bfc0cffa043839aa34c7e8b1b7f25 first pass at haskell/triangle

--- triangle/Triangle.hs
+++ triangle/Triangle.hs
@@ -1,10 +1,42 @@
-module Triangle where
+{-|
+Module      : Triangle
+Copyright   : (c) Eric Bailey, 2015
+License     : MIT
+
+Maintainer  : Eric Bailey
+Stability   : experimental
+Portability : portable
+
+Determining if a triangle is illogical, equilateral, isosceles, or scalene.
+-}
+module Triangle (TriangleType(..), triangleType) where
 
 import Control.Monad (ap)
 
-data TriangleType = Illogical | Equilateral | Isosceles | Scalene
+-- | Qualifying triangles.
+data TriangleType =
+  -- | Fails the
+  -- <https://en.wikipedia.org/wiki/Triangle_inequality triangle inequality>.
+  --
+  -- <<illogical.png>>
+  Illogical
+  -- | All sides are equal.
+  --
+  -- <<equilateral.png>>
+  | Equilateral
+  -- | Two sides are equal.
+  --
+  -- <<isosceles.png>>
+  | Isosceles
+  -- | Passes the
+  -- <https://en.wikipedia.org/wiki/Triangle_inequality triangle inequality>.
+  --
+  -- <<scalene.png>>
+  | Scalene
   deriving (Eq, Show)
 
+-- | Given thre sides, returns a 'TriangleType' describing the quality of the
+-- given triangle.
 triangleType :: Int -> Int -> Int -> TriangleType
 triangleType x y z
   | isIllogical   = Illogical
@@ -17,7 +49,10 @@ triangleType x y z
         picked        = picks sides
         sides         = [x,y,z]
 
--- http://stackoverflow.com/a/12872133
+-- | Given a list, returns a list of pairs of each element in the list,
+-- and a list of every other element.
+-- See also:
+-- <http://stackoverflow.com/a/12872133 Answer on Stack Overflow>
 picks :: [a] -> [(a, [a])]
 picks [] = []
 picks (x : xs) = (x, xs) : [(y, x : ys) | (y, ys) <- picks xs]
