1c36a6f125ecf69ec4675d9134499914083821e1
Eric Bailey <eric@ericb.me>
2015-06-22 06:46:45 -0500

    grains tricks - Lisp: http://exercism.io/submissions/f49679e525f74f84863e19cfa733ba74 - Scheme: http://exercism.io/submissions/89dc2b768ca2471aa6632ec18cbc9bd5 - Haskell: http://exercism.io/submissions/70da25ec530646eeaa2fd4abca31cae0

--- grains/grains.scm
+++ grains/grains.scm
@@ -1,3 +1,5 @@
+;; -*- compile-command: "guile grains-test.scm" -*-
+
 (define-module (grains)
   #:export (square total)
   #:autoload (srfi srfi-1) (iota))
@@ -5,6 +7,5 @@
 (define (square n)
   (expt 2 (1- n)))
 
-;; Traverses the list only once.
 (define (total)
-  (reduce (lambda (n acc) (+ acc (square n))) 0 (iota 64 1)))
+  (1- (square 65)))
