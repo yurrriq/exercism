d87ccd1a49004d55e6c20f65806f966bf7132b52
Eric Bailey <eric@ericb.me>
2015-06-22 07:59:12 -0500

    http://exercism.io/submissions/118a37e284144bfe8a648bb6a0005ae9

--- gigasecond/gigasecond.lisp
+++ gigasecond/gigasecond.lisp
@@ -1,7 +1,25 @@
+;; -*- compile-command: "sbcl --non-interactive --load gigasecond-test.lisp" -*-
+
+(ql:quickload "cl-arrows")
+
 (in-package #:cl-user)
 (defpackage #:gigasecond
-  (:use #:cl)
+  (:use #:cl #:cl-arrows)
   (:export #:from))
 (in-package #:gigasecond)
 
-(defun from (year month day))
+(defconstant gigasecond 1000000000 "The number one billion")
+
+(defun take* (sequence end)
+  "Sort of like a strict version of #'clojure.core/take."
+  (subseq sequence 0 end))
+
+(defun from (year month date hour minute second)
+  "Given a year, month, date, hour, minute a second, returns a list of the
+  same arguments a gigasecond later."
+  (-> (encode-universal-time second minute hour date month year 0)
+      (+ gigasecond)
+      (decode-universal-time 0)
+      (multiple-value-list)
+      (take* 6)
+      (reverse)))
