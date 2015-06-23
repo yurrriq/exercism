46198a2f279c0a702ab7215125edd194b8d1bfad
Eric Bailey <eric@ericb.me>
2015-06-23 01:44:43 -0500

    http://exercism.io/submissions/62c2bc0ebe064df09c42a7397a3b28cd

--- bob/bob.lisp
+++ bob/bob.lisp
@@ -1,7 +1,23 @@
+;; -*- compile-command: "sbcl --non-interactive --load bob-test.lisp" -*-
+
 (in-package #:cl-user)
 (defpackage #:bob
   (:use #:cl)
   (:export #:response-for))
 (in-package #:bob)
 
-(defun response-for (input))
+(defun question-p (prompt)
+  (char-equal (char prompt (1- (length prompt))) #\?))
+
+(defun silent-p (prompt)
+  (zerop (length (string-trim #(#\Space #\Tab) prompt))))
+
+(defun yelled-p (prompt)
+  (and (some #'upper-case-p prompt)
+       (string= prompt (string-upcase prompt))))
+
+(defun response-for (prompt)
+  (cond ((silent-p prompt)   "Fine. Be that way!")
+        ((yelled-p prompt)   "Whoa, chill out!")
+        ((question-p prompt) "Sure.")
+        (t "Whatever.")))
