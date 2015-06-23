054d857cdaa77f31a40086439d9583a8c1a9c839
Eric Bailey <eric@ericb.me>
2015-06-23 02:17:23 -0500

    http://exercism.io/submissions/4f836c37e8864712961821c8f1926d7c

--- rna-transcription/dna.lisp
+++ rna-transcription/dna.lisp
@@ -1,9 +1,23 @@
+;; -*- compile-command: "sbcl --non-interactive --load *-test.lisp" -*-
+
 (in-package #:cl-user)
 (defpackage #:dna
   (:use #:cl)
   (:export #:to-rna))
 (in-package #:dna)
 
-(defun to-rna (str)
-  "Transcribe a string representing DNA nucleotides to RNA."
-  )
+(defconstant dna->rna
+  '((#\G . #\C)
+    (#\C . #\G)
+    (#\T . #\A)
+    (#\A . #\U)))
+
+(defun transcribe (nucleotide)
+  "Given a DNA nucleotide, returns its RNA complement,
+  or signals an error when an invalid nucleotide is encountered."
+  (or (cdr (assoc nucleotide dna->rna))
+      (signal 'error)))
+
+(defun to-rna (dna)
+  "Given a DNA strand, returns its transcribed RNA strand."
+  (concatenate 'string (mapcar #'transcribe (coerce dna 'list))))
