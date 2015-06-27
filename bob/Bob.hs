c4b422d5f3bec57309f8d8c73e3c3c6dc2dbfd74
Eric Bailey <eric@ericb.me>
2015-06-27 16:03:35 -0500

    http://exercism.io/submissions/2ab3ee519b0a4e52b88674316d30724a

--- bob/Bob.hs
+++ bob/Bob.hs
@@ -1,16 +1,14 @@
-{-# LANGUAGE LambdaCase, ViewPatterns #-}
-
 module Bob (responseFor) where
 
 import Control.Monad (ap)
 import Data.Char (isSpace, isUpper, toUpper)
 
 responseFor :: String -> String
-responseFor = \case
-  (isYelled   -> True) -> "Whoa, chill out!"
-  (isSilent   -> True) -> "Fine. Be that way!"
-  (isQuestion -> True) -> "Sure."
-  _                   -> "Whatever."
-  where isYelled   = ap ((&&) . any isUpper) ((==) =<< map toUpper)
-        isSilent   = (==) =<< filter isSpace
-        isQuestion = ('?' ==) . last
+responseFor s
+  | isYelled s   = "Whoa, chill out!"
+  | isSilent s   = "Fine. Be that way!"
+  | isQuestion s = "Sure."
+  | otherwise    = "Whatever."
+    where isYelled   = ap ((&&) . any isUpper) ((==) =<< map toUpper)
+          isSilent   = (==) =<< filter isSpace
+          isQuestion = ('?' ==) . last
