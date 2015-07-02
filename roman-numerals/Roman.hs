d0cfe3f3e58a4bc6dc0292ae9e26c100e17d5f58
Eric Bailey <eric@ericb.me>
2015-07-01 23:22:25 -0500

    http://exercism.io/submissions/36337289e4cb473b8b941ac66a05f332
    - Naming things
    - Reformatted the `[(Arabic, Roman)]` to consume less vertical space
    - Used `ShowS` for increased efficiency
    - Used `divMod` to avoid redundant calculations
    - Used better pattern-matching to drop the `head`, `null` and `tail` calls

--- roman-numerals/Roman.hs
+++ roman-numerals/Roman.hs
@@ -3,30 +3,25 @@ module Roman (numerals) where
 type Arabic = Int
 type Roman  = String
 
-translations :: [(Arabic, Roman)]
-translations = [(1000,  "M"),
-                (900,  "CM"),
-                (500,   "D"),
-                (400,  "CD"),
-                (100,   "C"),
-                (90,   "XC"),
-                (50,    "L"),
-                (40,   "XL"),
-                (10,    "X"),
-                (9,    "IX"),
-                (5,     "V"),
-                (4,    "IV"),
-                (1,     "I")]
+conversions :: [(Arabic, Roman)]
+conversions = [(1000,  "M"), (900, "CM"),
+               (500,   "D"), (400, "CD"),
+               (100,   "C"), (90,  "XC"),
+               (50,    "L"), (40,  "XL"),
+               (10,    "X"), (9,   "IX"),
+               (5,     "V"), (4,   "IV"),
+               (1,     "I")]
 
 numerals :: Arabic -> Roman
-numerals n = go n "" translations
-  where go :: Arabic -> String -> [(Arabic, Roman)] -> Roman
-        go x r' ts
-          | null ts   = r'
-          | m > 0     = go (x-a*m) (r' ++ concat (replicate m r)) (tail ts)
-          | otherwise = go x r' (tail ts)
-            where (a,r) = head ts
-                  m     = x `div` a
+numerals = go ("" ++) conversions
+  where go :: ShowS -> [(Arabic, Roman)] -> Arabic -> Roman
+        go f [] _ = f ""
+        go f ((a, r):cs') x
+          | x >= a     = go f' cs' x'
+          | otherwise = go f cs' x
+          where (n, x') = x `divMod` a
+                f'      = f . (concat (replicate n r) ++)
+
 
 -- Local Variables:
 -- compile-command: "runhaskell roman-numerals_test.hs"
