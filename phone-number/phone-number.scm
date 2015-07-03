c64b22ed43033c8c2a8c454c237911651e760998
Eric Bailey <eric@ericb.me>
2015-07-02 21:52:59 -0500

    added scheme/phone-number

---
+++ phone-number/phone-number.scm
@@ -0,0 +1,16 @@
+(define-module (phone-number)
+  #:export (numbers area-code pprint)
+  #:autoload (ice-9 format) (format))
+
+
+
+
+
+(define (numbers number-string)
+  )
+
+(define (area-code number-string)
+  )
+
+(define (pprint number-string)
+  )
