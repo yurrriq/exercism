6f71dc32a83080667b4763a26ea0181cf9b2ff16
Eric Bailey <eric@ericb.me>
2015-07-04 02:19:49 -0500

    http://exercism.io/submissions/91e3c0baa5574408acfc1bb08a5cbd7d

--- anagram/anagram.scm
+++ anagram/anagram.scm
@@ -1,2 +1,24 @@
 (define-module (anagram)
-  #:export (anagrams-for))
+  #:export (anagrams-for)
+  #:autoload (srfi srfi-1) (every)
+  #:autoload (srfi srfi-26) (cute))
+
+;;; ===== HELPER FUNCTIONS =====
+
+(define (anagram? x y)
+  (every (cute <> x y) (list string-length=? (negate string=?) same-letters?)))
+
+(define (char-ci-sort str)
+  (sort (string->list str) char-ci<?))
+
+(define (same-letters? x y)
+  (every char-ci=? (char-ci-sort x) (char-ci-sort y)))
+
+(define (string-length=? x y)
+  (= (string-length x) (string-length y)))
+
+
+;;; ===== PUBLIC API =====
+
+(define (anagrams-for word candidates)
+  (filter (cute anagram? word <>) candidates))
