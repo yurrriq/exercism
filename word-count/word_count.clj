4e435782ec7439a154d7f51add61d719ddc53754
Eric Bailey <eric@ericb.me>
2015-07-20 18:19:57 -0500

    http://exercism.io/submissions/b68438f2b29e49b286b938b388070fce updated to Clojure 1.7 syntax

--- word-count/word_count.clj
+++ word-count/word_count.clj
@@ -1,5 +1,5 @@
 (ns word-count)
 
 (defn word-count [s]
-  (reduce #(update-in %1 [%2] (fnil inc 0)) {}
+  (reduce #(update %1 %2 (fnil inc 0)) {}
           (re-seq #"\w+" (clojure.string/lower-case s))))
