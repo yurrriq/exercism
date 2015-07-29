e393501106959bee3f428440bd83f8b33eb3174a
Eric Bailey <eric@ericb.me>
2015-07-29 03:04:22 -0500

    http://exercism.io/submissions/f32a85d5b1a442fe8f525c8cb81eec91

--- robot-name/project.clj
+++ robot-name/project.clj
@@ -1,6 +1,8 @@
-(defproject robot-name "0.1.0-SNAPSHOT"
-  :description "robot-name exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/robot-name"
+(defproject robot-name "0.1.0"
+  :description  "robot-name exercise."
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/robot-name"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.typed "0.3.9"]]
+  :plugins      [[lein-typed "0.3.5"]])
