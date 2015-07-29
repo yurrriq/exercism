07937161eccdcd6ef2a9a724b1922e0c149f348e
Eric Bailey <eric@ericb.me>
2015-07-29 04:56:06 -0500

    http://exercism.io/submissions/ea64a8ab5dfc4a2e9c5650decc8f2303

--- leap/project.clj
+++ leap/project.clj
@@ -1,6 +1,6 @@
-(defproject leap "0.1.0-SNAPSHOT"
-  :description "leap exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/leap"
+(defproject leap "0.1.0"
+  :description  "leap exercise."
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/leap"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]])
