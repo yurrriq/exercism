55d5b25e3aa556078e56d598f616ab4a5a976e7b
Eric Bailey <eric@ericb.me>
2015-08-01 04:33:43 -0500

    http://exercism.io/submissions/fdf99d26d99c4c4b8ecce3ef3a61f9be

---
+++ crypto-square/CryptoSquare.hs
@@ -0,0 +1,33 @@
+module CryptoSquare (ciphertext,
+                     plaintextSegments,
+                     normalizePlaintext,
+                     squareSize,
+                     normalizeCiphertext) where
+
+import           Control.Monad   (ap)
+import           Data.Bool       (bool)
+import           Data.Char       (isAlphaNum, toLower)
+import           Data.List       (transpose)
+import           Data.List.Split (chunksOf)
+import           Data.Maybe      (mapMaybe)
+
+ciphertext :: String -> String
+ciphertext = concat . transpose . plaintextSegments
+
+plaintextSegments :: String -> [String]
+plaintextSegments = (chunksOf =<< squareSize) . normalizePlaintext
+
+normalizePlaintext :: String -> String
+normalizePlaintext = mapMaybe (iffAp isAlphaNum toLower)
+
+squareSize :: String -> Int
+squareSize = ceiling . (sqrt :: Double -> Double) . fromIntegral . length
+
+normalizeCiphertext :: String -> String
+normalizeCiphertext = unwords . transpose . plaintextSegments
+
+iff :: (a -> Bool) -> a -> Maybe a
+iff = ap (bool Nothing . Just)
+
+iffAp :: (a -> Bool) -> (a -> b) -> a -> Maybe b
+iffAp p f x = iff p x >> (Just . f) x
