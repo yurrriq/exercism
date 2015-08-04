4f7f7e69da6f113154d2e57f03375aadb792147e
Eric Bailey <eric@ericb.me>
2015-08-03 23:16:44 -0500

    http://exercism.io/submissions/3e46087674b849a4b458aba0cffb7241
    intial submission
    TODO:
    - full docstring coverage
    - type annotations

--- meetup/project.clj
+++ meetup/project.clj
@@ -1,6 +1,7 @@
-(defproject meetup "0.1.0-SNAPSHOT"
-  :description "meetup exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/meetup"
+(defproject meetup "0.1.0"
+  :description  "meetup exercise."
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/meetup"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.typed "0.3.9"]])
