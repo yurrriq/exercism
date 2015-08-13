1ea74c014a27f41518fffe185386947ab530ee02
Eric Bailey <eric@ericb.me>
2015-08-13 18:39:47 -0500

    http://exercism.io/submissions/c0bc429918954d19a1a89cfe1f3afffc

--- point-mutations/dna.erl
+++ point-mutations/dna.erl
@@ -1,10 +1,13 @@
 -module(dna).
 -export([hamming_distance/2]).
 
+hamming_distance(Strand1, Strand2) ->
+  zipfoldl(fun (X, Y, Z) -> Z + ne_to_int(X, Y) end, 0, Strand1, Strand2).
+
+zipfoldl(F, Acc, [HA|TA], [HB|TB]) -> zipfoldl(F, F(HA, HB, Acc), TA, TB);
+zipfoldl(_, Acc, [], []) -> Acc.
+
 bool_to_int(true)  -> 1;
 bool_to_int(false) -> 0.
 
 ne_to_int(X, Y) -> bool_to_int(X =/= Y).
-
-hamming_distance(Strand1, Strand2) ->
-  lists:sum(lists:zipwith(fun ne_to_int/2, Strand1, Strand2)).
