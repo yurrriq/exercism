54d3096d6fb179864050415e4f5106ea6de069c6
Eric Bailey <eric@ericb.me>
2015-08-14 14:01:30 -0500

    http://exercism.io/submissions/3469bf93b9094a0c9bf34839b6c14675
    use `=` to compare numbers

--- point-mutations/point-mutations.scm
+++ point-mutations/point-mutations.scm
@@ -2,6 +2,6 @@
   #:export (hamming-distance))
 
 (define (hamming-distance x y)
-  (if (eqv? (string-length x) (string-length y))
+  (if (= (string-length x) (string-length y))
       (length (filter not (map char=? (string->list x) (string->list y))))
       (error "Can't calculate distance for strings of unequal length.")))
