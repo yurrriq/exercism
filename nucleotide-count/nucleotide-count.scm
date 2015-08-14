b03a0559c5e7db20b5c476dad210060618c12c4d
Eric Bailey <eric@ericb.me>
2015-08-14 14:00:21 -0500

    http://exercism.io/submissions/eb8b77e757fb43c5889182e69076e0cc

--- nucleotide-count/nucleotide-count.scm
+++ nucleotide-count/nucleotide-count.scm
@@ -1,2 +1,24 @@
 (define-module (nucleotide-count)
-  #:export (nucleotide-counts dna-count))
+  #:export (nucleotide-counts dna-count)
+  #:autoload (srfi srfi-1) (fold))
+
+(define valid-nucleotides (string->list "ACGT"))
+
+(define (validate nucleotide)
+  (if (memv nucleotide valid-nucleotides)
+      nucleotide
+      (error "Invalid nucleotide")))
+
+(define (dna-count nucleotide strand)
+  (validate nucleotide) ; If no error, throw away results
+  (fold (λ (nucleotide* sum)
+          (if (char=? (validate nucleotide*) nucleotide)
+              (1+ sum)
+              sum))
+        0
+        (string->list strand)))
+
+(define (nucleotide-counts strand)
+  (map (λ (nucleotide)
+         `(,nucleotide . ,(dna-count nucleotide strand)))
+       valid-nucleotides))
