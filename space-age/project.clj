df76f8d37a87c94e2914787e5dc2304fbf32d57a
Eric Bailey <eric@ericb.me>
2015-08-16 03:45:30 -0500

    http://exercism.io/submissions/2c903ed17d5b46b8a6e9133e9a2f5650

--- space-age/project.clj
+++ space-age/project.clj
@@ -1,6 +1,7 @@
-(defproject space-age "0.1.0-SNAPSHOT"
-  :description "space-age exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/space-age"
+(defproject space-age "0.1.0"
+  :description  "space-age exercise."
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/space-age"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.typed "0.3.9"]])
