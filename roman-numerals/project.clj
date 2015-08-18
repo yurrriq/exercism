9f479ba86c0636f634a65783b7699bfc7690c5a5
Eric Bailey <eric@ericb.me>
2015-08-18 04:16:17 -0500

    http://exercism.io/submissions/15da3c6c1565421386ea7d8333bbfc3c

--- roman-numerals/project.clj
+++ roman-numerals/project.clj
@@ -1,6 +1,7 @@
-(defproject roman-numerals "0.1.0-SNAPSHOT"
-  :description "roman-numerals exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/roman-numerals"
+(defproject roman-numerals "0.1.0"
+  :description  "roman-numerals exercise."
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/roman-numerals"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure    "1.7.0"]
+                 [org.clojure/core.typed "0.3.9"]])
---
+++ roman-numerals/roman_numerals.clj
@@ -0,0 +1,49 @@
+(ns roman-numerals
+  "Converting decimal numbers to Roman numerals."
+  {:author "Eric Bailey"}
+  (:require [clojure.core.typed :refer [Int NilableNonEmptyASeq
+                                        ann defalias fn let loop]])
+  (:refer-clojure :exclude [fn let loop]))
+
+;; == TYPE ALIASES =============================================================
+
+(defalias Arabic Int)
+(defalias Roman  String)
+
+
+;; == PRIVATE API ==============================================================
+
+(ann arabic->roman '['[Arabic Roman] *])
+(def ^:private arabic->roman
+  [[1000  "M"], [900 "CM"],
+   [500   "D"], [400 "CD"],
+   [100   "C"], [90  "XC"],
+   [50    "L"], [40  "XL"],
+   [10    "X"], [9   "IX"],
+   [5     "V"], [4   "IV"],
+   [1     "I"]])
+
+(ann divmod [Arabic Arabic -> '[Arabic Arabic]])
+(defn- divmod [^Integer n ^Integer d] [(unchecked-divide-int n d) (mod n d)])
+
+
+;; == PUBLIC API ===============================================================
+
+(ann numerals [Arabic -> Roman])
+(defn numerals [n]
+  (loop [f   :- [Roman -> Roman] str
+         ars :- (NilableNonEmptyASeq '[Arabic Roman]) (seq arabic->roman)
+         n   :- Arabic n]
+    (if-let [[[a r] & ars'] ars]
+      (if (< n a) (recur f ars' n)
+          (let [[n m] (divmod n a)]
+            (recur (comp f #(str (apply str (repeat n r)) %)) ars' m)))
+      (f ""))))
+
+
+;; == EMACS CONFIG =============================================================
+
+;; Local Variables:
+;; mode: clojure
+;; eval: (typed-clojure-mode)
+;; End:
