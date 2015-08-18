061a81d64711738835822ac1d74421a6e7de7eb6
Eric Bailey <eric@ericb.me>
2015-08-17 21:40:35 -0500

    http://exercism.io/submissions/d014bd99e0484c659d1762627fbc8f74

--- scrabble-score/project.clj
+++ scrabble-score/project.clj
@@ -1,6 +1,7 @@
-(defproject scrabble-score "0.1.0-SNAPSHOT"
+(defproject scrabble-score "0.1.0"
   :description "scrabble-score exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/scrabble-score"
+  :url          "https://github.com/yurrriq/exercism/tree/clojure/scrabble-score"
   :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :test-paths   [""]
+  :dependencies [[org.clojure/clojure    "1.7.0"]
+                 [org.clojure/core.typed "0.3.9"]])
