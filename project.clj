d9b68e7ca56d15ed525d80280e125d433322c9d2
Eric Bailey <eric@ericb.me>
2015-08-19 15:15:31 -0500

    Add project.clj path hacks

--- project.clj
+++ project.clj
@@ -1,47 +1,13 @@
-(defproject xclojure "0.1.0"
-  :description  "Exercism Exercises in Clojure"
-  :url          "https://github.com/yurrriq/exercism/tree/clojure"
-  :source-paths ["anagram"
-                 "beer-song"
-                 "binary"
-                 "bob"
-                 "etl"
-                 "gigasecond"
-                 "grade-school"
-                 "grains"
-                 "leap"
-                 "meetup"
-                 "nucleotide-count"
-                 "phone-number"
-                 "point-mutations"
-                 ;; "prime-factors"
-                 "rna-transcription"
-                 "robot-name"
-                 "roman-numerals"
-                 "scrabble-score"
-                 "space-age"
-                 "triangle"
-                 "word-count"]
-  :test-paths   ["anagram"
-                 "beer-song"
-                 "binary"
-                 "bob"
-                 "etl"
-                 "gigasecond"
-                 "grade-school"
-                 "grains"
-                 "leap"
-                 "meetup"
-                 "nucleotide-count"
-                 "phone-number"
-                 "point-mutations"
-                 ;; "prime-factors"
-                 "rna-transcription"
-                 "robot-name"
-                 "roman-numerals"
-                 "scrabble-score"
-                 "space-age"
-                 "triangle"
-                 "word-count"]
-  :dependencies [[org.clojure/clojure    "1.7.0"]
-                 [org.clojure/core.typed "0.3.11"]])
+(let [paths (-> (:out (clojure.java.shell/sh "ls"))
+                (clojure.string/split #"\n")
+                (->> (remove #{"README.org" "project.clj" "target"
+                               ;; Removed until solved
+                               "prime-factors"}))
+                vec)]
+  (defproject xclojure "0.1.0"
+    :description  "Exercism Exercises in Clojure"
+    :url          "https://github.com/yurrriq/exercism/tree/clojure"
+    :source-paths ~paths
+    :test-paths   ~paths
+    :dependencies [[org.clojure/clojure    "1.7.0"]
+                   [org.clojure/core.typed "0.3.11"]]))
