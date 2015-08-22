c254b5d01ab7c7f4b6d4ca0f15bd49f32a7dc908
Eric Bailey <eric@ericb.me>
2015-08-22 18:21:34 -0500

    http://exercism.io/submissions/b21afcbb9a8d4de3b63fe8974ef4e8f2

--- rna-transcription/rna_transcription.clj
+++ rna-transcription/rna_transcription.clj
@@ -1,5 +1,20 @@
-(ns rna-transcription)
+(ns rna-transcription
+  "Transcribing DNA strands to their RNA complements."
+  {:author "Eric Bailey"})
 
-(defn to-rna [dna]
-  (let [invalid #(throw (AssertionError. "Invalid DNA strand."))]
-    (apply str (map #(case % \G \C, \C \G, \T \A, \A \U, (invalid)) dna))))
+(def ^:private dna->rna
+  "A map from DNA nucleotide to RNA complement."
+  {\A \U
+   \C \G
+   \G \C
+   \T \A})
+
+(defn- invalid
+  "Throw a self-describing `AssertionError` for and invalid DNA nucleotide."
+  [n] (throw (AssertionError. (str "Invalid nucleotide: " n))))
+
+(defn to-rna
+  "Given a DNA strand, translate each nucleotide to RNA and return the result,
+  throwing an `AssertionError` if any invalid nucleotide is found."
+  [dna]
+  (apply str (map (some-fn dna->rna invalid) dna)))
