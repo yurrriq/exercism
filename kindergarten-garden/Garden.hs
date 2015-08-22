634d677e28a1649b1095e5dda692fd559ec6697e
Eric Bailey <eric@ericb.me>
2015-08-22 04:46:14 -0500

    http://exercism.io/submissions/5303683866ac4ea790983770121debf3

--- kindergarten-garden/Garden.hs
+++ kindergarten-garden/Garden.hs
@@ -1,4 +1,14 @@
-{-# LANGUAGE LambdaCase #-}
+{-|
+Module      : Garden
+Copyright   : (c) Eric Bailey, 2015
+License     : MIT
+
+Maintainer  : Eric Bailey
+Stability   : experimental
+Portability : portable
+
+Parsing kindergarten garden diagrams.
+-}
 
 module Garden (Plant(..), defaultGarden, garden, lookupPlants) where
 
@@ -14,16 +24,22 @@ data Plant   = Grass
              | Violets
              deriving (Bounded, Enum, Eq, Read, Show)
 
-type Layout  = String
+-- | A diagram is a string.
+type Diagram = String
 
+-- | A student('s first name) is a string.
 type Student = String
 
+-- | A garden is a map from 'Student' to a list of 'Plants'.
 type Garden  = Map Student [Plant]
 
-defaultGarden :: Layout -> Garden
+-- | Call 'garden' with 'classList' and return the default 'Garden'.
+defaultGarden :: Diagram -> Garden
 defaultGarden = garden classList
 
-garden :: [Student] -> Layout -> Garden
+-- | Given a list of 'Student's and a 'Diagram',
+-- return the 'Garden' represented by the the given 'Garden'.
+garden :: [Student] -> Diagram -> Garden
 garden students = M.fromListWith (++) .
                   concatMap (sortZip students . kidRows) .
                   reverse . linesMap fromString
@@ -32,12 +48,17 @@ garden students = M.fromListWith (++) .
     kidRows  = chunksOf 2
     sortZip  = zip . sort
 
+-- | Given a 'Student' and a 'Garden', return the list of 'Plant's
+-- belonging to the given 'Student'.
 lookupPlants :: Student -> Garden -> [Plant]
 lookupPlants = M.findWithDefault []
 
+-- Given a string of first letters of 'Plant' names,
+-- return the list of 'Plant's it represents.
 fromString :: String -> [Plant]
 fromString = mapMaybe $ (toEnum <$>) . (`elemIndex` "GCRV")
 
+-- The default list of 'Student's.
 classList :: [Student]
 classList = ["Alice",  "Bob",    "Charlie", "David",
              "Eve",    "Fred",   "Ginny",   "Harriet",
