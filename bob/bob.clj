708c6d8aa125ea1fdf10d5149efe86855d74bc34
Eric Bailey <eric@ericb.me>
2015-08-22 16:44:42 -0500

    Cosmetic tweaks to bob
    http://exercism.io/submissions/caeb4ecf46694f50b8f10bd368f65dc4
    - `(:require [clojure.string :as s])`
    - Use `clojure.string/blank?`
    - Pull out predicates into named functions
    - Add superfluous docstrings
    - Use `(condp apply [expr] pred result ... default)` trick
    - Align pred/result pairs

--- bob/bob.clj
+++ bob/bob.clj
@@ -1,12 +1,25 @@
 (ns bob
   "Mimicking the conversational ineptitude of a lackadaisical teenager."
-  {:author "Eric Bailey"})
+  {:author "Eric Bailey"}
+  (:require [clojure.string :as s]))
 
-(defn response-for [prompt]
+(defn- question?
+  "Given a prompt, return `true` if it ends with `?`, otherwise `false`."
+  [prompt]
+  (.endsWith prompt "?"))
+
+(defn- yelled?
+  "Given a prompt, return `true` if there exists an uppercase letter and the
+  prompt is equal to itself uppercased, otherwise `false`."
+  [prompt]
+  (and #(re-find #"[A-Z]" prompt)
+       (= prompt (s/upper-case prompt))))
+
+(defn response-for
   "Given a prompt, return a teenager's response."
-  (cond
-    (re-matches #"^\s*$" prompt) "Fine. Be that way!"
-    (and (re-find #"[A-Z]" prompt)
-         (= prompt (clojure.string/upper-case prompt))) "Whoa, chill out!"
-    (.endsWith prompt "?") "Sure."
-    :else "Whatever."))
+  [prompt]
+  (condp apply [prompt]
+    s/blank?  "Fine. Be that way!"
+    yelled?   "Whoa, chill out!"
+    question? "Sure."
+    :else     "Whatever."))
