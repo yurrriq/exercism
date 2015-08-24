d70b7a11a369a08b885c70d0f329fd726f0b3416
Eric Bailey <eric@ericb.me>
2015-08-24 14:22:16 -0500

    http://exercism.io/submissions/0b75bfd1c40c463ca7b28a69f195f971

--- anagram/anagram.clj
+++ anagram/anagram.clj
@@ -1,8 +1,21 @@
 (ns anagram
+  "Selecting sublists of anagrams."
+  {:author "Eric Bailey"}
   (:require [clojure.string :refer [lower-case]]))
 
-(defn anagrams-for [s coll]
-  (filter #(let [comparison (map lower-case [s %])]
-             (when (apply not= comparison)
-               (apply = (map sort comparison))))
-          coll))
+(defn- anagram-for?
+  "Given a word, return a transducer that removes candidates which are
+  case-insensitive equal to or not an anagram of the given word."
+  [word]
+  (let [word-lowered (lower-case word)
+        word-sorted  (sort word-lowered)]
+    (remove (fn [candidate]
+              (let [candidate-lowered (lower-case candidate)]
+                (or (= candidate-lowered word-lowered)
+                    (not= (sort candidate-lowered) word-sorted)))))))
+
+(defn anagrams-for
+  "Given a word and a list of anagram candidates, return a sublist
+  where each candidate is an anagram for the given word."
+  [word candidates]
+  (transduce (anagram-for? word) conj [] candidates))
