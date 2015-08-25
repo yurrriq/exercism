37882c0f5c8ecb0982caa7c97520e5301588e592
Eric Bailey <eric@ericb.me>
2015-08-24 20:39:37 -0500

    http://exercism.io/submissions/c926ac3cc2c64d6281d63b3208dc62e6

--- point-mutations/point_mutations.clj
+++ point-mutations/point_mutations.clj
@@ -1,14 +1,30 @@
 (ns point-mutations)
 
-(defn hamming-distance [a b]
-  (when (apply = (map count [a b]))
-    (reduce (fn [n matches?] (cond-> n matches? inc)) 0
-            (map not= a b))))
+(defn hamming-distance
+  "Given two DNA strands, compute and return their Hamming distance,
+  i.e. the number of positions at which the corresponding nucleotides are
+  different. If the strands differ in length, return nil."
+  [a b]
+  (when (= (count a) (count b))
+    (reduce (fn [distance equal?]
+              (if equal?
+                distance
+                (inc distance)))
+            0 (map = a b))))
 
 (comment
-  ;; Slower
-  (count (filter true? (map not= a b)))
+  (defn tranducing [a b]
+    (let [hamming-xform (comp (map (partial apply =))
+                              (map (fn [same?] (if same? 0 1))))]
+      (when (= (count a) (count b))
+        (transduce hamming-xform + (map vector a b)))))
 
-  ;; Slowest
-  ;; https://gist.github.com/mihneadb/4318257
-  (count (filter true? (map (partial reduce not=) (map vector a b)))))
+  (defn looping [a b]
+    (loop [d 0
+           a a
+           b b]
+      (if (and (empty? a) (empty? b)) d
+          (when-let [[h & t] (seq b)]
+            (recur (if (= (first a) h) d (inc d))
+                   (rest a)
+                   t))))))
