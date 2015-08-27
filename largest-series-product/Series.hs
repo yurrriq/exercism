1d99c2a056dca4b0486d3c699e93296920f76f60
Eric Bailey <eric@ericb.me>
2015-08-27 13:05:02 -0500

    Add type signatures to haskell/largest-series-product

---
+++ largest-series-product/Series.hs
@@ -0,0 +1,22 @@
+{-|
+Module      : Series
+Copyright   : Public Domain
+License     : Unlicense
+
+Maintainer  : Eric Bailey
+Stability   : stable
+Portability : portable
+
+Calculating the largest product for a series of consecutive digits of length n.
+-}
+
+module Series (digits, slices, largestProduct) where
+
+digits :: [Char] -> [Int]
+digits = undefined
+
+slices :: Int -> [Char] -> [[Int]]
+slices = undefined
+
+largestProduct :: Int -> [Char] -> Int
+largestProduct = undefined
