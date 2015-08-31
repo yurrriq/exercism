00924897ad237af8e3c676f61991410772c40c00
Eric Bailey <eric@ericb.me>
2015-08-31 03:08:28 -0500

    :lipstick:
    Also, use `all isSpace` for `isSilent`.

--- bob/Bob.hs
+++ bob/Bob.hs
@@ -18,10 +18,11 @@ import           Data.Char     (isSpace, isUpper, toUpper)
 -- | Given a prompt ('String'), returns a teenager's response ('String').
 responseFor :: String -> String
 responseFor s
-  | isYelled s   = "Whoa, chill out!"
-  | isSilent s   = "Fine. Be that way!"
+  | isYelled   s = "Whoa, chill out!"
+  | isSilent   s = "Fine. Be that way!"
   | isQuestion s = "Sure."
   | otherwise    = "Whatever."
-    where isYelled   = ap ((&&) . any isUpper) ((==) =<< map toUpper)
-          isSilent   = (==) =<< filter isSpace
-          isQuestion = ('?' ==) . last
+    where
+      isYelled   = ap ((&&) . any isUpper) ((==) =<< map toUpper)
+      isSilent   = all isSpace
+      isQuestion = ('?' ==) . last
