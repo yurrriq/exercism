57809a22722226cb289b154ea2c5a8aa864ac429
Eric Bailey <eric@ericb.me>
2015-09-15 15:49:20 -0500

    Add docs, cleanup :construction:

--- matrix/Matrix.hs
+++ matrix/Matrix.hs
@@ -1,78 +1,100 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Matrix ( Matrix, fromString, fromList
-              , rows, cols, row, column, shape
+{-|
+Module      : Matrix
+Copyright   : (c) Eric Bailey, 2015
+License     : MIT
+
+Maintainer  : Eric Bailey
+Stability   : stable
+Portability : portable
+
+Creating and manipulating 2D matrices.
+-}
+
+module Matrix ( Matrix{-(..}-}, Shape, empty, matrix, fromString, fromList
+              , rows, cols, shape, row, column
               , flatten, reshape, transpose)
        where
 
 import           Control.Arrow ((&&&))
 import           Control.Monad (ap)
+-- import           Data.List.Split (chop)
 import           Data.Vector   (Vector)
 import qualified Data.Vector   as V
 
+-- | A 2D matrix.
 data Matrix a = Matrix
-  { cells :: Vector a
-  , rows  :: Int
-  , cols  :: Int
+  { cells :: Vector a -- ^ A flat vector of cells.
+  , rows  :: Int      -- ^ The number of rows.
+  , cols  :: Int      -- ^ The number of columns.
   }
   deriving (Eq, Show)
 
-matrix :: (Int, Int) -> Vector a -> Matrix a
+-- | A 'Shape' is a pair representing the number of 'rows' and 'cols'
+-- in a 'Matrix'.
+type Shape = (Int, Int)
+
+-- | The empty 'Matrix'.
+empty :: Matrix a
+empty = matrix (0, 0) V.empty
+
+-- | Given a 'Shape' and a vector of cells, create and return a 'Matrix'.
+matrix :: Shape -> Vector a -> Matrix a
 matrix (x, y) v
   | x * y == V.length v = Matrix { cells = v, rows = x, cols = y }
   | otherwise          = error "Incorrect number of cells."
 
-empty :: Matrix a
-empty = matrix (0, 0) V.empty
-
--- TODO: redo
+-- | Given a string representing a 'Matrix', with rows delimited by newlines and
+-- columns delimited by whitespace, create and return the 'Matrix'.
 fromString :: Read a => String -> Matrix a
 fromString = fromList . map readRow . lines
   where readRow = (\case [] -> []; (x, xs):_ -> x : readRow xs) . reads
+-- chop :: ([a] -> (b, [a])) -> [a] -> [b]
 
+-- | Given a list of rows (lists of items), create and return a 'Matrix'.
 fromList :: [[a]] -> Matrix a
 fromList []         = empty
 fromList ([]:_)     = empty
 fromList xxs@(xs:_) = matrix (length xxs, length xs) $
                       V.fromList (concat xxs)
 
+-- | Given a 'Matrix' @m@, return @(@'rows'@ m, @'cols'@ m)@.
+shape :: Matrix a -> Shape
+shape = rows &&& cols
+
+-- | Given an index @n@ and a 'Matrix', return the vector of items at row @n@ in
+-- the given matrix.
 row :: Int -> Matrix a -> Vector a
-row n = (flip (V.slice =<< (n *)) . cells) `ap` cols
+row n = (dice . cells) `ap` cols
+  where dice = flip (V.slice =<< (n *))
+-- where dice v x = V.slice (n * x) x v
 -- row n m = (\x -> V.slice (n * x) x $ cells m) (cols m)
 
+-- | Given an index @n@ and a 'Matrix', return the vector of itmes at column @n@
+-- in the given matrix.
 column :: Int -> Matrix a -> Vector a
 column n m = takeNth (cols m) (V.drop n (cells m))
 
--- TODO: cleanup
--- NOTE: this is NOT efficient
+-- | Given a 'Matrix', return a copy, rotated 90° to the right.
 transpose :: Matrix a -> Matrix a
 transpose = go =<< cols
   where go n m = matrix (n, rows m) $
                  V.concatMap (`column` m) $
                  V.fromList [0..pred n]
--- transpose m = (matrix . flip (,) (rows m)) `ap`
---               (V.concatMap (`column` m) . V.fromList . enumFromTo 0 . pred) $
---               cols m
--- transpose m = (\n ->
---                  matrix (n, rows m) $
---                  V.concatMap (`column` m) (V.fromList [0..pred n]))
---               (cols m)
 
-reshape :: (Int, Int) -> Matrix a -> Matrix a
+
+-- | Given a 'Shape' @s@ and a 'Matrix', return 'matrix'@ s (@'cells'@ m)@.
+reshape :: Shape -> Matrix a -> Matrix a
 reshape = (. cells) . matrix
 
+-- | Given a 'Matrix', return its 'cells'.
 flatten :: Matrix a -> Vector a
 flatten = cells
 
-shape :: Matrix a -> (Int, Int)
-shape = rows &&& cols -- <3 that
-
--- | Return a list of every nth element of a given list.
+-- | Return a vector of every nth element of a given vector.
+--
+-- See also: <http://yurrriq.codes/haxkell/clojure-list/Data-List-Clojure.html#v:takeNth Data.List.Clojure.takeNth>
 takeNth :: Int -> Vector a -> Vector a
 takeNth n v | V.null v  = V.empty
             | otherwise = V.head v `V.cons` takeNth n (V.drop n v)
-
-
-
-
--- NOTE: Data.Vector.backpermute
