552d92dec11b74668bd3a7b31ab854d71be15943
Eric Bailey <eric@ericb.me>
2015-09-19 20:33:24 -0500

    http://exercism.io/submissions/b6289783aacd47bb80902f024dc61314

---
+++ etl/etl.erl
@@ -0,0 +1,15 @@
+-module(etl).
+
+-export([transform/1]).
+
+transform(X) -> orddict:to_list(orddict:from_list(lists:flatten(invert(X)))).
+
+invert(Pairs) ->
+  F = fun ({Key, Values}, Acc1) ->
+          G = fun (Value, Acc2) ->
+                  H = fun (Old) -> lists:flatten([Old] ++ [Key]) end,
+                  orddict:update(string:to_lower(Value), H, Key, Acc2)
+              end,
+          lists:foldl(G, Acc1, Values)
+      end,
+  lists:foldl(F, orddict:new(), Pairs).
