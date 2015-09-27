a80753068aa643c4a7df87ce5e71473febe2e91e
Eric Bailey <eric@ericb.me>
2015-09-27 07:38:53 -0500

    http://exercism.io/submissions/0cf5e8c952ef430690ab9e2b7a6e7938
    Incorporate @michaelkebe's suggestions.

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,7 +1,6 @@
 exports.compute = function(a, b) {
   if (a.length != b.length) { throw 'DNA strands must be of equal length.' }
-  var bs = b.split('')
-  return a.split('').reduce(function(distance, nucleotide, index, _array) {
-    return distance + ((nucleotide != bs[index]) | 0)
+  return a.split('').reduce(function(distance, nucleotide, index) {
+    return distance + ((nucleotide != b[index]) | 0)
   }, 0)
 }
