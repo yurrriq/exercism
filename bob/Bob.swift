9402880091f6dd49debb7c2b8efa007e85916c44
Eric Bailey <eric@ericb.me>
2015-12-28 02:30:37 -0600

    bob
    http://exercism.io/submissions/8562d68092ad42f8aab161b15b12cee9

--- bob/Bob.swift
+++ bob/Bob.swift
@@ -6,9 +6,37 @@
 //
 //
 
+import Foundation
+
 class Bob {
 
-  class func hey(input: String) -> String{
+  class func hey(input: String) -> String {
+    if input.isSilent   { return "Fine, be that way." }
+    if input.isYelled   { return "Woah, chill out!"   }
+    if input.isQuestion { return "Sure."              }
+
     return "Whatever."
   }
+
+}
+
+
+extension String {
+
+  /// Return `true` iff `self` contains only whitespace.
+  var isSilent: Bool {
+    return stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).isEmpty
+  }
+
+  /// Return `true` iff `self` has at least one uppercase letter and is equal to itself uppercased.
+  var isYelled: Bool {
+    let notUppercase = NSCharacterSet.uppercaseLetterCharacterSet().invertedSet
+    return !stringByTrimmingCharactersInSet(notUppercase).isEmpty && self == uppercaseString
+  }
+
+  /// Return `true` iff `self` ends with `"?"`.
+  var isQuestion: Bool {
+    return hasSuffix("?")
+  }
+
 }
