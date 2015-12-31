1090d9e4ea8205204194a6ccfd4c3c5b98f6bb79
Eric Bailey <eric@ericb.me>
2015-12-31 01:03:52 -0600

    Tweak bob

--- bob/bob.erl
+++ bob/bob.erl
@@ -1,18 +1,51 @@
 -module(bob).
 -export([response_for/1]).
 
-response_for(Prompt) ->
-    case Prompt of
-        [] -> "Fine. Be that way!";
-        _  ->
-            Last      = lists:last(Prompt),
-            SomeUpper = lists:any(fun (C) -> C >= $A andalso C =< $Z end, Prompt),
-            Trimmed   = tl(re:replace(Prompt, "\\s*", "")),
-            Yelled    = string:to_upper(Prompt),
-            case {Last, SomeUpper, Trimmed, Prompt} of
-                {_, true, _, Yelled} -> "Whoa, chill out!";
-                {$?, _, _, _}        -> "Sure.";
-                {_, _, [], _}        -> "Fine. Be that way!";
-                {_, _, _, _}         -> "Whatever."
-            end
-    end.
+%% @type prompt(). A prompt is a string.
+-type prompt() :: string().
+
+%% @type response(). A response is a string.
+-type response() :: string().
+
+%% @type character(). A character is an integer.
+-type character() :: integer().
+
+%% @doc Given a prompt, return an appropriate response.
+-spec response_for(prompt()) -> response().
+response_for([]) -> "Fine. Be that way!";
+response_for(Prompt) when is_list(Prompt) ->
+  answer(Prompt,
+         [ {fun is_yelled/1,    "Whoa, chill out!"}
+         , {fun is_question/1,  "Sure."}
+         , {fun is_silent/1,    "Fine. Be that way!"}]).
+
+%% @doc Given a `Prompt' and a list of {`Predicate', `Response'},
+%% return the first `Response' where `Predicate(Prompt)' holds.
+%%
+%% If no predicate holds, return `"Whatever"'.
+-spec answer(prompt(), [{function(), response()}]) -> response().
+answer(_, []) -> "Whatever.";
+answer(Prompt, [{Pred, Response} | Rest]) ->
+  case Pred(Prompt) of
+    true  -> Response;
+    false -> answer(Prompt, Rest)
+  end.
+
+%% @doc Given a prompt, return `true' iff it has at least one uppercase letter
+%% and is equal to itself uppercased.
+-spec is_yelled(prompt()) -> boolean().
+is_yelled(S) -> lists:any(fun is_upper/1, S) andalso S =:= string:to_upper(S).
+
+%% @doc Given a prompt, return `true' iff it ends with `$?'.
+-spec is_question(prompt()) -> boolean().
+is_question([]) -> false;
+is_question(S)  -> lists:last(S) =:= $?.
+
+%% @doc Given a prompt, return `true' iff it is empty or only whitespace.
+-spec is_silent(prompt()) -> boolean().
+is_silent(S) -> re:replace(S, "\\s*", "", [{return, list}]) =:= [].
+
+%% @doc Given a character, return `true' iff it is an uppercase letter A-Z.
+-spec is_upper(character()) -> boolean().
+is_upper(C) when C >= $A, C =< $Z -> true;
+is_upper(_) -> false.
