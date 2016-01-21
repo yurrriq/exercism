8f0e47c6d04029323833a4a86b4d66072ebc016d
Eric Bailey <eric@ericb.me>
2016-01-21 12:18:28 -0600

    word-count
    http://exercism.io/submissions/88dbbe89e3dd4fbeb4a6a55dd90f56fc

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -5,7 +5,20 @@ defmodule Words do
   Words are compared case-insensitively.
   """
   @spec count(String.t) :: map()
-  def count(sentence) do
+  def count(sentence), do: Enum.reduce(words(sentence), %{}, &update_counter/2)
 
+  defp update_counter(word, counter) do
+    Dict.update(counter, String.downcase(word), 1, &inc/1)
   end
+
+  @non_separator ~r/[^\p{L}\d-]+/u
+
+  defp words(sentence) do
+    Enum.filter(Regex.split(@non_separator, sentence), &not_empty?/1)
+  end
+
+  defp not_empty?(""), do: false
+  defp not_empty?(_),  do: true
+
+  defp inc(x), do: x + 1
 end
