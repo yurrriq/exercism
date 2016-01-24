db8d81ed032559b98fc94976927ed1761f7f7ce0
Eric Bailey <eric@ericb.me>
2016-01-23 22:57:30 -0600

    hello-world
    http://exercism.io/submissions/2432d8c009bf4edb928fdcee60f0db65

--- hello-world/hello-world.el
+++ hello-world/hello-world.el
@@ -4,6 +4,9 @@
 
 ;;; Code:
 
+(defun hello (&optional name)
+  "Say hello, optionally to `NAME'."
+  (format "Hello, %s!" (or name "World")))
 
 (provide 'hello-world)
 ;;; hello-world.el ends here
