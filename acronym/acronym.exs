1a710e098b1af828dd5d51bf307d856da572a84d
Eric Bailey <eric@ericb.me>
2016-02-14 21:52:16 -0600

    acronym
    http://exercism.io/submissions/bbd898d2574f48eabbb09e21c58887c5

---
+++ acronym/acronym.exs
@@ -0,0 +1,25 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(String.t()) :: String.t()
+  def abbreviate(string) do
+    tokenize(string) |> Enum.map(&ffirst_capital/1) |> Enum.join
+  end
+
+  @doc """
+  Given a singleton string list, return the first letter of the string,
+  capitalized.
+  """
+  @spec ffirst_capital([String.t()]) :: String.t()
+  def ffirst_capital([string]), do: String.first(string) |> String.upcase
+
+  @doc """
+  Match letters starting with an uppercase one, or any letters,
+  splitting between a pair of adjacent lower and uppercase letters.
+  """
+  @spec tokenize(String.t()) :: [[String.t()]]
+  def tokenize(string), do: Regex.scan(~r/\p{Lu}+\p{Ll}*|\p{L}+/, string)
+end
+
