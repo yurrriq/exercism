445cc99343cbc2e59cd09bb2b8ca117ee4c9321e
Eric Bailey <eric@ericb.me>
2016-02-16 16:27:38 -0600

    Update acronym
    Pull out @word_or_camel.
    
    http://exercism.io/submissions/b0c748630291475a927ee1ee83fe9386

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -15,11 +15,12 @@ defmodule Acronym do
   @spec ffirst_capital([String.t()]) :: String.t()
   def ffirst_capital([string]), do: String.first(string) |> String.upcase
 
+  @word_or_camel ~r/\p{Lu}+\p{Ll}*|\p{L}+/u
+
   @doc """
   Match letters starting with an uppercase one, or any letters,
   splitting between a pair of adjacent lower and uppercase letters.
   """
   @spec tokenize(String.t()) :: [[String.t()]]
-  def tokenize(string), do: Regex.scan(~r/\p{Lu}+\p{Ll}*|\p{L}+/, string)
+  def tokenize(string), do: Regex.scan(@word_or_camel, string)
 end
-
