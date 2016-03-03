b358ed13e832398f0c0f2e3e881a5dec4ffae210
Eric Bailey <eric@ericb.me>
2016-03-03 01:23:23 -0600

    Implement list-ops
    http://exercism.io/submissions/b770d3dd28c6450985b3c97931b8f759
    
    Use continuation-passing style to avoid blowing the stack.

--- list-ops/list-ops.scm
+++ list-ops/list-ops.scm
@@ -6,3 +6,45 @@
             my-fold
             my-append
             my-concatenate))
+
+(define (my-length lst) (my-fold (lambda (_ acc) (1+ acc)) 0 lst))
+
+(define (my-reverse lst) (my-fold cons '() lst))
+
+(define (my-map proc lst) (map-cps proc lst identity))
+
+(define (my-filter pred lst) (filter pred lst))
+
+(define (my-fold proc init lst)
+  (if (null? lst) init
+      (my-fold proc (proc (car lst) init) (cdr lst))))
+
+(define (my-append lst1 lst2) (append-cps lst1 lst2 identity))
+
+(define (my-concatenate lol) (foldr-cps my-append '() lol))
+
+
+;;;===================================================================
+;;; Internal functions
+;;;===================================================================
+
+(define (append-cps lst1 lst2 k)
+  (if (null? lst1) (k lst2)
+      (append-cps (cdr lst1) lst2 (lambda (x) (k (cons (car lst1) x))))))
+
+(define (map-cps proc lst k)
+  (if (null? lst) (k '())
+      (map-cps proc (cdr lst) (lambda (x) (k (cons (proc (car lst)) x))))))
+
+(define (foldr-cps proc init lst)
+  (letrec ((foldr-cps* (lambda (lst2 k)
+                         (if (null? lst2) (k init)
+                             (foldr-cps* (cdr lst2)
+                                         (lambda (x)
+                                           (k (proc (car lst2) x))))))))
+    (foldr-cps* lst identity)))
+
+
+;; Local Variables:
+;; compile-command: "guile list-ops-test.scm"
+;; End:
