d3106433f8895a0922e7774ca6074b46a46533cc
Eric Bailey <eric@ericb.me>
2016-03-19 13:46:25 -0500

    Update anagram
    http://exercism.io/submissions/e6ec2b75894d49579ee757c3f9dffe60
    
    Use memoize, on and some-fn for brevity/clarity.
    
    Replace
    
        (transduce xform conj [] coll)
    
    with the lazy
    
        (sequence xform coll)
    
    http://blog.cognitect.com/blog/2014/8/6/transducers-are-coming

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -3,19 +3,24 @@
   {:author "Eric Bailey"}
   (:require [clojure.string :refer [lower-case]]))
 
+(def ^:private normalize (memoize lower-case))
+
+(def ^:private sort-norm (memoize (comp sort normalize)))
+
+;; http://stackoverflow.com/a/31755514/1793234
+(defn- on
+  ([f g]   (fn [x y] (f (g x) (g y))))
+  ([f g x] (partial (on f g) x)))
+
 (defn- anagram-for?
   "Given a word, return a transducer that removes candidates which are
   case-insensitive equal to or not an anagram of the given word."
   [word]
-  (let [word-lowered (lower-case word)
-        word-sorted  (sort word-lowered)]
-    (remove (fn [candidate]
-              (let [candidate-lowered (lower-case candidate)]
-                (or (= candidate-lowered word-lowered)
-                    (not= (sort candidate-lowered) word-sorted)))))))
+  (remove (some-fn (on    = normalize word)
+                   (on not= sort-norm word))))
 
 (defn anagrams-for
   "Given a word and a list of anagram candidates, return a sublist
   where each candidate is an anagram for the given word."
   [word candidates]
-  (transduce (anagram-for? word) conj [] candidates))
+  (sequence (anagram-for? word) candidates))
