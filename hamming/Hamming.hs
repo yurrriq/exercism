05e319af61a095dd121b9e11618d066c2a66fe0c
Eric Bailey <eric@ericb.me>
2016-04-13 20:11:01 -0500

    Submit haskell/hamming
    http://exercism.io/submissions/f02d91c3243948d3ba5f7f9045f6e737

---
+++ hamming/Hamming.hs
@@ -0,0 +1,19 @@
+{-|
+Module      : Hamming
+Copyright   : (c) Eric Bailey, 2016
+License     : MIT
+
+Maintainer  : Eric Bailey
+Stability   : experimental
+Portability : portable
+
+Computing the Hamming distance between two DNA strands.
+-}
+
+module Hamming (distance) where
+
+import           Data.Bool               (bool)
+import           Data.Function.Pointless ((.:))
+
+distance :: String -> String -> Int
+distance = foldr (bool id (+1)) 0 .: zipWith (/=)
