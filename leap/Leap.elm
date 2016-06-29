4c71d2842b8a952f7cff1d797f843c46c74a1d52
Eric Bailey <eric@ericb.me>
2016-06-29 05:01:37 -0500

    Add type signatures to Leap

--- leap/Leap.elm
+++ leap/Leap.elm
@@ -1,7 +1,9 @@
 module Leap exposing (isLeapYear)
 
+divides : Int -> Int -> Bool
 divides d n = n `rem` d == 0
 
+isLeapYear : Int -> Bool
 isLeapYear year =
   400 `divides` year ||
     (not (100 `divides` year) && 4 `divides` year)
