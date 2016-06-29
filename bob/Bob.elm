d00f10f6a7ab83eff029193e389af38cfea31dea
Eric Bailey <eric@ericb.me>
2016-06-29 06:24:18 -0500

    Clean up Bob
    http://exercism.io/submissions/dbe7ed4f5ff54afea7cd580060da9f1a

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,32 +1,50 @@
 module Bob exposing (hey)
 
-import Char   as C
-import String as S
+import Char   exposing (isLower, isUpper)
+import String exposing (any, endsWith, isEmpty, trim)
 
+{-| Get a lackadaisical teenager's response.
+
+    hey "What's up?"               == "Sure".
+    hey "That doesn't make sense." == "Whatever."
+-}
 hey : String -> String
-hey s =
-  let
-    isYelled : Bool
-    isYelled = (S.any C.isUpper s) && (s == S.toUpper s)
+hey prompt =
+  if      isYelled   prompt  then "Whoa, chill out!"
+  else if isSilent   prompt  then "Fine. Be that way!"
+  else if isQuestion prompt  then "Sure."
+  else                            "Whatever."
 
-    isSpace : Char -> Bool
-    isSpace c =
-      case c of
-        ' '  -> True
-        '\t' -> True
-        '\n' -> True
-        '\r' -> True
-        '\f' -> True
-        '\v' -> True
-        _    -> False
+{-| Determine if a string is empty or contains only whitespace.
 
-    isSilent : Bool
-    isSilent = S.all isSpace s
+    isSilent "  \t  " == True
+    isSilent "Hill"   == False
+    isSilent ""       == True
+-}
+isSilent : String -> Bool
+isSilent = isEmpty << trim
 
-    isQuestion : Bool
-    isQuestion = "?" == S.right 1 s
-  in
-    if      isYelled   then "Whoa, chill out!"
-    else if isSilent   then "Fine. Be that way!"
-    else if isQuestion then "Sure."
-    else                    "Whatever."
+{-| Determine if a string ends with a question mark.
+
+    isQuestion "Oh?" == True
+    isQuestion "No." == False
+-}
+isQuestion : String -> Bool
+isQuestion = endsWith "?"
+
+{-| Determine whether every letter is uppercase.
+
+    isYelled "HEY!"    == True
+    isYelled "H3Y."    == True
+    isYelled "VÄRFÖR?" == True
+-}
+isYelled : String -> Bool
+isYelled str = any isUpper str && notany isLower str
+
+{-| Determine if *no* character satisfies a predicate.
+
+    notany Char.isUpper "example" == True
+    notany Char.isUpper "LIES"    == False
+-}
+notany : (Char -> Bool) -> String -> Bool
+notany pred = not << any pred
