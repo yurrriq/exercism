230c27b8eb492b6d0b4e62213ddde102e89166f6
Eric Bailey <eric@ericb.me>
2017-07-21 02:18:33 -0500

    pascals-triangle: submit
    http://exercism.io/submissions/2d6d7a6a1af446e58d8c47cdfbd22d64

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,4 +1,40 @@
-module Triangle (rows) where
+{-# LANGUAGE ApplicativeDo #-}
+
+
+module Triangle
+  ( rows
+  ) where
+
+
+import Control.Applicative (ZipList(..))
+
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows n = take n triangle
+
+
+triangle :: [[Integer]]
+triangle = iterate go [1]
+  where
+    go :: [Integer] -> [Integer]
+    go xs =
+      getZipList $ do
+        x <- ZipList (0 : xs)
+        y <- ZipList (xs ++ [0])
+        return (x + y)
+
+
+{-
+
+
+{-# LANGUAGE ParallelListComp #-}
+
+
+triangle :: [[Integer]]
+triangle = iterate go [1]
+  where
+    go :: [Integer] -> [Integer]
+    go xs = [x + y | x <- 0 : xs | y <- xs ++ [0]]
+
+
+-}
