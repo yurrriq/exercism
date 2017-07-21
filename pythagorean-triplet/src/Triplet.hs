dca40d849dedd300b5dc208999728091b04ee55a
Eric Bailey <eric@ericb.me>
2017-07-21 03:50:20 -0500

    pythagorean-triplet: submit
    http://exercism.io/submissions/d677d9a3fc1d4371a13d5c479eab54b8

--- pythagorean-triplet/src/Triplet.hs
+++ pythagorean-triplet/src/Triplet.hs
@@ -1,10 +1,39 @@
-module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
+module Triplet
+  ( Triplet
+  , isPythagorean
+  , mkTriplet
+  , pythagoreanTriplets
+  ) where
 
-isPythagorean :: (Int, Int, Int) -> Bool
-isPythagorean triplet = error "You need to implement this function."
 
-mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
-mkTriplet a b c = error "You need to implement this function."
+import           Control.Monad (guard, join)
 
-pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
-pythagoreanTriplets minFactor maxFactor = error "You need to implement this function."
+
+newtype Triplet a =
+  Triplet (a, a, a)
+  deriving (Eq, Show)
+
+
+isPythagorean :: Triplet Int -> Bool
+isPythagorean (Triplet (a, b, c)) = square a + square b == square c
+
+
+mkTriplet :: Int -> Int -> Int -> Triplet Int
+mkTriplet a b c
+  | b < a = mkTriplet b a c
+  | c < b = mkTriplet a c b
+  | otherwise = Triplet (a, b, c)
+
+
+pythagoreanTriplets :: Int -> Int -> [Triplet Int]
+pythagoreanTriplets minFactor maxFactor =
+  do a <- [minFactor .. maxFactor]
+     b <- [a .. maxFactor]
+     c <- [b .. maxFactor]
+     let triplet = mkTriplet a b c
+     guard (isPythagorean triplet)
+     pure triplet
+
+
+square :: Integral a => a -> a
+square = join (*)
