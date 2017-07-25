961890caf650d7f05c16ee1d6fa0e341ceabbfcc
Eric Bailey <eric@ericb.me>
2017-07-25 04:14:42 -0500

    hello-world: submit
    http://exercism.io/submissions/f42d13ac789f4aa1bc433d919f5f2321

---
+++ hello-world/default.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+with nixpkgs;
+
+let
+
+  gems = callPackage ../. {};
+
+in
+
+stdenv.mkDerivation rec {
+  name = "hello-world-${version}";
+  version = "0.0.1";
+
+  buildInputs = [ gems ruby ];
+}
