99dc424a8c8c383c52cd4e13bcec8e85d8f989e6
Eric Bailey <eric@ericb.me>
2017-07-25 14:39:22 -0500

    rna-transcription: submit
    http://exercism.io/submissions/c24ae4b55d5a43629aec3148f13db078

--- rna-transcription/rna_transcription_test.rb
+++ rna-transcription/rna_transcription_test.rb
@@ -4,42 +4,34 @@ require_relative 'rna_transcription'
 # Common test data version: 1.0.0 0b20fff
 class RnaTranscriptionTest < Minitest::Test
   def test_rna_complement_of_cytosine_is_guanine
-    # skip
     assert_equal 'G', Complement.of_dna('C')
   end
 
   def test_rna_complement_of_guanine_is_cytosine
-    skip
     assert_equal 'C', Complement.of_dna('G')
   end
 
   def test_rna_complement_of_thymine_is_adenine
-    skip
     assert_equal 'A', Complement.of_dna('T')
   end
 
   def test_rna_complement_of_adenine_is_uracil
-    skip
     assert_equal 'U', Complement.of_dna('A')
   end
 
   def test_rna_complement
-    skip
     assert_equal 'UGCACCAGAAUU', Complement.of_dna('ACGTGGTCTTAA')
   end
 
   def test_dna_correctly_handles_invalid_input
-    skip
     assert_equal '', Complement.of_dna('U')
   end
 
   def test_dna_correctly_handles_completely_invalid_input
-    skip
     assert_equal '', Complement.of_dna('XXX')
   end
 
   def test_dna_correctly_handles_partially_invalid_input
-    skip
     assert_equal '', Complement.of_dna('ACGTXXXCTTAA')
   end
 
@@ -61,7 +53,6 @@ class RnaTranscriptionTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 4, BookKeeping::VERSION
   end
 end
