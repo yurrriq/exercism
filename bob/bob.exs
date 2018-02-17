a7cefb4dafcbbbb2e4537e06c3571855fe345c61
Eric Bailey <eric@ericb.me>
2018-02-17 02:31:00 -0600

    Squashed 'elixir/' content from commit 7bbc18d
    git-subtree-dir: elixir
    git-subtree-split: 7bbc18d275b251a94875069713d81f364a863fbc

---
+++ bob/bob.exs
@@ -0,0 +1,58 @@
+defmodule Prompt do
+  @moduledoc """
+  String predicates for `Teenager`.
+  """
+
+  @doc "Returns true if `input` ends with \"?\", otherwise false."
+  def question?(input) do
+    String.last(input) == "?"
+  end
+
+  @doc "Returns true if `input` is empty or only whitespace, otherwise false."
+  def silent?(input) do
+    String.strip(input) == ""
+  end
+
+  @doc """
+  Returns true if `input` contains at least one uppercase letter and all
+  letters are uppercase, otherwise false.
+  """
+  def yelled?(input) do
+    some_upper?(input) && String.upcase(input) == input
+  end
+
+  defp some_upper?(input) do
+    String.match?(input, ~r/\p{Lu}/u)
+  end
+end
+
+defmodule Teenager do
+  @moduledoc """
+  Responds as a teenager would.
+
+  ## Examples
+
+      iex> Teenager.hey("Teenagers are lackadaisical.")
+      "Whatever."
+
+      iex> Teenager.hey("Are you lackadaisical?")
+      "Sure."
+
+      iex> Teenager.hey("STOP BEING LACKADAISICAL!.")
+      "Whoa, chill out!"
+
+      iex> Teenager.hey("   ")
+      "Fine. Be that way!"
+  """
+
+  @doc "Given some input, returns a lackadaisical response."
+  def hey(input) do
+    import Prompt
+    cond do
+      question?(input) -> "Sure."
+      silent?(input)   -> "Fine. Be that way!"
+      yelled?(input)   -> "Whoa, chill out!"
+      true             -> "Whatever."
+    end
+  end
+end
---
+++ word-count/word_count.exs
@@ -0,0 +1,19 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map()
+  def count(sentence), do: words(sentence) |> Enum.reduce(%{}, &inc_counter/2)
+
+  @non_separator ~r/[^\p{L}\d-]+/u
+
+  defp words(sentence), do: Regex.split(@non_separator, sentence, trim: true)
+
+  defp inc_counter(key, dict) do
+    Dict.update(dict, String.downcase(key), 1, &inc/1)
+  end
+
+  defp inc(x), do: x + 1
+end
