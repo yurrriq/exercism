c2b27725433f1bf2aaf92a22cc3cadfe4edeca4f
Eric Bailey <eric@ericb.me>
2018-09-29 12:14:15 -0500

    [ocaml] Add initial Nix setup

---
+++ ocaml/default.nix
@@ -0,0 +1,30 @@
+let
+
+  fetchTarballFromGitHub =
+    { owner, repo, rev, sha256, ... }:
+    builtins.fetchTarball {
+      url = "https://github.com/${owner}/${repo}/tarball/${rev}";
+      inherit sha256;
+    };
+
+  fromJSONFile = f: builtins.fromJSON (builtins.readFile f);
+
+in
+
+with import (fetchTarballFromGitHub (fromJSONFile ./nixpkgs-src.json)) {};
+
+stdenv.mkDerivation {
+  name = "exercism-ocaml-env";
+  buildInputs = with pkgs; ([
+    exercism
+    pkgconfig
+  ] ++ (with ocamlPackages; [
+    base
+    core
+    dune
+    findlib
+    ocaml
+    ounit
+    utop
+  ]));
+}
