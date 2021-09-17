cd1cdfcd4eff933854f3f682841f4bb4b3c41bcf
Eric Bailey <eric@ericb.me>
2021-09-17 03:03:57 -0500

    [rust] assembly-line: submit

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,11 +1,13 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let success_rate: f64 = match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.90,
+        9..=10 => 0.77,
+        _ => panic!("Invalid speed: {}", speed),
+    };
+    (speed as f64) * 221.0 * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
