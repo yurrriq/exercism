457bcd8ca4d92fb69bd4aea5ae63a5b87526f8a2
Eric Bailey <eric@ericb.me>
2021-09-17 17:13:45 -0500

    [rust] low-power-embedded-game: tweak implementation

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     let quotient = dividend / divisor;
-    (quotient, dividend - (quotient * divisor))
+    (quotient, dividend - quotient * divisor)
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate()
-        .filter_map(|(i, x)| if i % 2 == 0 { Some(x) } else { None })
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
 
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        self.0.abs() + self.1.abs()
+        let Position(x, y) = self;
+        x.abs() + y.abs()
     }
 }
