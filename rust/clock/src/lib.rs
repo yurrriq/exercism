05dc1dba0e246e25ba0771735eb8a96cf46179f6
Eric Bailey <eric@ericb.me>
2021-09-18 01:59:25 -0500

    [rust] clock: submit

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,42 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl ToString for Clock {
+    fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Self {
+            hours: 0,
+            minutes: 0,
+        }
+        .add_minutes(hours * 60 + minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let new_time = self.hours * 60 + self.minutes + minutes;
+
+        let mut new_hours = (new_time / 60) % 24;
+        let mut new_minutes = new_time % 60;
+
+        while new_minutes < 0 {
+            new_hours -= 1;
+            new_minutes += 60;
+        }
+
+        while new_hours < 0 {
+            new_hours += 24;
+        }
+
+        Self {
+            hours: new_hours,
+            minutes: new_minutes,
+        }
     }
 }
