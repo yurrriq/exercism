5d2eb386fc578c8773fd7bed536d21947fcc87cd
Eric Bailey <eric@ericb.me>
2021-09-18 00:38:40 -0500

    [rust] reverse-string: properly handle graphemes

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect::<String>()
+    input.graphemes(true).rev().collect()
 }
