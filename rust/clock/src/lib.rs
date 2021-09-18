9dfa60fd96fe9e14b95deab14afc08fa8f9b5093
Eric Bailey <eric@ericb.me>
2021-09-18 02:15:07 -0500

    [rust] clock: Euclid ftw

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -4,39 +4,30 @@ pub struct Clock {
     minutes: i32,
 }
 
-impl ToString for Clock {
-    fn to_string(&self) -> String {
-        format!("{:02}:{:02}", self.hours, self.minutes)
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Self::default().add_minutes(hours * 60 + minutes)
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Self {
+            hours: (self.hours + (self.minutes + minutes).div_euclid(60)).rem_euclid(24),
+            minutes: (self.minutes + minutes).rem_euclid(60),
+        }
     }
 }
 
-impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
+impl Default for Clock {
+    fn default() -> Self {
         Self {
             hours: 0,
             minutes: 0,
         }
-        .add_minutes(hours * 60 + minutes)
     }
+}
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        let new_time = self.hours * 60 + self.minutes + minutes;
-
-        let mut new_hours = (new_time / 60) % 24;
-        let mut new_minutes = new_time % 60;
-
-        while new_minutes < 0 {
-            new_hours -= 1;
-            new_minutes += 60;
-        }
-
-        while new_hours < 0 {
-            new_hours += 24;
-        }
-
-        Self {
-            hours: new_hours,
-            minutes: new_minutes,
-        }
+impl ToString for Clock {
+    fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
     }
 }
