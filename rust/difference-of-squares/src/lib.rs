8b7d971b4bef3e3e1bd61f5bfa0049b2110d3b10
Eric Bailey <eric@ericb.me>
2021-09-23 23:40:15 -0500

    [rust] difference-of-squares: submit, using closed formulae

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+pub fn square_of_sum(n : u32) -> u32 {
+    ((n * (n + 1)) / 2).pow(2)
 }
 
-pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+pub fn sum_of_squares(n : u32) -> u32 {
+    (n * (n + 1) * (2 * n + 1)) / 6
 }
 
-pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+pub fn difference(n : u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
 }
