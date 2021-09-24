650e089f0821d4437cf8d5cb21585918a3cf12c7
Eric Bailey <eric@ericb.me>
2021-09-23 23:06:19 -0500

    [rust] luhn: submit messy solution

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,27 @@
 /// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+pub fn is_valid(code : &str) -> bool {
+    let code = code.chars().filter(|&c| c != ' ');
+
+    if code.clone().collect::<String>() == "0" {
+        return false;
+    }
+
+    let mut sum = 0;
+
+    for (i, c) in code.rev().enumerate() {
+        if let Some(mut n) = c.to_digit(10) {
+            if i % 2 != 0 {
+                n *= 2;
+                if n > 9 {
+                    n -= 9;
+                }
+            }
+
+            sum += n;
+        } else {
+            return false;
+        }
+    }
+
+    sum % 10 == 0
 }
