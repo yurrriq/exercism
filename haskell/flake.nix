fcb7c63c2b5692f3703c1fb9f48b48ad0b4a61f5
Eric Bailey <eric@ericb.me>
2021-11-01 23:40:40 -0500

    [haskell] Start setting up Nix environment

---
+++ haskell/flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Solutions to Haskell exercises from exercism.io";
+
+  inputs = {
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
+  };
+
+  outputs = { self, emacs-overlay, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        overlays = [
+          emacs-overlay.overlay
+          self.overlay
+        ];
+        system = "x86_64-linux";
+      };
+    in
+    {
+      overlay = final: prev: {
+        haskellPackages = prev.haskellPackages.override {
+          overrides = hfinal: hprev: {
+            digits = hprev.callCabal2nix "digits"
+              (prev.fetchFromGitHub {
+                owner = "yurrriq";
+                repo = "digits";
+                rev = "c3a2c2bacc4a2e2c51beefa2fdb90da9a5bddf6b";
+                hash = "sha256-/n2gf33zShj6LexHRplp975teCZyLAsg0rmXK9AHoK0=";
+              })
+              { };
+          };
+        };
+
+        myEmacs = prev.emacsWithPackagesFromUsePackage {
+          alwaysEnsure = true;
+          config = ./emacs.el;
+        };
+      };
+
+      devShell.x86_64-linux = with pkgs; mkShell {
+        FONTCONFIG_FILE = makeFontsConf {
+          fontDirectories = [
+            (nerdfonts.override { fonts = [ "Iosevka" ]; })
+          ];
+        };
+        buildInputs = [
+          cabal-install
+          direnv
+          exercism
+          ghc
+          ghcid
+          haskell-language-server
+          haskellPackages.cabal-plan
+          haskellPackages.hpack
+          haskellPackages.hlint
+          haskellPackages.ormolu
+          haskellPackages.pointfree
+          myEmacs
+          nix-direnv
+          rnix-lsp
+        ];
+      };
+    };
+}
