8a97b859eff622c835d95caf5a468b519ac91fc9
Eric Bailey <eric@ericb.me>
2022-02-26 23:30:55 -0600

    [go] Configure initial dev environment

---
+++ go/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Solutions to Go exercises from exercism.io";
+
+  inputs = {
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+  };
+
+  outputs = { self, emacs-overlay, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        overlays = [
+          emacs-overlay.overlay
+          self.overlay
+        ];
+        system = "x86_64-linux";
+      };
+    in
+    {
+      overlay = final: prev: {
+        myEmacs = prev.emacsWithPackagesFromUsePackage {
+          alwaysEnsure = true;
+          config = ./emacs.el;
+        };
+      };
+
+      devShell.x86_64-linux = with pkgs; mkShell {
+        FONTCONFIG_FILE = makeFontsConf {
+          fontDirectories = [
+            (nerdfonts.override { fonts = [ "Iosevka" ]; })
+          ];
+        };
+        buildInputs = [
+          exercism
+          go
+          go-tools
+          gopls
+          myEmacs
+          rnix-lsp
+        ];
+      };
+    };
+}
