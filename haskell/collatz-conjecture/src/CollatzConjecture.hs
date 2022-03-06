80652c2f4f2f124538ceb78a8f92d4a9cb076687
Eric Bailey <eric@ericb.me>
2022-03-05 23:47:34 -0600

    [haskell] collatz-conjecture: compute the sequence too

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,6 +1,5 @@
 -- |
 -- Module      : CollatzConjecture
--- Description : ...
 -- Copyright   : (c) Eric Bailey, 2022
 -- License     : MIT
 -- Maintainer  : eric@ericb.me
@@ -11,11 +10,17 @@ module CollatzConjecture
   )
 where
 
+-- | Compute the number of steps required to reach 1.
 collatz :: Integer -> Maybe Integer
-collatz = go 0
-  where
-    go steps n
-      | n <= 0 = Nothing
-      | n == 1 = Just steps
-      | even n = go (steps + 1) (n `div` 2)
-      | otherwise = go (steps + 1) (3 * n + 1)
+collatz n
+  | n < 1 = Nothing
+  | otherwise = Just (fromIntegral (length (collatzSeq n)))
+
+collatzSeq :: Integer -> [Integer]
+collatzSeq = takeWhile (/= 1) . iterate collatzIter
+
+collatzIter :: Integer -> Integer
+collatzIter 1 = 1
+collatzIter n
+  | even n = n `div` 2
+  | otherwise = 3 * n + 1
