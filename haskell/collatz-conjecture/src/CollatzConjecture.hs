432c9284871748fbd350381b443d485bfed3e94f
Eric Bailey <eric@ericb.me>
2022-03-05 23:20:13 -0600

    [haskell] collatz-conjecture: submit

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,21 @@
-module CollatzConjecture (collatz) where
+-- |
+-- Module      : CollatzConjecture
+-- Description : ...
+-- Copyright   : (c) Eric Bailey, 2022
+-- License     : MIT
+-- Maintainer  : eric@ericb.me
+-- Stability   : experimental
+-- Portability : portable
+module CollatzConjecture
+  ( collatz,
+  )
+where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz = go 0
+  where
+    go steps n
+      | n <= 0 = Nothing
+      | n == 1 = Just steps
+      | even n = go (steps + 1) (n `div` 2)
+      | otherwise = go (steps + 1) (3 * n + 1)
