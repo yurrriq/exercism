bd9c456fe54811e6013af8492fa27530823f3d11
Eric Bailey <eric@ericb.me>
2022-03-06 00:08:39 -0600

    [haskell] nth-prime: submit

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,16 @@
-module Prime (nth) where
+module Prime
+  ( nth,
+  )
+where
 
+import Data.List (nubBy)
+
+-- | Compute the nth prime.
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth n
+  | n < 1 = Nothing
+  | otherwise = Just (primes !! pred n)
+
+-- | Naively compute an infinite list of primes.
+primes :: [Integer]
+primes = nubBy (((> 1) .) . gcd) [2 ..]
