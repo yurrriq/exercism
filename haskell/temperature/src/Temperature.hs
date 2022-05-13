ed62131705e3338d0fabe8ea72d93ad40d648479
Eric Bailey <eric@ericb.me>
2022-05-13 02:27:30 -0500

    [haskell] temperature: submit

--- haskell/temperature/src/Temperature.hs
+++ haskell/temperature/src/Temperature.hs
@@ -1,14 +1,29 @@
-module Temperature (tempToC, tempToF) where
+-- |
+-- Module      : Temperature
+-- Description : Convert temperatures between Celsius and Fahrenheit.
+-- Copyright   : (c) Eric Bailey, 2022
+-- License     : MIT
+-- Maintainer  : eric@ericb.me
+-- Stability   : experimental
+-- Portability : portable
+module Temperature
+  ( tempToC,
+    tempToF,
+  )
+where
 
-{- Implement the function `tempToC` to convert
-`  Fahrenheit to Celsius                    -}
+-- | Convert a temperature in Fahrenheit to its equivalent in Celsius.
+--
+-- >>> tempToC 32
+-- 0.0
+tempToC :: (Integral a, RealFrac b) => a -> b
+tempToC temp = (fromIntegral temp - 32) / 1.8
+{-# SPECIALIZE INLINE tempToC :: Integer -> Float #-}
 
-tempToC :: Integer -> Float
-tempToC temp = error "Implement this function."
-
-
-{- Implement the function `tempToF` to convert
-`  Celsius to Fahrenheit                    -}
-
-tempToF :: Float -> Integer
-tempToF temp = error "Implement this function."
+-- | Convert a temperature in Celsius to its equivalent in Fahrenheit.
+--
+-- >>> tempToF 4
+-- 40
+tempToF :: (Integral a, RealFrac b) => b -> a
+tempToF temp = ceiling (temp * 1.8 + 32)
+{-# SPECIALIZE INLINE tempToF :: Float -> Integer #-}
