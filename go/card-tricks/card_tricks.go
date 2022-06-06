6084bc2f20d4aca5fa451c3ef7b54db9b57b5e80
Eric Bailey <eric@ericb.me>
2022-06-05 23:27:51 -0500

    [go] card-tricks: submit

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -1,28 +1,43 @@
 package cards
 
-// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+// Return a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	return []int{2, 6, 9}
 }
 
-// GetItem retrieves an item from a slice at given position.
-// If the index is out of range, we want it to return -1.
+// Retrieve an item from a slice at given position.
+// If the index is out of range, return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	if index < 0 || index >= len(slice) {
+		return -1
+	} else {
+		return slice[index]
+	}
 }
 
-// SetItem writes an item to a slice at given position overwriting an existing value.
+// Write an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	if index < 0 || index >= len(slice) {
+		return append(slice, value)
+	} else {
+		slice[index] = value
+		return slice
+	}
 }
 
-// PrependItems adds an arbitrary number of values at the front of a slice.
+// Add an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, value ...int) []int {
-	panic("Please implement the PrependItems function")
+	return append(value, slice...)
 }
 
-// RemoveItem removes an item from a slice by modifying the existing slice.
+// Remove an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if index < 0 || index >= len(slice) {
+		return slice
+	} else if index == 0 {
+		return slice[1:]
+	} else {
+		return append(slice[:index], slice[index+1:]...)
+	}
 }
