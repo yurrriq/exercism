42354b453156d8189c02635113149276d0ad772e
Eric Bailey <eric@ericb.me>
2022-06-05 21:59:00 -0500

    [go] vehicle-purchase: submit

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -1,16 +1,35 @@
 package purchase
 
-// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+import "fmt"
+
+// Determine whether a license is needed to drive a given kind of vehicle.
+// Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	return kind == "car" || kind == "truck"
 }
 
-// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+// Recommend a vehicle for selection.
+// Always recommend the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	var choice string
+	if option1 < option2 {
+		choice = option1
+	} else {
+		choice = option2
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", choice)
 }
 
-// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+// Calculate how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	var currentValue float64
+	if age < 3 {
+		currentValue = 0.8
+	} else if age < 10 {
+		currentValue = 0.7
+	} else {
+		currentValue = 0.5
+	}
+
+	return originalPrice * currentValue
 }
