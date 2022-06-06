6a2e6e5176b895a1d31048833648b9c64a7fc4d6
Eric Bailey <eric@ericb.me>
2022-06-05 22:40:52 -0500

    [go] need-for-speed: submit

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -1,26 +1,38 @@
 package speed
 
-// TODO: define the 'Car' type struct
-
-// NewCar creates a new remote controlled car with full battery and given specifications.
-func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+type Car struct {
+	battery      int
+	batteryDrain int
+	distance     int
+	speed        int
 }
 
-// TODO: define the 'Track' type struct
+// Create a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{battery: 100, batteryDrain: batteryDrain, distance: 0, speed: speed}
+}
+
+type Track struct {
+	distance int
+}
 
 // NewTrack created a new track
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+	return Track{distance}
 }
 
-// Drive drives the car one time. If there is not enough battery to drive on more time,
-// the car will not move.
+// Drive the car one time.
+// If there is not enough battery to drive on more time, the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.battery > car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
 }
 
-// CanFinish checks if a car is able to finish a certain track.
+// Check if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+	segments := (track.distance - car.distance) / car.speed
+	return car.battery-(car.batteryDrain*segments) > 0
 }
