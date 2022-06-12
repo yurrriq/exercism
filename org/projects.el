708bb0c2eed7aa4bbd7fe156d928435312aa0260
Eric Bailey <eric@ericb.me>
2022-06-12 02:09:51 -0500

    Start updating Org mode config

--- org/projects.el
+++ org/projects.el
@@ -1,10 +1,10 @@
 (add-to-list
  'org-publish-project-alist
  '("exercism"
-   :base-directory "~/src/yurrriq/exercism/"
+   :base-directory "~/src/github.com/yurrriq/exercism/"
    :base-extension "org"
-   :publishing-directory "~/src/yurrriq/exercism/_site/"
-   :exclude "\\(README\\|setup\\).org"
+   :publishing-directory "~/src/github.com/yurrriq/exercism/_site/"
+   :exclude "\\(README.org\\|setup.org\\|.direnv\\)"
    :recursive t
    :auto-sitemap t
    :sitemap-filename "index.org"
@@ -14,16 +14,16 @@
    :table-of-contents t
    :section-numbers nil
    :html-doctype "html5"
-   :html-postamble "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script>"
+   :html-postamble "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js\"></script>"
    :style-include-default nil
    :publishing-function org-html-publish-to-html))
 
 (add-to-list
  'org-publish-project-alist
  '("exercism-site"
-   :base-directory "~/src/yurrriq/exercism/"
+   :base-directory "~/src/github.com/yurrriq/exercism/"
    :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf"
-   :publishing-directory "~/src/yurrriq/exercism/_site/"
+   :publishing-directory "~/src/github.com/yurrriq/exercism/_site/"
    :recursive t
    :publishing-function org-publish-attachment
    :components ("exercism")))
