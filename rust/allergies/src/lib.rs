56492a54aff56967a48e7970185661a8d30a0261
Eric Bailey <eric@ericb.me>
2022-08-06 15:39:24 -0500

    [rust] allergies: submit

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,33 +1,42 @@
-pub struct Allergies;
+extern crate int_enum;
+use int_enum::IntEnum;
 
-#[derive(Debug, PartialEq, Eq)]
+extern crate enum_iterator;
+use enum_iterator::IntoEnumIterator;
+
+pub struct Allergies {
+    score: u32,
+}
+
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, PartialEq, IntEnum, IntoEnumIterator)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
 }
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        test_bit(self.score, *allergen as u32)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Allergen::into_enum_iter()
+            .filter(|allergen| self.is_allergic_to(allergen))
+            .collect()
     }
+}
+
+fn test_bit(n: u32, k: u32) -> bool {
+    n & k != 0
 }
