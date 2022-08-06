7c00ba1b10273f1359a88b8bdee9ccea0acce356
Eric Bailey <eric@ericb.me>
2022-08-06 14:10:33 -0500

    [rust] armstrong-numbers: drop unnecessary variable

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -9,10 +9,9 @@ fn is_armstrong_number_base(num: u32, base: u32) -> bool {
         return true
     }
 
-    let string = num.to_string();
     let k = (num as f32).log(base as f32).floor() as u32 + 1;
     // let k = num.log(base) + 1;
-    let sum = string.chars().fold(0, |sum, character| {
+    let sum = num.to_string().chars().fold(0, |sum, character| {
         sum + character.to_digit(base).expect("Invalid digit!").pow(k)
     });
 
