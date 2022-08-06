ac521070700f431036b678c9b1b5ece5ab4c1032
Eric Bailey <eric@ericb.me>
2022-08-06 13:12:26 -0500

    [rust] armstrong-numbers: submit

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let string = num.to_string();
+    let k = string.len() as u32;
+    let sum = string.chars().fold(0, |sum, character| {
+        sum + character.to_digit(10).expect("Invalid digit!").pow(k)
+    });
+    sum == num
 }
