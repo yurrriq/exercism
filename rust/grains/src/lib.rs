6840a3995a3fcd161599105b1f18b10d9ac42efc
Eric Bailey <eric@ericb.me>
2022-08-07 15:13:26 -0500

    [rust] grains: submit again

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -2,7 +2,11 @@ const MAX_SQUARE: u32 = 64;
 const ALL_SQUARES: std::ops::RangeInclusive<u32> = 1..=MAX_SQUARE;
 
 pub fn square(s: u32) -> u64 {
-    assert!(ALL_SQUARES.contains(&s), "Square must be between 1 and {}", MAX_SQUARE);
+    assert!(
+        ALL_SQUARES.contains(&s),
+        "Square must be between 1 and {}",
+        MAX_SQUARE
+    );
     1 << (s - 1)
 }
 
