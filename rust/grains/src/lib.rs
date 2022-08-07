111bd389aebf222d39409ee2f7cc60b77a464858
Eric Bailey <eric@ericb.me>
2022-08-07 11:16:02 -0500

    [rust] grains: submit again

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,12 +1,11 @@
-pub fn square(s: u32) -> u64 {
-    assert!((1..=64).contains(&s), "Square must be between 1 and 64");
+const MAX_SQUARE: u32 = 64;
+const ALL_SQUARES: std::ops::RangeInclusive<u32> = 1..=MAX_SQUARE;
 
-    match s {
-        1 => 1,
-        s => 2_u64 * square(s - 1),
-    }
+pub fn square(s: u32) -> u64 {
+    assert!(ALL_SQUARES.contains(&s), "Square must be between 1 and {}", MAX_SQUARE);
+    1 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    (1..=64).map(square).sum()
+    ALL_SQUARES.map(square).sum()
 }
