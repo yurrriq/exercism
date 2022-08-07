39b2eefcf5c9b7f9bf28350c67596ceb0ed0cbd8
Eric Bailey <eric@ericb.me>
2022-08-07 11:01:31 -0500

    [rust] grains: submit

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    assert!((1..=64).contains(&s), "Square must be between 1 and 64");
+
+    match s {
+        1 => 1,
+        s => 2_u64 * square(s - 1),
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).map(square).sum()
 }
