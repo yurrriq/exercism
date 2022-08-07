41d49fc6c8dcdd62d6aac127531cc9b8675a55b4
Eric Bailey <eric@ericb.me>
2022-08-06 22:47:34 -0500

    [rust] beer-song: submit messy first attempt

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,54 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    format!(
+        "{}, {}.\n{}.\n",
+        match n {
+            0 => on_the_wall(&of_beer("No more bottles")),
+            n => on_the_wall(&of_beer(&bottles(n))),
+        },
+        of_beer(&bottles(n)),
+        match n {
+            0 => {
+                format!(
+                    "Go to the store and buy some more, {}",
+                    on_the_wall(&of_beer(&bottles(99)))
+                )
+            }
+            1 => {
+                format!(
+                    "Take it down and pass it around, {}",
+                    on_the_wall(&of_beer(&bottles(n - 1)))
+                )
+            }
+            n => {
+                format!(
+                    "Take one down and pass it around, {}",
+                    on_the_wall(&of_beer(&bottles(n - 1)))
+                )
+            }
+        }
+    )
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..start + 1)
+        .rev()
+        .map(&verse)
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+fn bottles(n: u32) -> String {
+    match n {
+        0 => String::from("no more bottles"),
+        1 => String::from("1 bottle"),
+        n => format!("{} bottles", n),
+    }
+}
+
+fn of_beer(suffix: &str) -> String {
+    format!("{} of beer", suffix)
+}
+
+fn on_the_wall(prefix: &str) -> String {
+    format!("{} on the wall", prefix)
 }
