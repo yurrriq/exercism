5282cbce1c05589c64fb187b67b64fa5c4c0d632
Eric Bailey <eric@ericb.me>
2022-08-07 15:23:39 -0500

    [rust] leap: submit with num-traits

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,12 +1,19 @@
-pub trait Divisible {
-    fn divides(self, numerator: Self) -> bool;
-}
+extern crate num_traits;
 
-impl Divisible for u64 {
-    fn divides(self, numerator: Self) -> bool {
-        numerator % self == 0
+use num_traits::Zero;
+
+use std::ops::Rem;
+
+pub trait Divisible: Rem + Sized {
+    fn divides(self, numerator: Self) -> bool
+    where
+        <Self as Rem>::Output: Zero,
+    {
+        (numerator % self).is_zero()
     }
 }
+
+impl Divisible for u64 {}
 
 pub fn is_leap_year(year: u64) -> bool {
     400.divides(year) || (4.divides(year) && !100.divides(year))
