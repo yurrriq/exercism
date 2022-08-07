d5e3811c3e5cb0b87da7b2dd4e1ab7f2c940d43a
Eric Bailey <eric@ericb.me>
2022-08-06 23:05:18 -0500

    [rust] beer-song: submit again

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,54 +1,33 @@
 pub fn verse(n: u32) -> String {
     format!(
-        "{}, {}.\n{}.\n",
-        match n {
-            0 => on_the_wall(&of_beer("No more bottles")),
-            n => on_the_wall(&of_beer(&bottles(n))),
-        },
-        of_beer(&bottles(n)),
-        match n {
-            0 => {
-                format!(
-                    "Go to the store and buy some more, {}",
-                    on_the_wall(&of_beer(&bottles(99)))
-                )
-            }
-            1 => {
-                format!(
-                    "Take it down and pass it around, {}",
-                    on_the_wall(&of_beer(&bottles(n - 1)))
-                )
-            }
-            n => {
-                format!(
-                    "Take one down and pass it around, {}",
-                    on_the_wall(&of_beer(&bottles(n - 1)))
-                )
-            }
-        }
+        "{} on the wall, {}.\n{}, {} on the wall.\n",
+        bottles(n),
+        bottles(n).to_lowercase(),
+        pass_or_buy(n),
+        bottles((100 + n - 1) % 100).to_lowercase()
     )
 }
 
 pub fn sing(start: u32, end: u32) -> String {
     (end..start + 1)
         .rev()
-        .map(&verse)
+        .map(verse)
         .collect::<Vec<String>>()
         .join("\n")
 }
 
 fn bottles(n: u32) -> String {
     match n {
-        0 => String::from("no more bottles"),
-        1 => String::from("1 bottle"),
-        n => format!("{} bottles", n),
+        0 => String::from("No more bottles of beer"),
+        1 => String::from("1 bottle of beer"),
+        n => format!("{} bottles of beer", n),
     }
 }
 
-fn of_beer(suffix: &str) -> String {
-    format!("{} of beer", suffix)
-}
-
-fn on_the_wall(prefix: &str) -> String {
-    format!("{} on the wall", prefix)
+fn pass_or_buy(n: u32) -> &'static str {
+    match n {
+        0 => "Go to the store and buy some more",
+        1 => "Take it down and pass it around",
+        _ => "Take one down and pass it around",
+    }
 }
