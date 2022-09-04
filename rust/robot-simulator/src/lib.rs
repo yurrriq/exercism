f46af6e7fc4bc0676e0569e123a532aa67d02157
Eric Bailey <eric@ericb.me>
2022-09-04 23:25:57 +0200

    [rust] robot-simulator: refactor

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -2,9 +2,6 @@ extern crate int_enum;
 
 use int_enum::IntEnum;
 
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[repr(usize)]
 #[derive(PartialEq, Eq, Debug, Clone, Copy, IntEnum)]
 pub enum Direction {
@@ -15,50 +12,61 @@ pub enum Direction {
 }
 
 pub struct Robot {
-    x : i32,
-    y : i32,
-    d : Direction,
+    x: i32,
+    y: i32,
+    direction: Direction,
 }
 
 impl Robot {
-    pub fn new(x : i32, y : i32, d : Direction) -> Self {
-        Robot { x, y, d }
+    pub fn new(x: i32, y: i32, direction: Direction) -> Self {
+        Robot { x, y, direction }
     }
 
-    #[must_use]
-    pub fn turn_right(mut self) -> Self {
-        self.d = Direction::from_int((self.d.int_value() + 1) % 4).unwrap();
-        self
+    pub fn turn_right(self) -> Self {
+        Self {
+            direction: Direction::from_int((self.direction.int_value() + 1) % 4).unwrap(),
+            ..self
+        }
     }
 
-    #[must_use]
-    pub fn turn_left(mut self) -> Self {
-        self.d = Direction::from_int((self.d.int_value() + 3) % 4).unwrap();
-        self
+    pub fn turn_left(self) -> Self {
+        Self {
+            direction: Direction::from_int((self.direction.int_value() + 3) % 4).unwrap(),
+            ..self
+        }
     }
 
-    #[must_use]
-    pub fn advance(mut self) -> Self {
-        match self.d {
-            Direction::North => self.y += 1,
-            Direction::East => self.x += 1,
-            Direction::South => self.y -= 1,
-            Direction::West => self.x -= 1,
-        };
-        self
+    pub fn advance(self) -> Self {
+        use Direction::*;
+        match self.direction {
+            North => Self {
+                y: self.y + 1,
+                ..self
+            },
+            East => Self {
+                x: self.x + 1,
+                ..self
+            },
+            South => Self {
+                y: self.y - 1,
+                ..self
+            },
+            West => Self {
+                x: self.x - 1,
+                ..self
+            },
+        }
     }
 
-    #[must_use]
-    pub fn instructions(self, instructions : &str) -> Self {
-        instructions.chars().fold(
-            self,
-            |robot, instruction| match instruction {
+    pub fn instructions(self, instructions: &str) -> Self {
+        instructions
+            .chars()
+            .fold(self, |robot, instruction| match instruction {
                 'A' => robot.advance(),
                 'L' => robot.turn_left(),
                 'R' => robot.turn_right(),
                 _ => unimplemented!("Unknown instruction"),
-            },
-        )
+            })
     }
 
     pub fn position(&self) -> (i32, i32) {
@@ -66,6 +74,6 @@ impl Robot {
     }
 
     pub fn direction(&self) -> &Direction {
-        &self.d
+        &self.direction
     }
 }
