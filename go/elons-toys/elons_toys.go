cb3225b401c4641c718e4d2695b999641db7af13
Eric Bailey <eric@ericb.me>
2022-09-07 22:02:19 +0200

    [go] elons-toys: submit

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,9 +1,24 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+func (car *Car) Drive() {
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+}
 
-// TODO: define the 'DisplayBattery() string' method
+func (car *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (car *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", car.battery)
+}
+
+// Check if a car is able to finish a certain track.
+func (car *Car) CanFinish(trackDistance int) bool {
+	segments := (trackDistance - car.distance) / car.speed
+	return car.battery-(car.batteryDrain*segments) >= 0
+}
