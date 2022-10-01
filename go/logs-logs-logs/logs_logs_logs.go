37be00e1a5079908c329e486cb0f2fef3a663f3c
Eric Bailey <eric@ericb.me>
2022-09-30 19:29:07 -0500

    [go] logs-logs-logs: submit

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,45 @@
 package logs
 
+var knownApplications = map[rune]string{
+	'❗': "recommendation",
+	'🔍': "search",
+	'☀': "weather",
+}
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, roon := range log {
+		if application, isKnown := knownApplications[roon]; isKnown {
+			return application
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := make([]rune, 0, len(log))
+	for _, roon := range log {
+		if roon == oldRune {
+			newLog = append(newLog, newRune)
+		} else {
+			newLog = append(newLog, roon)
+		}
+	}
+
+	return string(newLog)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	for i, _ := range log {
+		if i+1 > limit {
+			return false
+		}
+	}
+
+	return true
 }
