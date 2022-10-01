70d24f2feaddfa4a5544d2fd3a1735c0ccb9fc1d
Eric Bailey <eric@ericb.me>
2022-09-30 20:17:08 -0500

    [go] expenses: submit

--- go/expenses/expenses.go
+++ go/expenses/expenses.go
@@ -1,5 +1,7 @@
 package expenses
 
+import "fmt"
+
 // Record represents an expense record.
 type Record struct {
 	Day      int
@@ -15,32 +17,54 @@ type DaysPeriod struct {
 
 // Filter returns the records for which the predicate function returns true.
 func Filter(in []Record, predicate func(Record) bool) []Record {
-	panic("Please implement the Filter function")
+	out := make([]Record, 0, len(in))
+	for _, record := range in {
+		if predicate(record) {
+			out = append(out, record)
+		}
+	}
+
+	return out
 }
 
-// ByDaysPeriod returns predicate function that returns true when
-// the day of the record is inside the period of day and false otherwise.
-func ByDaysPeriod(p DaysPeriod) func(Record) bool {
-	panic("Please implement the ByDaysPeriod function")
+// ByDaysPeriod returns a predicate function that returns true when the day of
+// the record is within the given period and false otherwise.
+func ByDaysPeriod(period DaysPeriod) func(Record) bool {
+	return func(record Record) bool {
+		return record.Day >= period.From && record.Day <= period.To
+	}
 }
 
-// ByCategory returns predicate function that returns true when
-// the category of the record is the same as the provided category
-// and false otherwise.
-func ByCategory(c string) func(Record) bool {
-	panic("Please implement the ByCategory function")
+// ByCategory returns a predicate function that returns true when the category
+// of the record is the same as the provided category and false otherwise.
+func ByCategory(category string) func(Record) bool {
+	return func(record Record) bool {
+		return record.Category == category
+	}
 }
 
-// TotalByPeriod returns total amount of expenses for records
-// inside the period p.
-func TotalByPeriod(in []Record, p DaysPeriod) float64 {
-	panic("Please implement the TotalByPeriod function")
+// TotalByPeriod returns the total amount of expenses for records within the
+// given period.
+func TotalByPeriod(in []Record, period DaysPeriod) float64 {
+	total := float64(0)
+
+	for _, record := range Filter(in, ByDaysPeriod(period)) {
+		total += record.Amount
+	}
+
+	return total
 }
 
-// CategoryExpenses returns total amount of expenses for records
-// in category c that are also inside the period p.
-// An error must be returned only if there are no records in the list that belong
-// to the given category, regardless of period of time.
-func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
-	panic("Please implement the CategoryExpenses function")
+// CategoryExpenses returns the total amount of expenses for records in the
+// given category that are also within the given period.
+// An error must be returned only if there are no records in the list that
+// belong to the given category, regardless of period of time.
+func CategoryExpenses(in []Record, period DaysPeriod, category string) (float64, error) {
+	inCategory := Filter(in, ByCategory(category))
+
+	if len(inCategory) == 0 {
+		return 0, fmt.Errorf("unknown category %s", category)
+	}
+
+	return TotalByPeriod(inCategory, period), nil
 }
