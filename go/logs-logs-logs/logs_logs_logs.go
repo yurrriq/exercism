d5b53f5e32345a1d724ed92629b4ee4b2f444f0c
Eric Bailey <eric@ericb.me>
2022-09-30 19:31:05 -0500

    [go] logs-logs-logs: reimplement using strings and unicode/utf8

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,5 +1,10 @@
 package logs
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 var knownApplications = map[rune]string{
 	'❗': "recommendation",
 	'🔍': "search",
@@ -20,26 +25,11 @@ func Application(log string) string {
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	newLog := make([]rune, 0, len(log))
-	for _, roon := range log {
-		if roon == oldRune {
-			newLog = append(newLog, newRune)
-		} else {
-			newLog = append(newLog, roon)
-		}
-	}
-
-	return string(newLog)
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	for i, _ := range log {
-		if i+1 > limit {
-			return false
-		}
-	}
-
-	return true
+	return utf8.RuneCountInString(log) <= limit
 }
