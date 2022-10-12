5b7e2a1ee03c52b1b7c7a17ffd0821c3ae8b83d8
Eric Bailey <eric@ericb.me>
2022-10-11 22:49:37 -0500

    [go] census: refactor

--- go/census/census.go
+++ go/census/census.go
@@ -18,20 +18,13 @@ func NewResident(name string, age int, address map[string]string) *Resident {
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
-func (r *Resident) HasRequiredInfo() (hasRequiredInfo bool) {
-	if r.Name != "" && r.Address != nil {
-		street, ok := r.Address["street"]
-		hasRequiredInfo = ok && street != ""
-	}
-
-	return hasRequiredInfo
+func (r *Resident) HasRequiredInfo() bool {
+	return r.Name != "" && r.Address["street"] != ""
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	r.Name = ""
-	r.Age = 0
-	r.Address = nil
+	*r = Resident{}
 }
 
 // Count counts all residents that have provided the required information.
