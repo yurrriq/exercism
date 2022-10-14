88b874e3d5daec62e1d1ac130c904e2144aa2da9
Eric Bailey <eric@ericb.me>
2022-10-13 22:11:02 -0500

    [erlang] nix flake init

---
+++ erlang/emacs.el
@@ -0,0 +1,121 @@
+(column-number-mode 1)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+
+(require 'package)
+(setq-default frames-only-mode t
+              indent-tabs-mode nil
+              inhibit-splash-screen t
+              package-archives nil
+              package-enable-at-startup nil)
+(package-initialize)
+
+(load-theme 'wombat)
+
+(global-set-key (kbd "C-x C-k") 'kill-this-buffer)
+(global-set-key (kbd "s-u") 'revert-buffer)
+
+(set-face-attribute 'default nil :family "Iosevka" :height 110)
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq-default use-package-always-ensure t)
+
+(use-package company
+  :custom
+  (company-idle-begin 0.5)
+  :bind
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("M-<" . company-select-first)
+        ("M->" . company-select-last)))
+
+(use-package crux
+  :config
+  (global-set-key (kbd "C-a") 'crux-move-beginning-of-line))
+
+(use-package direnv
+  :config
+  (direnv-mode))
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+(use-package erlang)
+
+(use-package fill-column-indicator
+  :config
+  (setq-default fill-column 80)
+  (global-display-fill-column-indicator-mode))
+
+(use-package flycheck)
+
+(use-package helm-lsp)
+
+(use-package hl-todo
+  :demand
+  :config (global-hl-todo-mode t))
+
+(setq lsp-keymap-prefix "C-l")
+
+(use-package lsp-mode
+  :hook (erlang-mode . lsp-deferred)
+  :commands (lsp lsp-deferred)
+  :custom
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-modeline-code-actions-enable nil)
+  :config
+  (advice-add 'lsp :before #'direnv-update-environment)
+  (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
+                    :major-modes '(nix-mode)
+                    :server-id 'nix)))
+
+(use-package lsp-origami
+  :hook ((origami-mode . lsp-origami-mode)
+         (erlang-mode . origami-mode)))
+
+(use-package lsp-ui
+  :hook (erlang-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-position 'bottom)
+  (lsp-ui-sideline-enable t))
+
+(use-package nix-mode)
+
+(use-package paredit
+  :hook (emacs-lisp-mode . paredit-mode))
+
+(use-package rainbow-delimiters
+  :hook (emacs-lisp-mode . rainbow-delimiters-mode))
+
+(use-package smex
+  :demand
+  :config
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+
+(use-package which-key
+  :hook ((erlang-mode . which-key-mode)
+         (lsp-mode . lsp-enable-which-key-integration)))
+
+(use-package whitespace-cleanup-mode
+  :config (global-whitespace-cleanup-mode t))
+
+(use-package yaml-mode)
+
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*Flycheck errors*" eos)
+              (display-buffer-reuse-window
+               display-buffer-in-side-window)
+              (side            . bottom)
+              (reusable-frames . visible)
+              (window-height   . 0.33)))
