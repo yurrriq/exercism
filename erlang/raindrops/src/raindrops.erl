6cb9bbd9f42e6c3cffbe485d6df8580ad39478d2
Eric Bailey <eric@ericb.me>
2022-10-13 22:58:28 -0500

    [erlang] raindrops: submit

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,5 +2,25 @@
 
 -export([convert/1]).
 
+-define(RAINDROPS, [
+    {3, "Pling"},
+    {5, "Plang"},
+    {7, "Plong"}
+]).
 
-convert(_Number) -> undefined.
+-spec convert(Number :: number()) -> Result :: string().
+convert(Number) ->
+    case lists:foldr(do_convert(Number), [], ?RAINDROPS) of
+        [] -> integer_to_list(Number);
+        List -> lists:flatten(List)
+    end.
+
+-spec do_convert(Number :: number()) ->
+    fun(({Divisor :: number(), Drop :: string()}, Acc :: list(string())) -> string()).
+do_convert(Number) ->
+    fun
+        ({Divisor, Drop}, Acc) when Number rem Divisor =:= 0 ->
+            [Drop | Acc];
+        (_, Acc) ->
+            Acc
+    end.
