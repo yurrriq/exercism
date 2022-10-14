633c1426c6059962485ab0b9b3a39ed2f3641f68
Eric Bailey <eric@ericb.me>
2022-10-13 22:21:41 -0500

    [erlang] two-fer: submit

--- erlang/two-fer/src/two_fer.erl
+++ erlang/two-fer/src/two_fer.erl
@@ -2,7 +2,10 @@
 
 -export([two_fer/0, two_fer/1]).
 
+-spec two_fer() -> Result :: string().
+two_fer() ->
+    "One for you, one for me.".
 
-two_fer() -> undefined.
-
-two_fer(_Name) -> undefined.
+-spec two_fer(Name :: string()) -> Result :: string().
+two_fer(Name) ->
+    io_lib:format("One for ~s, one for me.", [Name]).
