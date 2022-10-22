4e93e79b355f10cc5f04aed38b1f29c7285224f8
Eric Bailey <eric@ericb.me>
2022-10-22 18:40:43 -0500

    [haskell] yacht: submit

--- haskell/yacht/src/Yacht.hs
+++ haskell/yacht/src/Yacht.hs
@@ -1,17 +1,48 @@
-module Yacht (yacht, Category(..)) where
+module Yacht
+  ( yacht,
+    Category (..),
+  )
+where
 
-data Category = Ones
-              | Twos
-              | Threes
-              | Fours
-              | Fives
-              | Sixes
-              | FullHouse
-              | FourOfAKind
-              | LittleStraight
-              | BigStraight
-              | Choice
-              | Yacht
+import Data.List (group, sort)
+
+data Category
+  = Ones
+  | Twos
+  | Threes
+  | Fours
+  | Fives
+  | Sixes
+  | FullHouse
+  | FourOfAKind
+  | LittleStraight
+  | BigStraight
+  | Choice
+  | Yacht
 
 yacht :: Category -> [Int] -> Int
-yacht category dice = error "You need to implement this function."
+yacht Ones dice = sum (filter (== 1) dice)
+yacht Twos dice = sum (filter (== 2) dice)
+yacht Threes dice = sum (filter (== 3) dice)
+yacht Fours dice = sum (filter (== 4) dice)
+yacht Fives dice = sum (filter (== 5) dice)
+yacht Sixes dice = sum (filter (== 6) dice)
+yacht FullHouse dice =
+  case group (sort dice) of
+    [[_, _], [_, _, _]] -> sum dice
+    [[_, _, _], [_, _]] -> sum dice
+    _ -> 0
+yacht FourOfAKind dice =
+  case group (sort dice) of
+    [[x, _, _, _], [_]] -> 4 * x
+    [[_], [x, _, _, _]] -> 4 * x
+    [[x, _, _, _, _]] -> 4 * x
+    _ -> 0
+yacht LittleStraight dice
+  | sort dice == [1, 2, 3, 4, 5] = 30
+yacht BigStraight dice
+  | sort dice == [2, 3, 4, 5, 6] = 30
+yacht Choice dice = sum dice
+yacht Yacht (die : dice)
+  | all (== die) dice = 50
+yacht _category _dice = 0
