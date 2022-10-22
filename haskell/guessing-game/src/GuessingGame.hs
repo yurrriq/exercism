2e9598d0fcae9596e03c9c8ed357309f580562db
Eric Bailey <eric@ericb.me>
2022-10-21 23:01:53 -0500

    [haskell] guessing-game: submit

--- haskell/guessing-game/src/GuessingGame.hs
+++ haskell/guessing-game/src/GuessingGame.hs
@@ -1,4 +1,23 @@
-module GuessingGame (reply) where
+-- |
+-- Module      : GuessingGame
+-- Copyright   : (c) Eric Bailey, 2022
+-- License     : MIT
+--
+-- Maintainer  : Eric Bailey
+-- Stability   : stable
+-- Portability : portable
+--
+-- A number guessing game.
+module GuessingGame
+  ( reply,
+  )
+where
 
+-- | Reply to a 'guess', responding differently depending on how close the guess
+-- was to the chosen number (@42@).
 reply :: Int -> String
-reply guess = error "Implement this function."
+reply guess
+  | guess == 42 = "Correct"
+  | guess == 41 || guess == 43 = "So close"
+  | guess < 41 = "Too low"
+  | otherwise = "Too high"
