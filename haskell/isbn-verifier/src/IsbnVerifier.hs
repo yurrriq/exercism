df70381a9294ee1eaf724cea218384dc75373e18
Eric Bailey <eric@ericb.me>
2022-10-22 01:05:05 -0500

    [haskell] isbn-verifier: submit

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,20 @@
-module IsbnVerifier (isbn) where
+module IsbnVerifier
+  ( isbn,
+  )
+where
+
+import Data.Char (digitToInt)
 
 isbn :: String -> Bool
-isbn = error "You need to implement this function."
+isbn = go 10 0
+  where
+    go 0 checksum [] =
+      checksum `rem` 11 == 0
+    go pos checksum ('-' : chars) =
+      go pos checksum chars
+    go 1 checksum "X" =
+      go 0 (checksum + 10) []
+    go pos checksum (char : chars)
+      | char `elem` "0123456789" =
+        go (pos - 1) (checksum + (digitToInt char * pos)) chars
+    go _ _ _ = False
