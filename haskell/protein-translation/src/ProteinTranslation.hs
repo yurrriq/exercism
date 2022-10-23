4d74d523aec112143c968a03c43f7decd067a220
Eric Bailey <eric@ericb.me>
2022-10-22 19:51:36 -0500

    [haskell] protein-translation: re-implement with Data.Map

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -4,17 +4,22 @@ module ProteinTranslation
 where
 
 import Data.List.Split (chunksOf)
+import Data.Map.Strict (Map, (!?))
+import qualified Data.Map.Strict as Map
 
 proteins :: String -> Maybe [String]
-proteins = fmap (takeWhile (/= "STOP")) . mapM go . chunksOf 3
-  where
-    go "AUG" = Just "Methionine"
-    go "UGG" = Just "Tryptophan"
-    go codon
-      | codon `elem` ["UUU", "UUC"] = Just "Phenylalanine"
-      | codon `elem` ["UUA", "UUG"] = Just "Leucine"
-      | codon `elem` ["UCU", "UCC", "UCA", "UCG"] = Just "Serine"
-      | codon `elem` ["UAU", "UAC"] = Just "Tyrosine"
-      | codon `elem` ["UGU", "UGC"] = Just "Cysteine"
-      | codon `elem` ["UAA", "UAG", "UGA"] = Just "STOP"
-    go _ = Nothing
+proteins = fmap (takeWhile (/= "STOP")) . mapM (translations !?) . chunksOf 3
+
+translations :: Map String String
+translations =
+  Map.fromList
+    . concatMap (\(codons, protein) -> [(codon, protein) | codon <- codons])
+    $ [ (["AUG"], "Methionine"),
+        (["UGG"], "Tryptophan"),
+        (["UUU", "UUC"], "Phenylalanine"),
+        (["UUA", "UUG"], "Leucine"),
+        (["UCU", "UCC", "UCA", "UCG"], "Serine"),
+        (["UAU", "UAC"], "Tyrosine"),
+        (["UGU", "UGC"], "Cysteine"),
+        (["UAA", "UAG", "UGA"], "STOP")
+      ]
