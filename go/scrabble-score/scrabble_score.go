a236003a65f485e672a7c3006fb8aeda9025522e
Eric Bailey <eric@ericb.me>
2022-10-26 23:47:45 -0500

    [go] scrabble-score: submit

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,29 @@
 package scrabble
 
-func Score(word string) int {
-	panic("Please implement the Score function")
+import "strings"
+
+var lettersScoreMap = map[string]int{
+	"AEIOULNRST": 1,
+	"DG":         2,
+	"BCMP":       3,
+	"FHVWY":      4,
+	"K":          5,
+	"JX":         8,
+	"QZ":         10,
+}
+
+func Score(word string) (score int) {
+WordLetterIterator:
+	for _, wordLetter := range strings.ToUpper(word) {
+		for letters, letterScore := range lettersScoreMap {
+			for _, letter := range letters {
+				if wordLetter == letter {
+					score += letterScore
+					continue WordLetterIterator
+				}
+			}
+		}
+	}
+
+	return score
 }
