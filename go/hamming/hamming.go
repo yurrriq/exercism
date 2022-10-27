3b58735931aef4886b4cb4d66f17eb352f986738
Eric Bailey <eric@ericb.me>
2022-10-27 00:05:35 -0500

    [go] hamming: submit

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,17 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+import "errors"
+
+func Distance(a, b string) (distance int, err error) {
+	if len(a) != len(b) {
+		return 0, errors.New("Can't compute Hamming distance for strings of unequal length")
+	}
+
+	for i, aRune := range a {
+		if aRune != []rune(b)[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
