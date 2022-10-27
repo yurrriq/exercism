ed6913c57172c65f0dcbdd11d258b996b01d02a5
Eric Bailey <eric@ericb.me>
2022-10-26 23:33:56 -0500

    [go] two-fer: submit

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,11 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer implements the Two Fer exercise.
 package twofer
 
-// ShareWith should have a comment documenting it.
+// Given a name, return a string with the message, "One for name, one for me."
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name == "" {
+		return ShareWith("you")
+	}
+
+	return "One for " + name + ", one for me."
 }
