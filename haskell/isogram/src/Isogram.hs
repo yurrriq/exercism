844f674a24dbda1727515040d906bd43b3b438f0
Eric Bailey <eric@ericb.me>
2022-10-28 20:43:28 -0500

    [haskell] isogram: submit

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,4 +1,15 @@
-module Isogram (isIsogram) where
+module Isogram
+  ( isIsogram,
+  )
+where
+
+import Data.Char (isAlpha, toUpper)
+import qualified Data.Set as Set
 
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram = go Set.empty . map toUpper . filter isAlpha
+  where
+    go _ "" = True
+    go seen (c : cs)
+      | c `Set.member` seen = False
+      | otherwise = go (Set.insert c seen) cs
