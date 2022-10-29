522adbdc9ede7a1f918aafa2eeaf4c9559cf59af
Eric Bailey <eric@ericb.me>
2022-10-28 19:57:48 -0500

    [haskell] armstrong-numbers: submit

--- haskell/armstrong-numbers/src/ArmstrongNumbers.hs
+++ haskell/armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,21 @@
-module ArmstrongNumbers (armstrong) where
+module ArmstrongNumbers
+  ( armstrong,
+  )
+where
+
+import Data.List (unfoldr)
+import Data.Monoid (Sum (..))
+import Data.Tuple (swap)
 
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong 0 = True
+armstrong n = foldMap (Sum . (^ k)) ds == Sum n
+  where
+    k = length ds
+    ds = toDigits 10 n
+
+toDigits :: Integral a => a -> a -> [a]
+toDigits base = unfoldr go
+  where
+    go 0 = Nothing
+    go m = Just . swap $ m `divMod` base
