193f064302cbb3390521e7a48cc9e0956a556265
Eric Bailey <eric@ericb.me>
2022-11-16 21:17:41 -0600

    [haskell] rail-fence-cipher: submit

--- haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,7 +1,26 @@
-module RailFenceCipher (encode, decode) where
+module RailFenceCipher
+  ( encode,
+    decode,
+  )
+where
+
+import Data.Char (isAlpha)
+import Data.List (sortOn)
 
 encode :: Int -> String -> String
-encode = error "You need to implement this function!"
+encode n = railFence n . filter isAlpha
 
 decode :: Int -> String -> String
-decode = error "You need to implement this function!"
+decode = unRailFence
+
+railFence :: Int -> [a] -> [a]
+railFence n = sortIndexed . zip (zigZag n)
+
+unRailFence :: Int -> [b] -> [b]
+unRailFence n str = sortIndexed (zip (railFence n [1 .. length str]) str)
+
+sortIndexed :: [(Int, b)] -> [b]
+sortIndexed = map snd . sortOn fst
+
+zigZag :: (Num a, Enum a) => a -> [a]
+zigZag n = cycle ([1 .. n] ++ [n - 1, n - 2 .. 2])
