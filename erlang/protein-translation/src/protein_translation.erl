58851b577b0b4537dbba3c262fc2e884d7b98af5
Eric Bailey <eric@ericb.me>
2022-11-21 19:59:59 -0600

    [erlang] protein-translation: submit

--- erlang/protein-translation/src/protein_translation.erl
+++ erlang/protein-translation/src/protein_translation.erl
@@ -2,5 +2,36 @@
 
 -export([proteins/1]).
 
+-define(AMINO_ACIDS, [
+    {"AUG", methionine},
+    {"UGG", tryptophan},
+    {"UUU", phenylalanine},
+    {"UUC", phenylalanine},
+    {"UUA", leucine},
+    {"UUG", leucine},
+    {"UCU", serine},
+    {"UCC", serine},
+    {"UCA", serine},
+    {"UCG", serine},
+    {"UAU", tyrosine},
+    {"UAC", tyrosine},
+    {"UGU", cysteine},
+    {"UGC", cysteine},
+    {"UAA", stop},
+    {"UAG", stop},
+    {"UGA", stop}
+]).
 
-proteins(_Strand) -> undefined.
+proteins(Strand) ->
+    do_proteins(Strand, []).
+
+do_proteins([X, Y, Z | Tail], Acc) ->
+    case proplists:get_value([X, Y, Z], ?AMINO_ACIDS) of
+        stop -> do_proteins([], Acc);
+        undefined -> {error, badarg};
+        Protein -> do_proteins(Tail, [Protein | Acc])
+    end;
+do_proteins([], Acc) ->
+    {ok, lists:reverse(Acc)};
+do_proteins(_, _) ->
+    {error, badarg}.
