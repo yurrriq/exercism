d75f0b5812c3a48ed9af421b08eb49c088805b14
Eric Bailey <eric@ericb.me>
2022-11-25 00:34:18 -0600

    [haskell] sgf-parsing: submit

--- haskell/sgf-parsing/src/Sgf.hs
+++ haskell/sgf-parsing/src/Sgf.hs
@@ -1,8 +1,51 @@
-module Sgf (parseSgf) where
+module Sgf
+  ( parseSgf,
+  )
+where
 
-import Data.Map  (Map)
+import Control.Applicative ((<|>))
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Text (Text)
-import Data.Tree (Tree)
+import qualified Data.Text as Text
+import Data.Tree (Tree (..))
+import Text.Trifecta
+
+-- | A tree of nodes.
+type SgfTree = Tree SgfNode
+
+-- | A node is a property list, each key can only occur once.
+-- Keys may have multiple values associated with them.
+type SgfNode = Map Text [Text]
 
 parseSgf :: Text -> Maybe (Tree (Map Text [Text]))
-parseSgf sgf = error "You need to implement this function."
+parseSgf sgf =
+  case parseString (parens sgfTree) mempty (Text.unpack sgf) of
+    Success result -> Just result
+    Failure _ -> Nothing
+
+sgfTree :: Parser SgfTree
+sgfTree = Node <$> sgfNode <*> (count 1 sgfTree <|> many (parens sgfTree))
+
+sgfNode :: Parser SgfNode
+sgfNode = char ';' *> (Map.fromList <$> many sgfProperty)
+
+sgfProperty :: Parser (Text, [Text])
+sgfProperty =
+  (,)
+    <$> (Text.pack <$> some upper)
+    <*> (map Text.pack <$> some (brackets sgfValue))
+
+sgfValue :: Parser String
+sgfValue = concat <$> some sanitizedChar
+  where
+    sanitizedChar = escapedChar <|> (sanitize <$> notChar ']')
+
+    escapedChar = char '\\' *> (escape <$> anyChar)
+
+    escape '\n' = ""
+    escape c = sanitize c
+
+    sanitize '\n' = "\n"
+    sanitize '\t' = " "
+    sanitize c = [c]
