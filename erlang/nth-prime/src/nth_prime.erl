284948bb04960857359f36ce32afbe41950b92fa
Eric Bailey <eric@ericb.me>
2023-02-19 02:03:56 -0600

    [erlang] nth-prime: commit slow solution

--- erlang/nth-prime/src/nth_prime.erl
+++ erlang/nth-prime/src/nth_prime.erl
@@ -2,5 +2,33 @@
 
 -export([prime/1]).
 
+-spec prime(pos_integer()) -> pos_integer().
+prime(1) ->
+    2;
+prime(2) ->
+    3;
+prime(N) when N >= 3 ->
+    nth_prime(N, {2, [2, 3]}).
 
-prime(_N) -> undefined.
+-spec nth_prime(_, {pos_integer(), [pos_integer(), ...]}) -> pos_integer().
+nth_prime(N, {K, [Prime | _]}) when K =:= N ->
+    Prime;
+nth_prime(N, {K, Primes}) ->
+    nth_prime(N, {K + 1, [next_prime(Primes) | Primes]}).
+
+-spec next_prime([pos_integer(), ...]) -> pos_integer().
+next_prime([Prime | Primes]) ->
+    next_prime([Prime | Primes], Prime + 1).
+
+-spec next_prime([pos_integer(), ...], pos_integer()) -> pos_integer().
+next_prime(Primes, Candidate) ->
+    case is_prime(Candidate, Primes) of
+        true ->
+            Candidate;
+        _ ->
+            next_prime(Primes, Candidate + 1)
+    end.
+
+-spec is_prime(pos_integer(), [pos_integer(), ...]) -> boolean().
+is_prime(Candidate, Primes) ->
+    not lists:any(fun(Prime) -> Candidate rem Prime =:= 0 end, Primes).
--- erlang/nth-prime/test/nth_prime_tests.erl
+++ erlang/nth-prime/test/nth_prime_tests.erl
@@ -8,9 +8,6 @@
 -include_lib("erl_exercism/include/exercism.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-
-
-
 '1_first_prime_test_'() ->
     {"first prime", ?_assertEqual(2, nth_prime:prime(1))}.
 
@@ -21,9 +18,7 @@
     {"sixth prime", ?_assertEqual(13, nth_prime:prime(6))}.
 
 '4_big_prime_test_'() ->
-    {"big prime",
-     ?_assertEqual(104743, nth_prime:prime(10001))}.
+    {"big prime", ?_assertEqual(104743, nth_prime:prime(10001))}.
 
 '5_there_is_no_zeroth_prime_test_'() ->
-    {"there is no zeroth prime",
-     ?_assertError(_, nth_prime:prime(0))}.
+    {"there is no zeroth prime", ?_assertError(_, nth_prime:prime(0))}.
