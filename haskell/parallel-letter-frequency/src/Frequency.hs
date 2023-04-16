4ac47b503a885106aae7f17561dfe45eb22130ea
Eric Bailey <eric@ericb.me>
2023-04-16 17:25:02 +0200

    [haskell] parallel-letter-frequency: submit

--- haskell/parallel-letter-frequency/src/Frequency.hs
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -1,7 +1,22 @@
-module Frequency (frequency) where
+module Frequency
+  ( frequency,
+  )
+where
 
-import Data.Map  (Map)
+import Control.Parallel.Strategies (parBuffer, rdeepseq, using)
+import Data.Char (isLetter, toLower)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Text (Text)
+import qualified Data.Text as Text
 
 frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = error "You need to implement this function."
+frequency _ [] = Map.empty
+frequency n texts = Map.unionsWith (+) results
+  where
+    results = map letterFrequency texts `using` parBuffer n rdeepseq
+    letterFrequency = Text.foldr go Map.empty
+      where
+        go c seen
+          | isLetter c = Map.insertWith (+) (toLower c) 1 seen
+          | otherwise = seen
