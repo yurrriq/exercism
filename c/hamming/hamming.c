69e2ec71d08ee78184a84c5a79e242659b57d66e
Eric Bailey <eric@ericb.me>
2023-05-01 22:32:37 -0500

    [c] hamming: submit

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,19 @@
 #include "hamming.h"
+#include <string.h>
+
+int compute(const char *lhs, const char *rhs)
+{
+    if (strlen(lhs) != strlen(rhs)) {
+        return -1;
+    }
+
+    int distance = 0;
+    unsigned long i;
+    for (i = 0; i < strlen(lhs); i++) {
+        if (lhs[i] != rhs[i]) {
+            distance++;
+        }
+    }
+
+    return distance;
+}
