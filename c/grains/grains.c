0c44f50fb86df75046a72e61a62f17c135a9104b
Eric Bailey <eric@ericb.me>
2023-05-01 22:08:32 -0500

    [c] grains: submit

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,16 @@
 #include "grains.h"
+#include <math.h>
+
+uint64_t square(uint8_t index)
+{
+    if (index >= 1 && index <= 64) {
+        return pow(2, index - 1);
+    }
+
+    return 0;
+}
+
+uint64_t total(void)
+{
+    return square(65) - 1;
+}
