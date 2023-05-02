d5a468af49e61c096fca552c06da1591c45b4bb2
Eric Bailey <eric@ericb.me>
2023-05-01 22:43:48 -0500

    [c] hamming: assign length_lhs

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -3,13 +3,15 @@
 
 int compute(const char *lhs, const char *rhs)
 {
-    if (strlen(lhs) != strlen(rhs)) {
+    unsigned long length_lhs = strlen(lhs);
+
+    if (length_lhs != strlen(rhs)) {
         return -1;
     }
 
     int distance = 0;
     unsigned long i;
-    for (i = 0; i < strlen(lhs); i++) {
+    for (i = 0; i < length_lhs; i++) {
         if (lhs[i] != rhs[i]) {
             distance++;
         }
