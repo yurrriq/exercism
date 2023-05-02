2a270697760dcd2a79cae004c0714a40a7db52cf
Eric Bailey <eric@ericb.me>
2023-05-01 21:19:30 -0500

    [haskell] diamond: submit

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,4 +1,26 @@
-module Diamond (diamond) where
+module Diamond
+  ( diamond,
+  )
+where
+
+import Data.Char (chr, isUpper, ord)
 
 diamond :: Char -> Maybe [String]
-diamond = error "You need to implement this function"
+diamond z
+  | isUpper z = Just (reflectRight (triangle z))
+  | otherwise = Nothing
+
+triangle :: Char -> [String]
+triangle z = map (triangleRow (ord z)) [65 .. ord z]
+
+triangleRow :: Int -> Int -> [Char]
+triangleRow ordZ ordC =
+  reflectLeft $
+    replicate (ordC - 65) ' '
+      <> (chr ordC : replicate (ordZ - ordC) ' ')
+
+reflectLeft :: [a] -> [a]
+reflectLeft = reverse <> tail
+
+reflectRight :: [a] -> [a]
+reflectRight = init <> reverse
--- haskell/diamond/test/Tests.hs
+++ haskell/diamond/test/Tests.hs
@@ -1,22 +1,32 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Data.Char               (isLetter, isPrint, isSpace)
-import Data.Foldable           (for_)
-import Data.List               (isSuffixOf)
-import Data.Maybe              (isJust, isNothing)
+import Data.Char (isLetter, isPrint, isSpace)
+import Data.Foldable (for_)
+import Data.List (isSuffixOf)
+import Data.Maybe (isJust, isNothing)
 import Data.String.Conversions (convertString)
-import Test.Hspec              (Spec, describe, it, shouldBe)
-import Test.Hspec.Runner       (configFastFail, defaultConfig, hspecWith)
-import Test.QuickCheck         (arbitraryASCIIChar, conjoin, counterexample,
-                                discard, elements, forAll, forAllShrink, Gen,
-                                Property, suchThat, Testable, (===))
-
 import Diamond (diamond)
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
+import Test.QuickCheck
+  ( Gen,
+    Property,
+    Testable,
+    arbitraryASCIIChar,
+    conjoin,
+    counterexample,
+    discard,
+    elements,
+    forAll,
+    forAllShrink,
+    suchThat,
+    (===),
+  )
 
 main :: IO ()
-main = hspecWith defaultConfig {configFastFail = True} specs
+main = hspecWith defaultConfig {configFailFast = True} specs
 
 specs :: Spec
 specs = describe "diamond" $ do
@@ -25,133 +35,152 @@ specs = describe "diamond" $ do
       isNothing . diamond
 
   it "should produce a value for an alpha character" $
-    forAll genAlphaChar $ isJust . diamond
+    forAll genAlphaChar $
+      isJust . diamond
 
   it "should have an odd number of rows" $
-    forAllDiamond $ odd . length
+    forAllDiamond $
+      odd . length
 
   it "should have equal top and bottom" $
     forAllDiamond $ \rows ->
       let halfRoundDown = length rows `div` 2
-      in take halfRoundDown rows === take halfRoundDown (reverse rows)
+       in take halfRoundDown rows === take halfRoundDown (reverse rows)
 
   it "should have the same width and height" $
     forAllDiamond $ \rows ->
-      let sameHeightWidth idx row = counterexample
-            (concat [ "The length of row with index "
-                    , show idx
-                    , " is not equal to the height" ])
-            (length row === length rows)
-      in conjoin $ zipWith sameHeightWidth [0 :: Int ..] rows
+      let sameHeightWidth idx row =
+            counterexample
+              ( concat
+                  [ "The length of row with index ",
+                    show idx,
+                    " is not equal to the height"
+                  ]
+              )
+              (length row === length rows)
+       in conjoin $ zipWith sameHeightWidth [0 :: Int ..] rows
 
   it "rows should start and end with the same letter" $
     forAllDiamond $
       let headEqualsLast row = not (null row) && take 1 row `isSuffixOf` row
-      in (&&) <$> not . null <*> all (headEqualsLast . filter (not . isSpace))
+       in (&&) <$> not . null <*> all (headEqualsLast . filter (not . isSpace))
 
   for_ cases test
   where
-    test Case{..} = it description assertion
+    test Case {..} = it description assertion
       where
         assertion = (fmap . fmap) convertString (diamond input) `shouldBe` Just expected
 
-
-data Case = Case { description :: String
-                 , input       :: Char
-                 , expected    :: [String]
-                 }
+data Case = Case
+  { description :: String,
+    input :: Char,
+    expected :: [String]
+  }
 
 cases :: [Case]
-cases = [ Case { description = "Degenerate case with a single 'A' row"
-               , input       = 'A'
-               , expected    = ["A"]
-               }
-        , Case { description = "Degenerate case with no row containing 3 distinct groups of spaces"
-               , input       = 'B'
-               , expected    = [" A ",
-                                "B B",
-                                " A "]
-               }
-        , Case { description = "Smallest non-degenerate case with odd diamond side length"
-               , input       = 'C'
-               , expected    = ["  A  ",
-                                " B B ",
-                                "C   C",
-                                " B B ",
-                                "  A  "]
-               }
-        , Case { description = "Smallest non-degenerate case with even diamond side length"
-               , input       = 'D'
-               , expected    = ["   A   ",
-                                "  B B  ",
-                                " C   C ",
-                                "D     D",
-                                " C   C ",
-                                "  B B  ",
-                                "   A   "]
-               }
-        , Case { description = "Largest possible diamond"
-               , input       = 'Z'
-               , expected    = [
-                        "                         A                         ",
-                        "                        B B                        ",
-                        "                       C   C                       ",
-                        "                      D     D                      ",
-                        "                     E       E                     ",
-                        "                    F         F                    ",
-                        "                   G           G                   ",
-                        "                  H             H                  ",
-                        "                 I               I                 ",
-                        "                J                 J                ",
-                        "               K                   K               ",
-                        "              L                     L              ",
-                        "             M                       M             ",
-                        "            N                         N            ",
-                        "           O                           O           ",
-                        "          P                             P          ",
-                        "         Q                               Q         ",
-                        "        R                                 R        ",
-                        "       S                                   S       ",
-                        "      T                                     T      ",
-                        "     U                                       U     ",
-                        "    V                                         V    ",
-                        "   W                                           W   ",
-                        "  X                                             X  ",
-                        " Y                                               Y ",
-                        "Z                                                 Z",
-                        " Y                                               Y ",
-                        "  X                                             X  ",
-                        "   W                                           W   ",
-                        "    V                                         V    ",
-                        "     U                                       U     ",
-                        "      T                                     T      ",
-                        "       S                                   S       ",
-                        "        R                                 R        ",
-                        "         Q                               Q         ",
-                        "          P                             P          ",
-                        "           O                           O           ",
-                        "            N                         N            ",
-                        "             M                       M             ",
-                        "              L                     L              ",
-                        "               K                   K               ",
-                        "                J                 J                ",
-                        "                 I               I                 ",
-                        "                  H             H                  ",
-                        "                   G           G                   ",
-                        "                    F         F                    ",
-                        "                     E       E                     ",
-                        "                      D     D                      ",
-                        "                       C   C                       ",
-                        "                        B B                        ",
-                        "                         A                         "]
-               }
-        ]
+cases =
+  [ Case
+      { description = "Degenerate case with a single 'A' row",
+        input = 'A',
+        expected = ["A"]
+      },
+    Case
+      { description = "Degenerate case with no row containing 3 distinct groups of spaces",
+        input = 'B',
+        expected =
+          [ " A ",
+            "B B",
+            " A "
+          ]
+      },
+    Case
+      { description = "Smallest non-degenerate case with odd diamond side length",
+        input = 'C',
+        expected =
+          [ "  A  ",
+            " B B ",
+            "C   C",
+            " B B ",
+            "  A  "
+          ]
+      },
+    Case
+      { description = "Smallest non-degenerate case with even diamond side length",
+        input = 'D',
+        expected =
+          [ "   A   ",
+            "  B B  ",
+            " C   C ",
+            "D     D",
+            " C   C ",
+            "  B B  ",
+            "   A   "
+          ]
+      },
+    Case
+      { description = "Largest possible diamond",
+        input = 'Z',
+        expected =
+          [ "                         A                         ",
+            "                        B B                        ",
+            "                       C   C                       ",
+            "                      D     D                      ",
+            "                     E       E                     ",
+            "                    F         F                    ",
+            "                   G           G                   ",
+            "                  H             H                  ",
+            "                 I               I                 ",
+            "                J                 J                ",
+            "               K                   K               ",
+            "              L                     L              ",
+            "             M                       M             ",
+            "            N                         N            ",
+            "           O                           O           ",
+            "          P                             P          ",
+            "         Q                               Q         ",
+            "        R                                 R        ",
+            "       S                                   S       ",
+            "      T                                     T      ",
+            "     U                                       U     ",
+            "    V                                         V    ",
+            "   W                                           W   ",
+            "  X                                             X  ",
+            " Y                                               Y ",
+            "Z                                                 Z",
+            " Y                                               Y ",
+            "  X                                             X  ",
+            "   W                                           W   ",
+            "    V                                         V    ",
+            "     U                                       U     ",
+            "      T                                     T      ",
+            "       S                                   S       ",
+            "        R                                 R        ",
+            "         Q                               Q         ",
+            "          P                             P          ",
+            "           O                           O           ",
+            "            N                         N            ",
+            "             M                       M             ",
+            "              L                     L              ",
+            "               K                   K               ",
+            "                J                 J                ",
+            "                 I               I                 ",
+            "                  H             H                  ",
+            "                   G           G                   ",
+            "                    F         F                    ",
+            "                     E       E                     ",
+            "                      D     D                      ",
+            "                       C   C                       ",
+            "                        B B                        ",
+            "                         A                         "
+          ]
+      }
+  ]
 
 genNonAlphaChar :: Gen Char
 genNonAlphaChar = arbitraryASCIIChar `suchThat` (not . isLetter)
 
 genAlphaChar :: Gen Char
-genAlphaChar = elements ['A'..'Z']
+genAlphaChar = elements ['A' .. 'Z']
 
 genDiamond :: Gen (Maybe [String])
 genDiamond = (fmap . fmap . fmap) convertString $ diamond <$> genAlphaChar
@@ -162,7 +191,7 @@ forAllDiamond p = forAll genDiamond $ maybe discard p
 shrinkNonAlphaChar :: Char -> String
 shrinkNonAlphaChar c =
   if isPrint c
-  then takeWhile (/= c) printableChars
-  else printableChars
+    then takeWhile (/= c) printableChars
+    else printableChars
   where
     printableChars = filter isPrint ['\0' .. '\127']
