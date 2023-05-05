ea181c3f9ced982784a7996381fb78adbb1b21d5
Eric Bailey <eric@ericb.me>
2023-05-04 20:53:41 -0500

    Adopt treefmt and pre-commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
+  };
+
+  outputs = { flake-utils, nixpkgs, treefmt-nix, ... }:
+    flake-utils.lib.eachDefaultSystem (_system:
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pre-commit
+          ];
+        };
+        formatter = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            black.enable = true;
+            clang-format.enable = true;
+            deadnix.enable = true;
+            gofmt.enable = true;
+            hlint.enable = true;
+            nixpkgs-fmt.enable = true;
+            # TODO: ocamlformat.enable = true;
+            ormolu = {
+              enable = true;
+              ghcOpts = [
+                "OverloadedStrings"
+                "TemplateHaskell"
+              ];
+            };
+            rufo.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      }
+    );
+}
