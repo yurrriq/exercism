4ad87da9c3fcb52007fb118ec035b18d22c6a5c9
Eric Bailey <eric@ericb.me>
2023-05-13 00:41:15 -0500

    [rust] raindrops: submit

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,23 @@
-pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{n} make?")
+use phf::phf_ordered_map;
+
+static RAINDROPS: phf::OrderedMap<u32, &'static str> = phf_ordered_map! {
+    3_u32 => "Pling",
+    5_u32 => "Plang",
+    7_u32 => "Plong",
+};
+
+pub fn raindrops(number: u32) -> String {
+    let mut result = String::from("");
+
+    for (divisor, word) in RAINDROPS.entries() {
+        if number % divisor == 0 {
+            result.push_str(word);
+        }
+    }
+
+    if result.is_empty() {
+        result = number.to_string();
+    }
+
+    result
 }
