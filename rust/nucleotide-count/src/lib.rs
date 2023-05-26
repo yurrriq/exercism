7ea864e9d7fd85e9e7bc755e0246bdb3d2477878
Eric Bailey <eric@ericb.me>
2023-05-26 02:36:53 -0500

    [rust] nucleotide-count: resubmit

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -4,13 +4,10 @@ use itertools::Itertools;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
     validate_nucleotide(nucleotide).and(
-        dna.chars().map(validate_nucleotide).fold_ok(0, |acc, x| {
-            if x == nucleotide {
-                acc + 1
-            } else {
-                acc
-            }
-        }),
+        dna.chars()
+            .map(validate_nucleotide)
+            .collect::<Result<Vec<char>, _>>()
+            .map(|xs| xs.into_iter().filter(|&x| x == nucleotide).count()),
     )
 }
 
@@ -25,5 +22,5 @@ pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
 }
 
 fn validate_nucleotide(x: char) -> Result<char, char> {
-    ['A', 'C', 'G', 'T'].contains(&x).then_some(x).ok_or(x)
+    "ACGT".contains(x).then_some(x).ok_or(x)
 }
