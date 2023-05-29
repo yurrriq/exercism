1644b8b240ba6fe707d668ddbd8aa6f55ef8b96a
Eric Bailey <eric@ericb.me>
2023-05-28 23:51:56 -0500

    [rust] prime-factors: re-submit
    https://doc.rust-lang.org/std/iter/fn.once.html

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,15 +1,16 @@
+use std::iter;
+
 pub fn factors(n: u64) -> Vec<u64> {
     let mut n = n;
     let mut factors = Vec::new();
-    let mut divisors = (3..).step_by(2);
-    let mut d = 2;
+
+    let mut divisors = iter::once(2).chain((3..).step_by(2));
     while n != 1 {
+        let d = divisors.next().unwrap();
         while n % d == 0 {
             n /= d;
             factors.push(d);
         }
-
-        d = divisors.next().unwrap();
     }
 
     factors
