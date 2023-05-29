26c31b4088612ce3d1b146ad5ce87a976d6e5faa
Eric Bailey <eric@ericb.me>
2023-05-29 00:48:25 -0500

    [erlang] prime-factors: submit

--- erlang/prime-factors/src/prime_factors.erl
+++ erlang/prime-factors/src/prime_factors.erl
@@ -1,6 +1,45 @@
+%%% ====================================================== [ prime_factors.erl ]
+%%% @doc Prime Factors
+%%% Compute the prime factors of a given natural number.
+%%% @end
+%%%
+%%% @author Eric Bailey
+%%% @copyright 2023 Eric Bailey
+%%% @version 0.0.1
+%%% @end
+%%% ==================================================================== [ EOH ]
 -module(prime_factors).
 
+%%% Public API.
 -export([factors/1]).
 
+%%% ============================================================= [ Public API ]
 
-factors(_Value) -> undefined.
+%% @doc Compute the prime factors of a given natural `Number'.
+-spec factors(Number :: pos_integer()) -> Factors :: [pos_integer(), ...].
+factors(Number) ->
+    do_factors(Number, 2, []).
+
+%%% ========================================================== [ Private Parts ]
+
+%% @doc Internal implementation of {@link factors/1}.
+-spec do_factors(
+    Number :: pos_integer(),
+    Divisor :: pos_integer(),
+    FactorsAcc :: [pos_integer()]
+) -> Factors :: [pos_integer()].
+%% If `Number' is 1, there are no (additional) prime factors.
+do_factors(1, _, Factors) ->
+    Factors;
+%% If `Divisor' divides `Number', prepend it to `Factors', divide `Number' by
+%% `Divisor' and repeat.
+do_factors(Number, Divisor, Factors) when Number rem Divisor =:= 0 ->
+    do_factors(Number div Divisor, Divisor, [Divisor | Factors]);
+%% If 2 does not divide `Number', the next trial divisor is 3.
+do_factors(Number, 2, Factors) ->
+    do_factors(Number, 3, Factors);
+%% Otherwise, add 2 to `Divisor' and repeat.
+do_factors(Number, Divisor, Factors) ->
+    do_factors(Number, Divisor + 2, Factors).
+
+%%% ==================================================================== [ EOF ]
--- erlang/prime-factors/test/prime_factors_tests.erl
+++ erlang/prime-factors/test/prime_factors_tests.erl
@@ -8,65 +8,86 @@
 -include_lib("erl_exercism/include/exercism.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-
-
-
 '1_no_factors_test_'() ->
     {"no factors",
-     ?_assertEqual(lists:sort([]),
-		   lists:sort(prime_factors:factors(1)))}.
+        ?_assertEqual(
+            lists:sort([]),
+            lists:sort(prime_factors:factors(1))
+        )}.
 
 '2_prime_number_test_'() ->
     {"prime number",
-     ?_assertEqual(lists:sort([2]),
-		   lists:sort(prime_factors:factors(2)))}.
+        ?_assertEqual(
+            lists:sort([2]),
+            lists:sort(prime_factors:factors(2))
+        )}.
 
 '3_another_prime_number_test_'() ->
     {"another prime number",
-     ?_assertEqual(lists:sort([3]),
-		   lists:sort(prime_factors:factors(3)))}.
+        ?_assertEqual(
+            lists:sort([3]),
+            lists:sort(prime_factors:factors(3))
+        )}.
 
 '4_square_of_a_prime_test_'() ->
     {"square of a prime",
-     ?_assertEqual(lists:sort([3, 3]),
-		   lists:sort(prime_factors:factors(9)))}.
+        ?_assertEqual(
+            lists:sort([3, 3]),
+            lists:sort(prime_factors:factors(9))
+        )}.
 
 '5_product_of_first_prime_test_'() ->
     {"product of first prime",
-     ?_assertEqual(lists:sort([2, 2]),
-		   lists:sort(prime_factors:factors(4)))}.
+        ?_assertEqual(
+            lists:sort([2, 2]),
+            lists:sort(prime_factors:factors(4))
+        )}.
 
 '6_cube_of_a_prime_test_'() ->
     {"cube of a prime",
-     ?_assertEqual(lists:sort([2, 2, 2]),
-		   lists:sort(prime_factors:factors(8)))}.
+        ?_assertEqual(
+            lists:sort([2, 2, 2]),
+            lists:sort(prime_factors:factors(8))
+        )}.
 
 '7_product_of_second_prime_test_'() ->
     {"product of second prime",
-     ?_assertEqual(lists:sort([3, 3, 3]),
-		   lists:sort(prime_factors:factors(27)))}.
+        ?_assertEqual(
+            lists:sort([3, 3, 3]),
+            lists:sort(prime_factors:factors(27))
+        )}.
 
 '8_product_of_third_prime_test_'() ->
     {"product of third prime",
-     ?_assertEqual(lists:sort([5, 5, 5, 5]),
-		   lists:sort(prime_factors:factors(625)))}.
+        ?_assertEqual(
+            lists:sort([5, 5, 5, 5]),
+            lists:sort(prime_factors:factors(625))
+        )}.
 
 '9_product_of_first_and_second_prime_test_'() ->
     {"product of first and second prime",
-     ?_assertEqual(lists:sort([2, 3]),
-		   lists:sort(prime_factors:factors(6)))}.
+        ?_assertEqual(
+            lists:sort([2, 3]),
+            lists:sort(prime_factors:factors(6))
+        )}.
 
 '10_product_of_primes_and_non_primes_test_'() ->
     {"product of primes and non-primes",
-     ?_assertEqual(lists:sort([2, 2, 3]),
-		   lists:sort(prime_factors:factors(12)))}.
+        ?_assertEqual(
+            lists:sort([2, 2, 3]),
+            lists:sort(prime_factors:factors(12))
+        )}.
 
 '11_product_of_primes_test_'() ->
     {"product of primes",
-     ?_assertEqual(lists:sort([5, 17, 23, 461]),
-		   lists:sort(prime_factors:factors(901255)))}.
+        ?_assertEqual(
+            lists:sort([5, 17, 23, 461]),
+            lists:sort(prime_factors:factors(901255))
+        )}.
 
 '12_factors_include_a_large_prime_test_'() ->
     {"factors include a large prime",
-     ?_assertEqual(lists:sort([11, 9539, 894119]),
-		   lists:sort(prime_factors:factors(93819012551)))}.
+        ?_assertEqual(
+            lists:sort([11, 9539, 894119]),
+            lists:sort(prime_factors:factors(93819012551))
+        )}.
