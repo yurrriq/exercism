84bfa6bab063aa33e0b202b08dbd66df9e1e3277
Eric Bailey <eric@ericb.me>
2023-05-30 01:11:29 -0500

    [erlang] list-ops: submit

--- erlang/list-ops/src/list_ops.erl
+++ erlang/list-ops/src/list_ops.erl
@@ -1,20 +1,58 @@
 -module(list_ops).
 
 -export([append/2, concat/1, filter/2, length/1, map/2, foldl/3, foldr/3,
-	 reverse/1]).
+         reverse/1]).
 
-append(_List1, _List2) -> undefined.
+append(Xs, Ys) when is_list(Xs), is_list(Ys) ->
+    foldr(fun cons/2, Ys, Xs).
 
-concat(_List) -> undefined.
+concat([]) ->
+    [];
+concat(Lists=[List|_]) when is_list(List) ->
+    foldr(fun append/2, [], Lists).
 
-filter(_Function, _List) -> undefined.
+filter(Pred, List) when is_function(Pred, 1) ->
+    F = fun(Y, Acc) ->
+                case Pred(Y) of
+                    true ->
+                        [Y | Acc];
+                    false ->
+                        Acc
+                end
+        end,
+    do_foldr(F, [], List).
 
-length(_List) -> undefined.
+length([_|Tail]) ->
+    1 + ?MODULE:length(Tail);
+length([]) ->
+    0.
 
-map(_Function, _List) -> undefined.
+map(F, List) when is_function(F, 1) ->
+    do_map(F, List).
 
-foldl(_Function, _Start, _List) -> undefined.
+do_map(F, [X | Xs]) ->
+    [F(X) | do_map(F, Xs)];
+do_map(_F, []) ->
+    [].
 
-foldr(_Function, _Start, _List) -> undefined.
+foldl(F, Acc, List) when is_function(F, 2) ->
+    do_foldl(F, Acc, List).
 
-reverse(_List) -> undefined.
+do_foldl(F, Acc, [X | Xs]) ->
+    do_foldl(F, F(X, Acc), Xs);
+do_foldl(_F, Acc, []) ->
+    Acc.
+
+foldr(F, Acc, List) when is_function(F, 2) ->
+    do_foldr(F, Acc, List).
+
+do_foldr(F, Acc, [X | Xs]) ->
+    F(X, do_foldr(F, Acc, Xs));
+do_foldr(_F, Acc, []) ->
+    Acc.
+
+reverse(List) ->
+    foldl(fun cons/2, [], List).
+
+cons(Head, Tail) ->
+    [Head | Tail].
