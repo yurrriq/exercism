58ff833081f082504b786fff4ff5ab25a22f1148
Eric Bailey <eric@ericb.me>
2023-06-01 23:43:46 -0500

    [c] armstrong-numbers: reformat

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -2,29 +2,30 @@
 #include <math.h>
 #include <stdlib.h>
 
-bool is_armstrong_number(int candidate) {
-  if (candidate < 0) {
-    return false;
-  }
-
-  if (candidate < 10) {
-    return true;
-  }
-
-  unsigned int number = candidate;
-  unsigned int num_digits = log10(candidate) + 1;
-
-  int sum_pow_digits = 0;
-  while (number > 0) {
-    unsigned int digit = number % 10;
-    unsigned int pow_digit = 1;
-    for (unsigned int i = 0; i < num_digits; i++) {
-      pow_digit *= digit;
+bool is_armstrong_number(int candidate)
+{
+    if (candidate < 0) {
+        return false;
     }
 
-    sum_pow_digits += pow_digit;
-    number /= 10;
-  }
+    if (candidate < 10) {
+        return true;
+    }
 
-  return candidate == sum_pow_digits;
+    unsigned int number = candidate;
+    unsigned int num_digits = log10(candidate) + 1;
+
+    int sum_pow_digits = 0;
+    while (number > 0) {
+        unsigned int digit = number % 10;
+        unsigned int pow_digit = 1;
+        for (unsigned int i = 0; i < num_digits; i++) {
+            pow_digit *= digit;
+        }
+
+        sum_pow_digits += pow_digit;
+        number /= 10;
+    }
+
+    return candidate == sum_pow_digits;
 }
