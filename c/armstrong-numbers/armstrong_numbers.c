f20141e54ddb79eccd7823abe28fb2cc022c5034
Eric Bailey <eric@ericb.me>
2023-06-01 22:57:37 -0500

    [c] armstrong-numbers: resubmit

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -2,35 +2,29 @@
 #include <math.h>
 #include <stdlib.h>
 
-unsigned int ipow(unsigned int base, unsigned int exp);
-
 bool is_armstrong_number(int candidate) {
+  if (candidate < 0) {
+    return false;
+  }
+
   if (candidate < 10) {
     return true;
   }
 
-  unsigned int n = candidate;
+  unsigned int number = candidate;
   unsigned int num_digits = log10(candidate) + 1;
 
-  int sum = 0;
-  while (n > 0) {
-    sum += ipow(n % 10, num_digits);
-    n /= 10;
+  int sum_pow_digits = 0;
+  while (number > 0) {
+    unsigned int digit = number % 10;
+    unsigned int pow_digit = 1;
+    for (unsigned int i = 0; i < num_digits; i++) {
+      pow_digit *= digit;
+    }
+
+    sum_pow_digits += pow_digit;
+    number /= 10;
   }
 
-  return candidate == sum;
-}
-
-unsigned int ipow(unsigned int base, unsigned int exp) {
-  unsigned int result = 1;
-  for (;;) {
-    if (exp & 1)
-      result *= base;
-    exp >>= 1;
-    if (!exp)
-      break;
-    base *= base;
-  }
-
-  return result;
+  return candidate == sum_pow_digits;
 }
