e50a5f838e41488121aec2d96ffd53f2e0d976dd
Eric Bailey <eric@ericb.me>
2023-06-03 00:15:30 -0500

    [clojure] bird-watcher: submit

--- clojure/bird-watcher/src/bird_watcher.clj
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -1,22 +1,23 @@
 (ns bird-watcher)
 
 (def last-week
-  )
+  [0 2 5 3 7 8 4])
 
 (defn today [birds]
-  )
+  (last birds))
 
 (defn inc-bird [birds]
-  )
+  (update birds (- (count birds) 1) inc))
 
 (defn day-without-birds? [birds]
-  )
+  (boolean (some zero? birds)))
 
 (defn n-days-count [birds n]
-  )
+  (reduce + (take n birds)))
 
 (defn busy-days [birds]
-  )
+  (count (filter #(>= % 5) birds)))
 
 (defn odd-week? [birds]
-  )
+  (= [1 0 1 0 1 0 1]
+     birds))
