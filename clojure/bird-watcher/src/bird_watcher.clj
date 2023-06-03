a717090682ae12ae40e8e6cc2e27d8e329f16729
Eric Bailey <eric@ericb.me>
2023-06-03 00:01:04 -0500

    [clojure] bird-watcher: download

---
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,22 @@
+(ns bird-watcher)
+
+(def last-week
+  )
+
+(defn today [birds]
+  )
+
+(defn inc-bird [birds]
+  )
+
+(defn day-without-birds? [birds]
+  )
+
+(defn n-days-count [birds n]
+  )
+
+(defn busy-days [birds]
+  )
+
+(defn odd-week? [birds]
+  )
