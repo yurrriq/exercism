8d572a52cffaa0b52681c63d51900cff9baea9a7
Eric Bailey <eric@ericb.me>
2023-06-02 23:37:48 -0500

    [clojure] lucians-luscious-lasagna: download

---
+++ clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -0,0 +1,30 @@
+;; 👋🏽 Hi there! Welcome to the Clojure Track.
+;; The online test-runner is powered by babashka and the Small Clojure Interpreter (SCI).
+;; Almost all language features are supported, with the exception of low-level constructs
+;; like `deftype`, and certain Java classes. For more info, see:
+;; https://github.com/babashka/babashka#differences-with-clojure
+
+(ns lucians-luscious-lasagna)
+
+(def expected-time
+  ;; Define the var here
+  )
+
+(defn remaining-time
+  "Takes the actual time in minutes the lasagna has been in the oven,
+   and returns how many minutes the lasagna still has to remain in the oven."
+  [actual-time]
+  ;; Define the function body here
+  )
+
+(defn prep-time
+  "Takes the number of layers added to the lasagna,
+   and returns how many minutes you spent preparing the lasagna"
+  [num-layers]
+  )
+
+(defn total-time
+  "Takes the number of layers of lasagna and the actual time in minutes it has been in the oven.
+   Returns how many minutes in total you've worked on cooking the lasagna"
+  [num-layers actual-time]
+  )
