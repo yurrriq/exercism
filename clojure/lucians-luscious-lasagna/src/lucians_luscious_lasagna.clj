1be4ad889a90d69f0e7e6aeb8da53e79f2d603c9
Eric Bailey <eric@ericb.me>
2023-06-02 23:56:34 -0500

    [clojure] lucians-luscious-lasagna: submit

--- clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
+++ clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -1,30 +1,24 @@
-;; 👋🏽 Hi there! Welcome to the Clojure Track.
-;; The online test-runner is powered by babashka and the Small Clojure Interpreter (SCI).
-;; Almost all language features are supported, with the exception of low-level constructs
-;; like `deftype`, and certain Java classes. For more info, see:
-;; https://github.com/babashka/babashka#differences-with-clojure
-
 (ns lucians-luscious-lasagna)
 
 (def expected-time
-  ;; Define the var here
-  )
+  "According to the cooking book, the expected oven time in minutes is 40."
+  40)
 
 (defn remaining-time
-  "Takes the actual time in minutes the lasagna has been in the oven,
-   and returns how many minutes the lasagna still has to remain in the oven."
+  "Given the actual time in minutes the lasagna has been in the oven,
+   return how many minutes the lasagna still has to remain in the oven."
   [actual-time]
-  ;; Define the function body here
-  )
+  (- expected-time actual-time))
 
 (defn prep-time
-  "Takes the number of layers added to the lasagna,
-   and returns how many minutes you spent preparing the lasagna"
+  "Given the number of layers added to the lasagna,
+   return how many minutes you spent preparing the lasagna."
   [num-layers]
-  )
+  (* num-layers 2))
 
 (defn total-time
-  "Takes the number of layers of lasagna and the actual time in minutes it has been in the oven.
+  "Given the number of layers of lasagna and the actual time in minutes it has been in the oven,
    Returns how many minutes in total you've worked on cooking the lasagna"
   [num-layers actual-time]
-  )
+  (+ (prep-time num-layers)
+     actual-time))
