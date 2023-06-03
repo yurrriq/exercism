f0447238930d440dcfc2897db41e0dd9eca51f88
Eric Bailey <eric@ericb.me>
2023-06-03 00:53:46 -0500

    [clojure] flatten-array: resubmit

--- clojure/flatten-array/src/flatten_array.clj
+++ clojure/flatten-array/src/flatten_array.clj
@@ -1,9 +1,6 @@
 (ns flatten-array)
 
 (defn flatten [arr]
-  (mapcat (fn [x]
-            (cond
-              (sequential? x) (flatten x)
-              (some? x) [x]
-              :else []))
-          arr))
+  (->> (tree-seq sequential? seq arr)
+       (filter (complement sequential?))
+       (filter some?)))
