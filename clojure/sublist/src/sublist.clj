2efec80f04a358f292c322eeb9798639ef70dee5
Eric Bailey <eric@ericb.me>
2023-06-04 02:15:37 -0500

    [clojure] sublist: resubmit

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,24 +1,13 @@
 (ns sublist)
 
-(defn- starts-with? [xs ys]
-  (or (empty? xs)
-      (and (seq? (seq ys))
-           (= (first xs) (first ys))
-           (recur (rest xs) (rest ys)))))
-
-(defn- tails [coll]
-  (reductions (fn [s _] (rest s)) (seq coll) coll))
-
 (defn- sublist? [xs ys]
-  (or (empty? xs)
-      (and (seq? (seq ys))
-           (some #(starts-with? (seq xs) %) (butlast (tails ys))))))
+  (some #{xs} (partition (count xs) 1 ys)))
 
 (defn classify
   "Determine the relation between `list1` and `list2`."
   [list1 list2]
   (cond
-    (= list1 list2) :equal
+    (= list1 list2)        :equal
     (sublist? list1 list2) :sublist
     (sublist? list2 list1) :superlist
-    :else :unequal))
+    :else                  :unequal))
