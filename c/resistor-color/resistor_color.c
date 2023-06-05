8fffbe412bd433ea46168dd41bc6cb69289f880e
Eric Bailey <eric@ericb.me>
2023-06-04 23:27:46 -0500

    [c] resistor-color: submit

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,13 @@
 #include "resistor_color.h"
+
+static resistor_band_t all_colors[10] = {COLORS};
+
+resistor_band_t *colors()
+{
+    return all_colors;
+}
+
+int color_code(resistor_band_t color)
+{
+    return color;
+}
--- c/resistor-color/resistor_color.h
+++ c/resistor-color/resistor_color.h
@@ -1,8 +1,13 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
-typedef enum {
+#define COLORS BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
 
+typedef enum {
+    COLORS
 } resistor_band_t;
 
+int color_code(resistor_band_t);
+
+resistor_band_t *colors();
 #endif
