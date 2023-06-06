3a36f09cdc2ce3c267265de7a029bee982fa83f9
Eric Bailey <eric@ericb.me>
2023-06-06 01:34:23 -0500

    [elixir] hamming: submit

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -9,5 +9,26 @@ defmodule Hamming do
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand2) do
+    hamming_distance(0, strand1, strand2)
+  end
+
+  defp hamming_distance(sum, [], []) do
+    {:ok, sum}
+  end
+
+  defp hamming_distance(_, [], _) do
+    {:error, "strands must be of equal length"}
+  end
+
+  defp hamming_distance(_, _, []) do
+    {:error, "strands must be of equal length"}
+  end
+
+  defp hamming_distance(sum, [ha | ta], [hb | tb]) when ha == hb do
+    hamming_distance(sum, ta, tb)
+  end
+
+  defp hamming_distance(sum, [_ | ta], [_ | tb]) do
+    hamming_distance(sum + 1, ta, tb)
   end
 end
