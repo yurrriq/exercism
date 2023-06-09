1b003fe7cb8f412118a045e64894d2f7c8bdffcb
Eric Bailey <eric@ericb.me>
2023-06-09 03:42:18 -0500

    [elixir] protein-translation: resubmit
    https://elixir-lang.org/getting-started/sigils.html

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,29 +1,23 @@
 defmodule ProteinTranslation do
   @amino_acids %{
-    "UGU" => "Cysteine",
-    "UGC" => "Cysteine",
-    "UUA" => "Leucine",
-    "UUG" => "Leucine",
-    "AUG" => "Methionine",
-    "UUU" => "Phenylalanine",
-    "UUC" => "Phenylalanine",
-    "UCU" => "Serine",
-    "UCC" => "Serine",
-    "UCA" => "Serine",
-    "UCG" => "Serine",
-    "UGG" => "Tryptophan",
-    "UAU" => "Tyrosine",
-    "UAC" => "Tyrosine",
-    "UAA" => "STOP",
-    "UAG" => "STOP",
-    "UGA" => "STOP"
-  }
+                 ~w(AUG) => "Methionine",
+                 ~w(UUU UUC) => "Phenylalanine",
+                 ~w(UUA UUG) => "Leucine",
+                 ~w(UCU UCC UCA UCG) => "Serine",
+                 ~w(UAU UAC) => "Tyrosine",
+                 ~w(UGU UGC) => "Cysteine",
+                 ~w(UGG) => "Tryptophan",
+                 ~w(UAA UAG UGA) => "STOP"
+               }
+               |> Enum.reduce(%{}, fn {codons, protein}, outer ->
+                 Enum.reduce(codons, outer, &Map.put(&2, &1, protein))
+               end)
 
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
-  def of_rna(rna), do: of_rna(rna, [])
+  def of_rna(rna, acc \\ [])
 
   def of_rna(<<x, y, z, tail::binary>>, acc) do
     case of_codon(<<x, y, z>>) do
