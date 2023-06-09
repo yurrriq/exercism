2f01b65ce6523c1300e10e9df38adc2b1c17daca
Eric Bailey <eric@ericb.me>
2023-06-09 03:12:23 -0500

    [elixir] protein-translation: submit

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,10 +1,40 @@
 defmodule ProteinTranslation do
+  @amino_acids %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
-  def of_rna(rna) do
+  def of_rna(rna), do: of_rna(rna, [])
+
+  def of_rna(<<x, y, z, tail::binary>>, acc) do
+    case of_codon(<<x, y, z>>) do
+      {:ok, "STOP"} -> of_rna("", acc)
+      {:ok, protein} -> of_rna(tail, [protein | acc])
+      {:error, "invalid codon"} -> {:error, "invalid RNA"}
+    end
   end
+
+  def of_rna("", acc), do: {:ok, Enum.reverse(acc)}
+  def of_rna(_, _), do: {:error, "invalid RNA"}
 
   @doc """
   Given a codon, return the corresponding protein
@@ -29,5 +59,10 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def of_codon(codon) do
+    case Map.get(@amino_acids, codon) do
+      "STOP" -> {:ok, "STOP"}
+      nil -> {:error, "invalid codon"}
+      protein -> {:ok, protein}
+    end
   end
 end
