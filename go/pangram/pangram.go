c1f20f207d93a5cd95bfb98444778935fc31067e
Eric Bailey <eric@ericb.me>
2023-06-09 17:43:58 -0500

    [go] pangram: submit

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,24 @@
 package pangram
 
+import (
+	"unicode"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	seen := make(map[rune]struct{}, 26)
+
+	for _, letter := range input {
+		letter = unicode.ToLower(letter)
+		if _, ok := seen[letter]; !ok {
+			seen[letter] = struct{}{}
+		}
+	}
+
+	for letter := 'a'; letter <= 'z'; letter++ {
+		if _, ok := seen[letter]; !ok {
+			return false
+		}
+	}
+
+	return true
 }
