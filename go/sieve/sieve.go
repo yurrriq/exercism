fb7ff7e008a8066c640481fbf7a58efde9b09d0d
Eric Bailey <eric@ericb.me>
2023-06-10 01:53:17 -0500

    [go] sieve: submit

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,37 @@
 package sieve
 
+import (
+	"math"
+	"sort"
+)
+
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	if limit <= 1 {
+		return nil
+	}
+
+	sieve := make(map[int]struct{}, limit-1)
+	for i := 2; i <= limit; i++ {
+		sieve[i] = struct{}{}
+	}
+
+	sqrtLimit := int(math.Ceil(math.Sqrt(float64(limit))))
+	for i := 2; i <= sqrtLimit; i++ {
+		if _, present := sieve[i]; present {
+			for j := i * i; j <= limit; j += i {
+				delete(sieve, j)
+			}
+		}
+	}
+
+	keys := make([]int, 0, len(sieve))
+	for k := range sieve {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
+	return keys
 }
