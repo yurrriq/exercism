9ffae29411a76735653589429d9164838c5aa8bb
Eric Bailey <eric@ericb.me>
2023-06-09 23:00:33 -0500

    [go] simple-linked-list: submit
    Avoid a memory leak by setting l.head.next = nil during Pop().

--- go/simple-linked-list/simple_linked_list.go
+++ go/simple-linked-list/simple_linked_list.go
@@ -1,27 +1,65 @@
 package linkedlist
 
-// Define the List and Element types here.
+import (
+	"errors"
+)
 
-func New(elements []int) *List {
-	panic("Please implement the New function")
+type Node struct {
+	value int
+	next  *Node
 }
 
-func (l *List) Size() int {
-	panic("Please implement the Size function")
+type List struct {
+	head *Node
+	size int
+}
+
+func New(elements []int) *List {
+	list := &List{}
+
+	for _, element := range elements {
+		list.Push(element)
+	}
+
+	return list
+}
+
+func (l *List) Size() (count int) {
+	return l.size
 }
 
 func (l *List) Push(element int) {
-	panic("Please implement the Push function")
+	l.head = &Node{value: element, next: l.head}
+	l.size++
 }
 
-func (l *List) Pop() (int, error) {
-	panic("Please implement the Pop function")
+func (l *List) Pop() (value int, err error) {
+	if l.size == 0 {
+		return -1, errors.New("empty list")
+	}
+
+	value, l.head, l.head.next = l.head.value, l.head.next, nil
+	l.size--
+
+	return value, nil
 }
 
 func (l *List) Array() []int {
-	panic("Please implement the Array function")
+	arr := make([]int, l.size)
+
+	for i, head := l.size-1, l.head; head != nil; i, head = i-1, head.next {
+		arr[i] = head.value
+	}
+
+	return arr
 }
 
 func (l *List) Reverse() *List {
-	panic("Please implement the Reverse function")
+	list := New(nil)
+
+	for value, err := l.Pop(); err == nil; value, err = l.Pop() {
+		list.Push(value)
+	}
+
+	return list
 }
