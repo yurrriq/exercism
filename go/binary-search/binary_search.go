4feb94287023cafa3b35bab874b0e6f3b0af996a
Eric Bailey <eric@ericb.me>
2023-06-10 19:25:28 -0500

    [go] binary-search: submit

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,5 +1,35 @@
 package binarysearch
 
-func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+import (
+	"golang.org/x/exp/constraints"
+)
+
+// SearchInts searches for an int in a slice of ints by repeatedly splitting the
+// slice in half, only keeping the half which contains desired int.
+func SearchInts(haystack []int, needle int) int {
+	index, _ := binarySearch(haystack, needle, 0)
+	return index
+}
+
+func binarySearch[T constraints.Ordered](haystack []T, needle T, iterations int) (int, int) {
+	length := len(haystack)
+	if length == 0 {
+		return -1, iterations
+	}
+
+	pivot := length / 2
+	middle := haystack[pivot]
+
+	switch {
+	case needle < middle:
+		return binarySearch(haystack[:pivot], needle, iterations+1)
+	case needle > middle:
+		index, iterations := binarySearch(haystack[pivot+1:], needle, iterations+1)
+		if index >= 0 {
+			return index + pivot + 1, iterations
+		}
+		return -1, iterations
+	default:
+		return pivot, iterations
+	}
 }
