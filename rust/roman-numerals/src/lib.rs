9d62be91ed9d1add30040ad6dfb9cde43d29d5a3
Eric Bailey <eric@ericb.me>
2023-06-11 21:17:55 -0500

    [rust] roman-numerals: submit

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -1,17 +1,41 @@
 use std::fmt::{Display, Formatter, Result};
 
-pub struct Roman;
+static ARABIC_TO_ROMAN: [(usize, &str); 13] = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+];
+
+pub struct Roman(usize);
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!(
-            "Return a roman-numeral string representation of the Roman object"
-        );
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut to_convert = self.0;
+        let mut to_display = String::new();
+
+        for (arabic, roman) in ARABIC_TO_ROMAN {
+            if arabic <= to_convert {
+                to_display.push_str(&roman.repeat(to_convert / arabic));
+                to_convert %= arabic;
+            }
+        }
+
+        write!(f, "{}", to_display)
     }
 }
 
-impl From<u32> for Roman {
-    fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{num}' number");
+impl From<usize> for Roman {
+    fn from(num: usize) -> Self {
+        Self(num)
     }
 }
