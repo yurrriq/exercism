14995718ac709ff6079f70f0682b4def7bcbf8cc
Eric Bailey <eric@ericb.me>
2023-06-11 23:48:40 -0500

    [emacs-lisp] gigasecond: resubmit

--- emacs-lisp/gigasecond/gigasecond.el
+++ emacs-lisp/gigasecond/gigasecond.el
@@ -9,13 +9,23 @@
 
 ;;; Code:
 
+(defconst +gigasecond+ (seconds-to-time (expt 10 9))
+  "One gigasecond (10^9 seconds) as a Lisp timestamp.")
+
+(defmacro -> (&rest body)
+  "The Thrush combinator from To Mock a Mockingbird."
+  (let ((result (pop body)))
+    (dolist (form body result)
+      (setq result (append (list (car form) result)
+                           (cdr form))))))
+
 (defun from (seconds minutes hours day-of-month month year)
-  (set-time-zone-rule t)
-  (butlast
-   (decode-time
-    (time-add (seconds-to-time (expt 10 9))
-              (encode-time seconds minutes hours day-of-month month year)))
-   3))
+  "Calculate the date one gigasecond (10^9 seconds) from the given date."
+  (-> (list seconds minutes hours day-of-month month year nil nil t)
+      (encode-time)
+      (time-add +gigasecond+)
+      (decode-time t)
+      (butlast 3)))
 
 (provide 'gigasecond)
 ;;; gigasecond.el ends here
