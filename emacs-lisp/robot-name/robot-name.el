aeed6c0be96187219641c795e2c0d90322455053
Eric Bailey <eric@ericb.me>
2023-06-11 23:50:47 -0500

    [emacs-lisp] robot-name: download

---
+++ emacs-lisp/robot-name/robot-name.el
@@ -0,0 +1,25 @@
+;;; robot-name.el --- Robot Name (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Build a robot with a name like AA000, that can be reset
+;; to a new name. Instructions are in README.md
+;;
+
+(defun build-robot ()
+  "Build a new robot with a random name."
+;;; Code:
+)
+
+(defun robot-name (robot)
+  "Get the ROBOT's name."
+;;; Code:
+)
+
+(defun reset-robot (robot)
+  "Reset the name of ROBOT.  Factory reset!"
+;;; Code:
+)
+
+(provide 'robot-name)
+;;; robot-name.el ends here
