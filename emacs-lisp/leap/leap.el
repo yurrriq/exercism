d2fd990bffc8973bca377900dc41196115ee1a51
Eric Bailey <eric@ericb.me>
2023-06-11 21:44:09 -0500

    [emacs-lisp] leap: submit

--- emacs-lisp/leap/leap.el
+++ emacs-lisp/leap/leap.el
@@ -2,9 +2,13 @@
 
 ;;; Commentary:
 
-(defun leap-year-p (year)
 ;;; Code:
-)
+
+(defun leap-year-p (year)
+  "Determine with `year' is a leap year."
+  (and (zerop (% year 4))
+       (or (not (zerop (% year 100)))
+           (zerop (% year 400)))))
 
 (provide 'leap-year-p)
 ;;; leap.el ends here
