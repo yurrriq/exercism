75a5cc06fabadb7ac491bd25a33cca384e2475e7
Eric Bailey <eric@ericb.me>
2023-06-12 01:32:50 -0500

    [python] sum-of-multiples: resubmit
    Use set comprehension and simplify.

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,12 +1,9 @@
 def sum_of_multiples(limit, values):
     return sum(
-        set(
-            [
-                multiple
-                for multiples in [
-                    range(base, limit, base) for base in values if base != 0
-                ]
-                for multiple in multiples
-            ]
-        )
+        {
+            multiple
+            for base in values
+            if base != 0
+            for multiple in range(base, limit, base)
+        }
     )
