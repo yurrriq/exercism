79e3b298bc8cce230242c37506438668aef091b8
Eric Bailey <eric@ericb.me>
2023-06-11 22:41:39 -0500

    [emacs-lisp] matching-brackets: submit

--- emacs-lisp/matching-brackets/matching-brackets.el
+++ emacs-lisp/matching-brackets/matching-brackets.el
@@ -4,11 +4,16 @@
 
 ;;; Code:
 
+(require 'rx)
+(require 'subr-x)
 
 (defun is-paired (value)
-  (error
-   "Delete this S-Expression and write your own implementation"))
-
+  (let ((only-brackets (replace-regexp-in-string (rx (not (in "[]{}()"))) "" value)))
+    (or (string-empty-p only-brackets)
+        (let ((pairs-removed (replace-regexp-in-string (rx (or "[]" "{}" "()")) "" only-brackets)))
+          (or (string-empty-p pairs-removed)
+              (when (not (string= only-brackets pairs-removed))
+                (is-paired pairs-removed)))))))
 
 (provide 'matching-brackets)
 ;;; matching-brackets.el ends here
