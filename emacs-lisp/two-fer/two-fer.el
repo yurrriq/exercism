56aab0d3380ec6184e8edd9b24801ba5f80d55a6
Eric Bailey <eric@ericb.me>
2023-06-11 22:52:41 -0500

    [emacs-lisp] two-fer: submit

--- emacs-lisp/two-fer/two-fer.el
+++ emacs-lisp/two-fer/two-fer.el
@@ -2,9 +2,10 @@
 
 ;;; Commentary:
 
-(defun two-fer (&optional name)
 ;;; Code:
-)
+
+(defun two-fer (&optional name)
+  (format "One for %s, one for me." (or name "you")))
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
