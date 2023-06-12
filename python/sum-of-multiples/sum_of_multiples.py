a0f4e58ff7fd71e43d8008e39bc7e9d3b4158c0e
Eric Bailey <eric@ericb.me>
2023-06-12 01:29:39 -0500

    [python] sum-of-multiples: submit

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,12 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit, values):
+    return sum(
+        set(
+            [
+                multiple
+                for multiples in [
+                    range(base, limit, base) for base in values if base != 0
+                ]
+                for multiple in multiples
+            ]
+        )
+    )
