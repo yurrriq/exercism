652c1a8424cfe7793aec325c8a3d80f6957b5b08
Eric Bailey <eric@ericb.me>
2023-06-12 00:12:17 -0500

    [emacs-lisp] robot-name: submit

--- emacs-lisp/robot-name/robot-name.el
+++ emacs-lisp/robot-name/robot-name.el
@@ -6,20 +6,33 @@
 ;; to a new name. Instructions are in README.md
 ;;
 
+;;; Code:
+
+(defun random-upper-case-alpha-char ()
+  (nth (random 26) (number-sequence ?A ?Z)))
+
+(defun random-digit-char ()
+  (nth (random 10) (number-sequence ?0 ?9)))
+
+(defun random-name ()
+  "Return a random robot name of the form `[A-Z]{2}[0-9]{3}'."
+  (concat (list (random-upper-case-alpha-char)
+                (random-upper-case-alpha-char)
+                (random-digit-char)
+                (random-digit-char)
+                (random-digit-char))))
+
 (defun build-robot ()
   "Build a new robot with a random name."
-;;; Code:
-)
+  (list :name (random-name)))
 
 (defun robot-name (robot)
   "Get the ROBOT's name."
-;;; Code:
-)
+  (plist-get robot :name))
 
 (defun reset-robot (robot)
   "Reset the name of ROBOT.  Factory reset!"
-;;; Code:
-)
+  (plist-put robot :name nil))
 
 (provide 'robot-name)
 ;;; robot-name.el ends here
