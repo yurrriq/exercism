ee63db193892512250599df302134bd3cda025ea
Eric Bailey <eric@ericb.me>
2023-06-11 22:50:50 -0500

    [emacs-lisp] two-fer: download

---
+++ emacs-lisp/two-fer/two-fer.el
@@ -0,0 +1,10 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun two-fer (&optional name)
+;;; Code:
+)
+
+(provide 'two-fer)
+;;; two-fer.el ends here
