f574f143c211b27488d1aeefc28cb163cfbb2b00
Eric Bailey <eric@ericb.me>
2023-06-12 00:57:54 -0500

    [python] etl: resubmit
    Use dict comprehension and type hints.

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,10 +1,7 @@
 from functools import reduce
 
 
-def transform(legacy_data):
-    return reduce(
-        lambda new_data, score: new_data
-        | dict.fromkeys([letter.lower() for letter in legacy_data[score]], score),
-        legacy_data,
-        {},
-    )
+def transform(legacy_data: dict[int, list[str]]) -> dict[str, int]:
+    return {
+        letter.lower(): score for score in legacy_data for letter in legacy_data[score]
+    }
