e1e068d578f052e8beeb5a52d1fe449192f3feb4
Eric Bailey <eric@ericb.me>
2023-06-13 01:55:02 -0500

    [python] saddle-points: submit

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,23 @@
 def saddle_points(matrix):
-    pass
+    if matrix == []:
+        return []
+
+    width = len(matrix[0])
+    if any(len(row) != width for row in matrix):
+        raise ValueError("irregular matrix")
+
+    height = len(matrix)
+    row_maxes = {
+        (x, y)
+        for (y, row) in enumerate(matrix)
+        for (x, val) in enumerate(row)
+        if matrix[y][x] == max(row)
+    }
+    col_mins = {
+        (x, y)
+        for y in range(height)
+        for x in range(width)
+        if matrix[y][x] == (col_min := min(matrix[y][x] for y in range(height)))
+    }
+
+    return [{"column": x + 1, "row": y + 1} for (x, y) in (row_maxes & col_mins)]
