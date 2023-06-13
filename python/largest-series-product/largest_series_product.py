8359a158e443b1006e2576d97d58d740fe20694c
Eric Bailey <eric@ericb.me>
2023-06-13 02:43:21 -0500

    [python] largest-series-product: submit

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,2 +1,31 @@
+from functools import reduce
+from operator import mul
+
+
 def largest_product(series, size):
-    pass
+    if size < 1:
+        raise ValueError("span must not be negative")
+
+    if size > len(series):
+        raise ValueError("span must be smaller than string length")
+
+    numbers = [digit_to_int(c) for c in list(series)]
+    chunks = chunks_of(numbers, size)
+
+    return max(product(chunk) for chunk in chunks)
+
+
+def product(lst):
+    return reduce(mul, lst, 1)
+
+
+def chunks_of(lst, n):
+    for i in range(0, len(lst) - n + 1):
+        yield lst[i : i + n]
+
+
+def digit_to_int(digit):
+    if not digit.isdigit():
+        raise ValueError("digits input must only contain digits")
+
+    return ord(digit) - 48
