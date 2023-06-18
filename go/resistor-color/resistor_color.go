ec7947c1341f1dc72bc1ee34fa61bc8fbe645ccc
Eric Bailey <eric@ericb.me>
2023-06-18 05:10:39 -0500

    [go] resistor-color: submit

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -1,12 +1,40 @@
 // Package resistorcolor implements the Resistor Color exercise.
 package resistorcolor
 
-// Colors should return the list of all colors.
+// A ResistorColor is a color code to denote a resistance value.
+type ResistorColor int
+
+// ResistorColors
+const (
+	Black ResistorColor = iota
+	Brown
+	Red
+	Orange
+	Yellow
+	Green
+	Blue
+	Violet
+	Grey
+	White
+)
+
+func (color ResistorColor) String() string {
+	return Colors()[color]
+}
+
+// Colors returns the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 }
 
 // ColorCode returns the resistance value of the given color.
-func ColorCode(_ string) int {
-	panic("Please implement the ColorCode function")
+func ColorCode(color string) int {
+	var code ResistorColor
+	for code = Black; code <= White; code++ {
+		if color == code.String() {
+			return int(code)
+		}
+	}
+
+	return -1
 }
