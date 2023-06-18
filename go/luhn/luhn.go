766aaa967d436f53ab8c1f2e9f4a078fa0bd5b70
Eric Bailey <eric@ericb.me>
2023-06-18 03:02:22 -0500

    [go] luhn: submit

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,30 @@
 package luhn
 
+import (
+	"unicode"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	runes, length, sum := []rune(id), 0, 0
+	for i := len(runes) - 1; i >= 0; i-- {
+		rune := runes[i]
+		switch {
+		case unicode.IsSpace(rune):
+			continue
+		case !unicode.IsDigit(rune):
+			return false
+		default:
+			digit := int(rune) - '0'
+			if (length & 1) == 1 {
+				digit *= 2
+			}
+			if digit > 9 {
+				digit -= 9
+			}
+			length++
+			sum += digit
+		}
+	}
+
+	return (length > 1) && (sum%10 == 0)
 }
