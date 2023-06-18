9182dc156eead834b566bb179048b2234d085116
Eric Bailey <eric@ericb.me>
2023-06-18 04:20:26 -0500

    [go] luhn: resubmit

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,30 +1,32 @@
+// Package luhn implements the Luhn exercise.
 package luhn
 
 import (
 	"unicode"
 )
 
+// Valid implements the Luhn algorithm to validate an identification number.
 func Valid(id string) bool {
-	runes, length, sum := []rune(id), 0, 0
-	for i := len(runes) - 1; i >= 0; i-- {
-		rune := runes[i]
+	var length, sum int
+	roons := []rune(id)
+	for i := len(roons) - 1; i >= 0; i-- {
+		roon := roons[i]
 		switch {
-		case unicode.IsSpace(rune):
+		case unicode.IsSpace(roon):
 			continue
-		case !unicode.IsDigit(rune):
+		case !unicode.IsDigit(roon):
 			return false
 		default:
-			digit := int(rune) - '0'
-			if (length & 1) == 1 {
+			digit := int(roon) - '0'
+			if length&1 == 1 {
 				digit *= 2
 			}
 			if digit > 9 {
 				digit -= 9
 			}
-			length++
 			sum += digit
+			length++
 		}
 	}
-
-	return (length > 1) && (sum%10 == 0)
+	return length > 1 && sum%10 == 0
 }
