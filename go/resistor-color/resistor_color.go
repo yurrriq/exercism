00f792ded7af6cf4676fe908e5627cab9fc96a2e
Eric Bailey <eric@ericb.me>
2023-06-18 04:49:20 -0500

    [go] resistor-color: download

---
+++ go/resistor-color/resistor_color.go
@@ -0,0 +1,12 @@
+// Package resistorcolor implements the Resistor Color exercise.
+package resistorcolor
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	panic("Please implement the Colors function")
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(_ string) int {
+	panic("Please implement the ColorCode function")
+}
