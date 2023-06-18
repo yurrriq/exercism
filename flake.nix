471361323f0eb87f8d06acea3e5407c525a74da6
Eric Bailey <eric@ericb.me>
2023-06-18 04:34:16 -0500

    [go] drop subflake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,23 @@
 {
-  description = "A very basic flake";
+  description = "Solutions to Exercism exercises";
 
   inputs = {
+    emacs-overlay = {
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs-stable";
+      };
+      url = "github:nix-community/emacs-overlay";
+    };
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.05";
     pre-commit-hooks = {
       inputs = {
         flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs-stable";
       };
       url = "github:cachix/pre-commit-hooks.nix";
     };
@@ -17,10 +27,15 @@
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, pre-commit-hooks, treefmt-nix, ... }:
+  outputs = { self, emacs-overlay, flake-utils, nixpkgs, pre-commit-hooks, treefmt-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          overlays = [
+            emacs-overlay.overlay
+          ];
+          inherit system;
+        };
       in
       {
         checks = {
@@ -36,12 +51,36 @@
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          buildInputs = with pkgs; [
-            exercism
-          ];
+        devShells = {
+          default = with pkgs; mkShell {
+            FONTCONFIG_FILE = makeFontsConf {
+              fontDirectories = [
+                (nerdfonts.override { fonts = [ "Iosevka" ]; })
+              ];
+            };
+            buildInputs = [
+              exercism
+              rnix-lsp
+            ];
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+          };
+
+          go = self.devShells.${system}.default.overrideAttrs (super: {
+            buildInputs = super.buildInputs ++ (with pkgs; [
+              (
+                emacsWithPackagesFromUsePackage {
+                  alwaysEnsure = true;
+                  config = ./go/emacs.el;
+                }
+              )
+              go
+              gopls
+              gotools
+              revive
+            ]);
+          });
         };
+
         formatter = treefmt-nix.lib.mkWrapper pkgs {
           projectRootFile = "flake.nix";
           programs = {
