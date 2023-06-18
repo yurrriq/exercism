17b96e51a69d7598c7e80071be5a6af17364232c
Eric Bailey <eric@ericb.me>
2023-06-18 01:59:24 -0500

    [go] collatz-conjecture: submit

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,27 @@
 package collatzconjecture
 
+const ErrInvalidNumber CollatzError = "Only strictly positive numbers are allowed"
+
+type CollatzError string
+
+func (err CollatzError) Error() string {
+	return string(err)
+}
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return -1, ErrInvalidNumber
+	}
+
+	var steps int
+	for steps = 0; n != 1; steps++ {
+		if 0 == n&1 {
+			n /= 2
+		} else {
+			n *= 3
+			n++
+		}
+	}
+
+	return steps, nil
 }
