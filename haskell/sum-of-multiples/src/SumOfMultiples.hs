4b7e6020680e26882b37a07c10c088b1b7d45f98
Eric Bailey <eric@ericb.me>
2023-06-19 02:20:00 -0500

    [haskell] sum-of-multiples: resubmit

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -10,15 +10,13 @@
 -- Calculating the sum of multiples.
 module SumOfMultiples where
 
--- | Given a number, returns the sum of all the multiples of 3 or 5
--- (@'sumOfMultiples' [3,5]@) up to but not including that number.
-sumOfMultiplesDefault :: Integral a => a -> a
-sumOfMultiplesDefault = sumOfMultiples [3, 5]
-
 -- | Given a list of numbers and a number, returns the sum of all the multiples
 -- of any of the numbers in the list up to but not including that number.
 sumOfMultiples :: (Integral a) => [a] -> a -> a
-sumOfMultiples = (sum .) . (. fromOneToExclusive) . anyDivides
+sumOfMultiples multiples =
+  sum
+    . anyDivides multiples
+    . fromOneToExclusive
 
 -- | Given a number, @x@, returns the list of numbers from 1 to @x - 1@.
 fromOneToExclusive :: Integral a => a -> [a]
@@ -26,21 +24,15 @@ fromOneToExclusive = enumFromTo 1 . pred
 
 -- | Given two lists of numbers, @xs@ and @ys@, returns a possibly empty list
 -- of every @y@ in @ys@ such that some @x@ in @xs@ 'divides' @y@.
-
--- |
--- __List comprehension__:
 --
--- > anyDivides xs ys = [y | y <- ys, any (`divides` y) xs]
---
--- __LaTeX__:
---
--- @
--- \{ y : y \in Y \quad \land \quad \exists \, x \; ( x \in X \land x\ |\ y ) \;\}
--- @
+-- \[
+--   \{ y \in Y \colon \exists x \in X(x|y) \}
+-- \]
 anyDivides :: Integral a => [a] -> [a] -> [a]
-anyDivides = filter . flip (any . flip divides)
+anyDivides xs ys = [y | y <- ys, any (`divides` y) xs]
 
 -- | Given two numbers, returns @True@ if the first divides the second,
 -- otherwise @False@.
 divides :: Integral a => a -> a -> Bool
+0 `divides` _ = False
 d `divides` n = n `rem` d == 0
