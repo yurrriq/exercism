901f760cb91539a3a8b3d7085196fe8aaaddc84b
Eric Bailey <eric@ericb.me>
2023-06-18 22:53:28 -0500

    [go] nucleotide-count: submit

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,19 +1,53 @@
+// Package dna implements the Nucleotide Count exercise.
 package dna
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
-type Histogram
+type Histogram map[Nucleotide]uint
+
+// A Nucleotide is a rune.
+type Nucleotide rune
+
+// 'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' for thymine.
+const (
+	Adenine  Nucleotide = 'A'
+	Cytosine            = 'C'
+	Guanine             = 'G'
+	Thymine             = 'T'
+)
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type DNA string
+
+// ErrInvalidNucleotide is thrown when an invalid nucleotide is encountered.
+const ErrInvalidNucleotide NucleotideError = "Invalid nucleotide"
+
+// A NucleotideError is a string.
+type NucleotideError string
+
+// Error describes a DNSError as a string.
+func (err NucleotideError) Error() string {
+	return string(err)
+}
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
-// Returns an error if d contains an invalid nucleotide.
-///
-// Counts is a method on the DNA type. A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-// Here, the Counts method has a receiver of type DNA named d.
-func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
-	return h, nil
+// Returns an error if dna contains an invalid nucleotide.
+func (dna DNA) Counts() (Histogram, error) {
+	counts := Histogram{Adenine: 0, Cytosine: 0, Guanine: 0, Thymine: 0}
+	roons := []rune(dna)
+	for _, roon := range roons {
+		switch roon {
+		case 'A':
+			counts[Adenine]++
+		case 'C':
+			counts[Cytosine]++
+		case 'G':
+			counts[Guanine]++
+		case 'T':
+			counts[Thymine]++
+		default:
+			return nil, ErrInvalidNucleotide
+		}
+	}
+
+	return counts, nil
 }
