24c397aad07075693c9c7f9a5bf54167ea5ca7d1
Eric Bailey <eric@ericb.me>
2023-06-19 23:50:55 -0500

    [haskell] saddle-points: submit

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -1,6 +1,16 @@
-module Matrix (saddlePoints) where
+module Matrix
+  ( saddlePoints,
+  )
+where
 
-import Data.Array (Array)
+import Data.Array (Array, Ix, assocs, bounds, (!))
 
-saddlePoints :: Array i e -> [i]
-saddlePoints matrix = error "You need to implement this function."
+saddlePoints :: (Ix i, Enum i, Ord e) => Array (i, i) e -> [(i, i)]
+saddlePoints matrix =
+  [ (y, x)
+    | ((y, x), saddlePoint) <- assocs matrix,
+      saddlePoint == maximum [matrix ! (y, col) | col <- [xLower .. xUpper]],
+      saddlePoint == minimum [matrix ! (row, x) | row <- [yLower .. yUpper]]
+  ]
+  where
+    ((yLower, xLower), (yUpper, xUpper)) = bounds matrix
