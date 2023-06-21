41b1d38ef160e7494355dc0a2735081689a0c82e
Eric Bailey <eric@ericb.me>
2023-06-21 00:43:01 -0500

    [go] reverse-string: submit

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,13 @@
+// Package reverse implements the Reverse String exercise.
 package reverse
 
+// Reverse a string.
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	runes := []rune(input)
+	length := len(runes)
+	for i := 0; i < length/2; i++ {
+		runes[i], runes[length-1-i] = runes[length-1-i], runes[i]
+	}
+
+	return string(runes)
 }
