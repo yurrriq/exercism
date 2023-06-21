36992a9de5a2255bf6acd4c2091681c176d611cf
Eric Bailey <eric@ericb.me>
2023-06-21 00:29:42 -0500

    [haskell] transpose: submit

--- haskell/transpose/src/Transpose.hs
+++ haskell/transpose/src/Transpose.hs
@@ -1,4 +1,34 @@
-module Transpose (transpose) where
+{-# LANGUAGE CPP #-}
+
+module Transpose
+  ( transpose,
+  )
+where
+
+-- NOTE: The test cases don't seem to follow the Reddit post.
+-- I find the Exercism problem description confusing if not incorrect.
+#ifdef REDDIT
+import Data.List (dropWhileEnd)
 
 transpose :: [String] -> [String]
-transpose lines = error "You need to implement this function."
+transpose [] = []
+transpose input =
+  take width $
+    map (dropWhileEnd (== ' ')) $
+      go paddedInput
+  where
+    go xs = map head xs : go (map tail xs)
+    paddedInput = map (padRight ' ' width) input
+    width = maximum (map length input)
+#else
+import qualified Data.List as List
+
+transpose :: [String] -> [String]
+transpose [] = []
+transpose input = List.transpose $ zipWith (padRight ' ') widths input
+  where
+    widths = scanr1 max (map length input)
+#endif
+
+padRight :: a -> Int -> [a] -> [a]
+padRight x n xs = take n (xs ++ repeat x)
