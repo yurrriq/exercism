506bc4df8566b8cf6fc3ffb18822b041ea578d98
Eric Bailey <eric@ericb.me>
2023-06-23 00:42:58 -0500

    [go] grains: submit

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,27 @@
+// Package grains implements the Grains exercise.
 package grains
 
-func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+// ErrInvalidSquare is returned when a given square is not with 1-64 inclusive.
+const ErrInvalidSquare SquareError = "Invalid square"
+
+// A SquareError is a string.
+type SquareError string
+
+// Error describes a SquareError as a string.
+func (err SquareError) Error() string {
+	return string(err)
 }
 
+// Square returns how many grains were on a given square.
+func Square(square int) (uint64, error) {
+	if 1 > square || square > 64 {
+		return 0, ErrInvalidSquare
+	}
+
+	return 1 << (square - 1), nil
+}
+
+// Total returns the total number of grains on the chessboard.
 func Total() uint64 {
-	panic("Please implement the Total function")
+	return 1<<64 - 1
 }
