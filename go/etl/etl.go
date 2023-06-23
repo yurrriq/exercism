a7b3f009d064b9b6fb2b2ebd53d28d916724c38d
Eric Bailey <eric@ericb.me>
2023-06-22 23:50:01 -0500

    [go] etl: submit

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,19 @@
+// Package etl implements the ETL exercise.
 package etl
 
-func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+import (
+	"strings"
+)
+
+// Transform a mapping from score to list of letters (one-to-many) to a mapping
+// from letter to score (one-to-one).
+func Transform(input map[int][]string) map[string]int {
+	transformed := make(map[string]int)
+	for score, letters := range input {
+		for _, letter := range letters {
+			transformed[strings.ToLower(letter)] = score
+		}
+	}
+
+	return transformed
 }
