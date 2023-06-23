50b456f76b99e9413f476403ae9a880072658038
Eric Bailey <eric@ericb.me>
2023-06-22 23:04:22 -0500

    [go] darts: submit

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,30 @@
+// Package darts implements the Darts exercise.
 package darts
 
-func Score(x, y float64) int {
-	panic("Please implement the Score function")
+import (
+	"math"
+)
+
+const (
+	innerRing  = 1.0
+	middleRing = 5.0
+	outerRing  = 10.0
+)
+
+// Score takes the x and y coordinates of a point in the target and returns the
+// number of points earned by a dart landing at that point.
+func Score(x, y float64) (score int) {
+	distance := math.Sqrt(x*x + y*y)
+	switch {
+	case distance > outerRing:
+		score = 0
+	case distance > middleRing:
+		score = 1
+	case distance > innerRing:
+		score = 5
+	default:
+		score = 10
+	}
+
+	return
 }
