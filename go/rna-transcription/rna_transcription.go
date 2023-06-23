04e76b3d6901080760b18d6a81cad985405f1d63
Eric Bailey <eric@ericb.me>
2023-06-22 22:10:19 -0500

    [go] rna-transcription: submit

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,24 @@
+// Package strand implements the RNA Transcription exercise.
 package strand
 
+// ToRNA converts a given DNA string into its RNA complement.
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	runes := []rune(dna)
+	rna := make([]rune, 0, len(runes))
+	for _, nucleotide := range runes {
+		switch nucleotide {
+		case 'G':
+			rna = append(rna, 'C')
+		case 'C':
+			rna = append(rna, 'G')
+		case 'T':
+			rna = append(rna, 'A')
+		case 'A':
+			rna = append(rna, 'U')
+		default:
+			panic("Invalid nucleotide")
+		}
+	}
+
+	return string(rna)
 }
