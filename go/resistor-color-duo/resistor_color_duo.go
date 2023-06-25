226d171335e1319816251d219d08fea6df1a515f
Eric Bailey <eric@ericb.me>
2023-06-25 17:11:31 -0500

    [go] resistor-color-duo: submit

--- go/resistor-color-duo/resistor_color_duo.go
+++ go/resistor-color-duo/resistor_color_duo.go
@@ -1,6 +1,63 @@
+// Package resistorcolorduo implements the Resistor Color Duo exercise.
 package resistorcolorduo
 
-// Value should return the resistance value of a resistor with a given colors.
+// Value returns the resistance value of a resistor with given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	length := len(colors)
+	if length < 2 {
+		return -1
+	}
+
+	// Ignore bands beyond the second.
+	length = 2
+
+	resistances := make([]int, length)
+
+decode:
+	for resistance, color := range allColors() {
+		for i, band := range colors {
+			// Ignore bands beyond the second.
+			if i > 1 {
+				continue decode
+			}
+			if band == color {
+				resistances[i] = resistance
+			}
+		}
+	}
+
+	totalResistance := 0
+	for i, resistance := range resistances {
+		// Ignore bands beyond the second.
+		if i > 1 {
+			continue
+		}
+		totalResistance += resistance * ipow(10, length-1-i)
+	}
+
+	return totalResistance
+}
+
+func allColors() []string {
+	return []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+}
+
+func ipow(base, exp int) int {
+	if exp < 0 {
+		panic("Negative exponent")
+	}
+
+	result := 1
+	for {
+		if exp%2 == 1 {
+			result *= base
+		}
+		exp /= 2
+		if exp == 0 {
+			break
+		}
+		base *= base
+	}
+
+	return result
 }
