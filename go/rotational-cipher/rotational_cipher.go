e652a1dde2a7455f62bbbbed88b294070626de32
Eric Bailey <eric@ericb.me>
2023-06-25 15:57:20 -0500

    [go] rotational-cipher: submit

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,5 +1,32 @@
+// Package rotationalcipher implements the Rotational Cipher exercise.
 package rotationalcipher
 
+// RotationalCipher rotates each letter in a given string by a given number of
+// letters.
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	if shiftKey%26 == 0 {
+		return plain
+	}
+
+	cipherText := make([]rune, len(plain))
+	for i, char := range plain {
+		cipherText[i] = shift(char, shiftKey)
+	}
+
+	return string(cipherText)
+}
+
+func shift(char rune, shiftKey int) rune {
+	switch {
+	case 'a' <= char && char <= 'z':
+		return shiftWrapped(char, shiftKey, 'a')
+	case 'A' <= char && char <= 'Z':
+		return shiftWrapped(char, shiftKey, 'A')
+	default:
+		return char
+	}
+}
+
+func shiftWrapped(char rune, shiftKey int, wrapAround int) rune {
+	return rune(wrapAround + ((int(char) - wrapAround + shiftKey) % 26))
 }
