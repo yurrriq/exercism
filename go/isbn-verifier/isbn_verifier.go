a53f95f1f3f89962b561ba06340c4d762c662ed4
Eric Bailey <eric@ericb.me>
2023-06-24 21:52:20 -0500

    [go] isbn-verifier: submit

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,32 @@
+// Package isbn implements the ISBN Verifier exercise.
 package isbn
 
+import (
+	"unicode"
+)
+
+// IsValidISBN validates a book identification number.
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	checksum := 0
+	k := 10
+	for i, roon := range []rune(isbn) {
+		if roon == '-' && (i == 1 || i == 5 || i == 11) {
+			continue
+		}
+
+		if 'X' == roon && k == 1 {
+			checksum += 10 * k
+			k--
+			continue
+		}
+
+		if !unicode.IsDigit(roon) || k < 1 {
+			return false
+		}
+
+		checksum += (int(roon) - '0') * k
+		k--
+	}
+
+	return (k == 0) && checksum%11 == 0
 }
