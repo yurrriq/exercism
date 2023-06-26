e1697f5e614e949b0ad75c1ad7bf104eef461b37
Eric Bailey <eric@ericb.me>
2023-06-25 20:07:20 -0500

    [go] clock: resubmit

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,22 +5,30 @@ import (
 	"fmt"
 )
 
-// A Clock has integers Hours (modulo 24) and Minutes (modulo 60).
+const (
+	dayMinutes  = 1440
+	hourMinutes = 60
+)
+
+// A Clock is a number of minutes (modulo 1440).
 type Clock struct {
-	Hours   int
-	Minutes int
+	minutes int
 }
 
 // New creates a new Clock from given hours and minutes.
 func New(h, m int) Clock {
-	return (Clock{Hours: 0, Minutes: 0}).Add(h*60 + m)
+	return Clock{0}.Add(h*hourMinutes + m)
 }
 
 // Add a given number of minutes to a Clock.
 func (c Clock) Add(m int) Clock {
-	mq, minutes := divide(c.Minutes+m, 60)
-	_, hours := divide(c.Hours+mq, 24)
-	return Clock{Hours: hours, Minutes: minutes}
+	minutes := (c.minutes + m) % dayMinutes
+
+	if minutes < 0 {
+		minutes += dayMinutes
+	}
+
+	return Clock{minutes}
 }
 
 // Subtract a given number of minutes from a Clock.
@@ -29,41 +37,5 @@ func (c Clock) Subtract(m int) Clock {
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.Hours, c.Minutes)
-}
-
-// Euclidean division
-func divide(dividend, divisor int) (quotient, remainder int) {
-	if divisor == 0 {
-		panic("Division by zero")
-	}
-	if divisor < 0 {
-		quotient, remainder = divide(dividend, -divisor)
-		quotient *= -1
-		return
-	}
-	if dividend < 0 {
-		quotient, remainder = divide(-dividend, divisor)
-		if remainder == 0 {
-			quotient *= -1
-			return
-		}
-		return -quotient - 1, divisor - remainder
-	}
-
-	return divideUnsigned(dividend, divisor)
-}
-
-// Unchecked Euclidean division
-func divideUnsigned(dividend, divisor int) (quotient, remainder int) {
-	remainder = dividend
-	if remainder < 0 {
-		remainder += divisor
-	}
-
-	for remainder >= divisor {
-		quotient++
-		remainder -= divisor
-	}
-	return
+	return fmt.Sprintf("%02d:%02d", c.minutes/hourMinutes, c.minutes%hourMinutes)
 }
