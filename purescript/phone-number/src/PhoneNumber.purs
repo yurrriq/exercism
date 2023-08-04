79b8a29f568514dd5dba901d14a7b55dd315159a
Eric Bailey <eric@ericb.me>
2023-08-04 05:26:51 -0500

    [purescript] phone-number: submit

--- purescript/phone-number/src/PhoneNumber.purs
+++ purescript/phone-number/src/PhoneNumber.purs
@@ -2,8 +2,42 @@ module PhoneNumber
   ( phoneNumber
   ) where
 
+import Control.Bind (bindFlipped)
+import Data.Array.NonEmpty (tail)
+import Data.Function (($), flip, (<<<))
+import Data.Functor (map)
 import Data.Maybe (Maybe)
-import Effect.Exception.Unsafe (unsafeThrow)
+import Data.Semigroup ((<>))
+import Data.String (joinWith)
+import Data.String.Regex (Regex, match)
+import Data.String.Regex.Flags (noFlags)
+import Data.String.Regex.Unsafe (unsafeRegex)
+import Data.Traversable (sequence)
 
+-- | Given a string representing a valid North American telephone number, return
+-- | `Just` a string of the digits, i.e. the area and exchange codes and the
+-- | subscriber number. If the input is invalid, return `Nothing`.
 phoneNumber :: String -> Maybe String
-phoneNumber = unsafeThrow "You need to implement this function."
+phoneNumber =
+  map (joinWith "")
+    <<< bindFlipped sequence
+    <<< map tail
+    <<< match nanp
+
+-- The North American Numbering Plan (NANP)
+nanp :: Regex
+nanp =
+  let
+    separator = "(?:[.-]|\\s+)?"
+  in
+    flip unsafeRegex noFlags $
+      -- optional country code
+      "^\\s*(?:\\+?1\\s*)?"
+        -- area code
+        <> "\\(?([2-9]\\d{2})\\)?"
+        <> separator
+        -- exchange code
+        <> "([2-9]\\d{2})"
+        <> separator
+        -- subscriber number
+        <> "(\\d{4})\\s*$"
