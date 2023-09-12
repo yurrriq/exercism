a506f058d3c085a69616e98c5f193e96845a489c
Eric Bailey <eric@ericb.me>
2023-09-11 22:34:39 -0500

    [haskell] pig-latin: resubmit

--- haskell/pig-latin/src/PigLatin.hs
+++ haskell/pig-latin/src/PigLatin.hs
@@ -1,22 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
+
 module PigLatin
   ( translate,
   )
 where
 
-import Data.Char (toLower)
-
 translate :: String -> String
-translate = unwords . map go . words
-  where
-    go word@('x' : 'r' : _) = word <> "ay"
-    go word@('y' : 't' : _) = word <> "ay"
-    go word =
-      case span isConsonant word of
-        ("q", 'u' : rest) -> rest <> "quay"
-        (consonants@(_ : "q"), 'u' : rest) -> rest <> consonants <> "uay"
-        ("", 'y' : rest) -> rest <> "yay"
-        (consonants, rest@('y' : _)) -> rest <> consonants <> "ay"
-        (consonants, rest) -> rest <> consonants <> "ay"
+translate = unwords . map translateWord . words
 
-isConsonant :: Char -> Bool
-isConsonant = (`elem` ("bcdfghjklmnpqrstvwxz" :: String)) . toLower
+translateWord :: String -> String
+translateWord word@('x' : 'r' : _) = word <> "ay"
+translateWord word@('y' : 't' : _) = word <> "ay"
+translateWord word =
+  case break (`elem` vowels) word of
+    ("q", 'u' : rest) -> rest <> "quay"
+    (consonants@(_ : "q"), 'u' : rest) -> rest <> consonants <> "uay"
+    ("", 'y' : rest) -> rest <> "yay"
+    (consonants, rest@('y' : _)) -> rest <> consonants <> "ay"
+    (consonants, rest) -> rest <> consonants <> "ay"
+
+vowels :: [Char]
+vowels = "aeiouy"
