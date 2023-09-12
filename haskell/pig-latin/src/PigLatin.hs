9c22406f503e9933ae67c8641ccdd073b407abbb
Eric Bailey <eric@ericb.me>
2023-09-11 22:17:14 -0500

    [haskell] pig-latin: submit

--- haskell/pig-latin/src/PigLatin.hs
+++ haskell/pig-latin/src/PigLatin.hs
@@ -1,4 +1,22 @@
-module PigLatin (translate) where
+module PigLatin
+  ( translate,
+  )
+where
+
+import Data.Char (toLower)
 
 translate :: String -> String
-translate xs = error "You need to implement this function."
+translate = unwords . map go . words
+  where
+    go word@('x' : 'r' : _) = word <> "ay"
+    go word@('y' : 't' : _) = word <> "ay"
+    go word =
+      case span isConsonant word of
+        ("q", 'u' : rest) -> rest <> "quay"
+        (consonants@(_ : "q"), 'u' : rest) -> rest <> consonants <> "uay"
+        ("", 'y' : rest) -> rest <> "yay"
+        (consonants, rest@('y' : _)) -> rest <> consonants <> "ay"
+        (consonants, rest) -> rest <> consonants <> "ay"
+
+isConsonant :: Char -> Bool
+isConsonant = (`elem` ("bcdfghjklmnpqrstvwxz" :: String)) . toLower
