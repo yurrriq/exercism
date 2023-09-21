951d3fd257c0e9a24b011be6e905bfc3c1ae198f
Eric Bailey <eric@ericb.me>
2023-09-20 23:07:55 -0500

    [haskell] palindrome-products: submit

--- haskell/palindrome-products/src/Palindromes.hs
+++ haskell/palindrome-products/src/Palindromes.hs
@@ -1,7 +1,35 @@
-module Palindromes (largestPalindrome, smallestPalindrome) where
+module Palindromes
+  ( largestPalindrome,
+    smallestPalindrome,
+  )
+where
+
+import Data.Bits ((.&.))
+import Data.List (tails)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
 largestPalindrome :: Integer -> Integer -> Maybe (Integer, [(Integer, Integer)])
-largestPalindrome minFactor maxFactor = error "You need to implement this function."
+largestPalindrome minFactor maxFactor =
+  Map.lookupMax $ palindromeProductsWithin minFactor maxFactor
 
 smallestPalindrome :: Integer -> Integer -> Maybe (Integer, [(Integer, Integer)])
-smallestPalindrome minFactor maxFactor = error "You need to implement this function."
+smallestPalindrome minFactor maxFactor =
+  Map.lookupMin $ palindromeProductsWithin minFactor maxFactor
+
+palindromeProductsWithin :: Integer -> Integer -> Map Integer [(Integer, Integer)]
+palindromeProductsWithin minFactor maxFactor =
+  Map.fromListWith (++) $
+    [ (xy, [(x, y)])
+      | x : ys <- tails [minFactor .. maxFactor],
+        y <- x : ys,
+        let xy = x * y,
+        isPalindrome (show xy)
+    ]
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = fward == bward
+  where
+    fward = take (q + (r .&. 1)) xs
+    bward = reverse $ drop q xs
+    (q, r) = length xs `divMod` 2
