4eaa395ee1abbf6ef60e4b783eb11662f8ef3406
Eric Bailey <eric@ericb.me>
2023-09-24 19:21:10 -0500

    [haskell] proverb: submit

--- haskell/proverb/src/Proverb.hs
+++ haskell/proverb/src/Proverb.hs
@@ -1,4 +1,17 @@
-module Proverb(recite) where
+module Proverb
+  ( recite,
+  )
+where
+
+import Data.List (intercalate)
 
 recite :: [String] -> String
-recite = error "You need to implement this function!"
+recite [] = ""
+recite inputs@(input : _)
+  | null proverb = "And all for the want of a " <> input <> "."
+  | otherwise = intercalate "\n" $ proverb ++ [recite [input]]
+  where
+    proverb =
+      [ "For want of a " <> want <> " the " <> loss <> " was lost."
+        | (want, loss) <- zip inputs (tail inputs)
+      ]
