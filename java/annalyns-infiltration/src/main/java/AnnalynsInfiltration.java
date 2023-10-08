31b7a69a13cdc33de2d4b1c425e7a494d8b2014e
Eric Bailey <eric@ericb.me>
2023-10-08 18:55:13 -0500

    [java] annalyns-infiltration: download

---
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,17 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+    }
+}
