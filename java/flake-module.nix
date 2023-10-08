b09141753bc56617629b3149954f93f676a542f4
Eric Bailey <eric@ericb.me>
2023-10-08 18:42:14 -0500

    [java] emacs -> idea

--- java/flake-module.nix
+++ java/flake-module.nix
@@ -7,14 +7,8 @@
         self'.devShells.default
       ];
       nativeBuildInputs = with pkgs; [
-        (
-          emacsWithPackagesFromUsePackage {
-            alwaysEnsure = true;
-            config = ./emacs.el;
-          }
-        )
         gradle
-        jdt-language-server
+        jetbrains.idea-community
         openjdk
       ];
 
