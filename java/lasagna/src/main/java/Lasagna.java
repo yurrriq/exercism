11ea8b24ae183e3b3a4dfd47142a60fcce956bf5
Eric Bailey <eric@ericb.me>
2023-10-08 00:21:47 -0500

    [java] lasagna: submit

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,17 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int actualMinutesInOven) {
+        return this.expectedMinutesInOven() - actualMinutesInOven;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int numberOfLayers) {
+        return 2 * numberOfLayers;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int numberOfLayers, int actualMinutesInOven) {
+        return this.preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+    }
 }
