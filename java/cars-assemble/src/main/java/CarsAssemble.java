6ddd906c332de7191a5eb50c7b426d8e92660b70
Eric Bailey <eric@ericb.me>
2023-10-09 00:21:26 -0500

    [java] cars-assemble: download

---
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -0,0 +1,10 @@
+public class CarsAssemble {
+
+    public double productionRatePerHour(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+}
