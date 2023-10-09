4c2c2ce01492e80c0a99ef842f59afbfda055186
Eric Bailey <eric@ericb.me>
2023-10-08 19:06:00 -0500

    [java] annalyns-infiltration: submit

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,20 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+        return prisonerIsAwake || knightIsAwake || archerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+        return prisonerIsAwake && !archerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        if (petDogIsPresent) {
+            return !archerIsAwake;
+        }
+        return (prisonerIsAwake && !knightIsAwake && !archerIsAwake);
     }
 }
