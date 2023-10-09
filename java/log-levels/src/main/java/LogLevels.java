f8e07a81ed247acfb44d4f3a3116d36c91589f82
Eric Bailey <eric@ericb.me>
2023-10-08 19:34:18 -0500

    [java] log-levels: submit

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogLevels {
-    
-    public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+
+    public static String message(String logLine) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile("^\\[(?:INFO|WARNING|ERROR)\\]: (?<message>.+)");
+        Matcher matcher = pattern.matcher(logLine);
+        if (matcher.find()) {
+            return matcher.group("message").trim();
+        } else {
+            throw new IllegalArgumentException("Bad logLine");
+        }
     }
 
-    public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+    public static String logLevel(String logLine) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile("^\\[(?<level>(?:INFO|WARNING|ERROR))\\]: .+");
+        Matcher matcher = pattern.matcher(logLine);
+        if (matcher.find()) {
+            return matcher.group("level").toLowerCase();
+        } else {
+            throw new IllegalArgumentException("Bad logLine");
+        }
     }
 
-    public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+    public static String reformat(String logLine)  throws IllegalArgumentException {
+        String message = LogLevels.message(logLine);
+        String logLevel = LogLevels.logLevel(logLine);
+        return String.format("%s (%s)", message, logLevel);
     }
+
 }
