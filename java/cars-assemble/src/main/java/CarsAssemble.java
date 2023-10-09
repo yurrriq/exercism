59f5c88cec6e07833af5fa1697903f0f04cfb002
Eric Bailey <eric@ericb.me>
2023-10-09 00:42:56 -0500

    [java] cars-assemble: submit

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,26 @@
-public class CarsAssemble {
+import java.lang.Math;
 
-    public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+public class CarsAssemble {
+    private final static int SPEED_OFF = 0;
+    private final static int SPEED_MAXIMUM = 0;
+    private final static int CARS_PER_HOUR = 221;
+
+    public static double productionRatePerHour(int speed) {
+        double successRate;
+        if (speed < 5) {
+            successRate = 1.0;
+        } else if (speed <= 8) {
+            successRate = 0.9;
+        } else if (speed == 9) {
+            successRate = 0.8;
+        } else {
+            successRate = 0.77;
+        }
+
+        return (double)speed * (double)CARS_PER_HOUR * successRate;
     }
 
-    public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    public static int workingItemsPerMinute(int speed) {
+        return (int) Math.floor(CarsAssemble.productionRatePerHour(speed) / 60.0);
     }
 }
