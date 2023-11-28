a39a6c4de46170bb84e0918885971386d52773ce
Eric Bailey <eric@ericb.me>
2023-11-28 17:51:49 -0600

    [ocaml] raindrops: resubmit

--- ocaml/raindrops/raindrops.ml
+++ ocaml/raindrops/raindrops.ml
@@ -1,16 +1,10 @@
 open Base
 
-
 let raindrop n =
   let divides d n = Int.rem n d = 0 in
-  let raindrops = [(3, "Pling"); (5, "Plang"); (7, "Plong")] in
-  Option.value ~default:(Int.to_string n)
-    (List.fold_left raindrops
-       ~init:None
-       ~f:(fun acc (d, drop) -> if divides d n then
-                                  (* FIXME: Option.map acc ~f:((^) drop) *)
-                                  match acc with
-                                  | None   -> Some drop
-                                  | Some s -> Some (s ^ drop)
-                                else
-                                  acc))
+  let raindrops = [ (3, "Pling"); (5, "Plang"); (7, "Plong") ] in
+  List.fold_left raindrops ~init:None ~f:(fun acc (d, drop) ->
+      if divides d n then
+        match acc with None -> Some drop | Some s -> Some (s ^ drop)
+      else acc)
+  |> Option.value ~default:(Int.to_string n)
