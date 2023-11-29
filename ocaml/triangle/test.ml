f506c0247db49a0787df7b8a1043812c3a910206
Eric Bailey <eric@ericb.me>
2023-11-28 18:06:13 -0600

    [ocaml] triangle: download

---
+++ ocaml/triangle/test.ml
@@ -0,0 +1,59 @@
+(* triangle - 1.0 *)
+open Base
+open OUnit2
+open Triangle
+
+let ae exp got _test_ctxt = assert_equal exp got ~printer:Bool.to_string
+
+let equilateral_triangle_tests = [
+  "all sides are equal" >::
+  ae true (is_equilateral 2 2 2);
+  "any side is unequal" >::
+  ae false (is_equilateral 2 3 2);
+  "no sides are equal" >::
+  ae false (is_equilateral 5 4 6);
+  "all zero sides is not a triangle" >::
+  ae false (is_equilateral 0 0 0);
+]
+let isosceles_triangle_tests = [
+  "last two sides are equal" >::
+  ae true (is_isosceles 3 4 4);
+  "first two sides are equal" >::
+  ae true (is_isosceles 4 4 3);
+  "first and last sides are equal" >::
+  ae true (is_isosceles 4 3 4);
+  "equilateral triangles are also isosceles" >::
+  ae true (is_isosceles 4 4 4);
+  "no sides are equal" >::
+  ae false (is_isosceles 2 3 4);
+  "first triangle inequality violation" >::
+  ae false (is_isosceles 1 1 3);
+  "second triangle inequality violation" >::
+  ae false (is_isosceles 1 3 1);
+  "third triangle inequality violation" >::
+  ae false (is_isosceles 3 1 1);
+]
+let scalene_triangle_tests = [
+  "no sides are equal" >::
+  ae true (is_scalene 5 4 6);
+  "all sides are equal" >::
+  ae false (is_scalene 4 4 4);
+  "first and second sides are equal" >::
+  ae false (is_scalene 4 4 3);
+  "first and third sides are equal" >::
+  ae false (is_scalene 3 4 3);
+  "second and third sides are equal" >::
+  ae false (is_scalene 4 3 3);
+  "may not violate triangle inequality" >::
+  ae false (is_scalene 7 3 2);
+]
+
+let () =
+  run_test_tt_main (
+    "triangle tests" >:::
+    List.concat [
+      equilateral_triangle_tests;
+      isosceles_triangle_tests;
+      scalene_triangle_tests;
+    ]
+  )
