3291c823ab43f8d568bc09587153978714fade07
Eric Bailey <eric@ericb.me>
2023-11-28 22:23:03 -0600

    [ocaml] hamming: submit

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,4 +1,14 @@
 type nucleotide = A | C | G | T
 
-let hamming_distance _ _ =
-  failwith "'hamming_distance' is missing"
+let hamming_distance l1 l2 =
+  let rec go acc xs ys =
+    match (xs, ys) with
+    | [], [] -> Ok acc
+    | x :: xs, y :: ys -> go (acc + Bool.to_int (x <> y)) xs ys
+    | _ -> Error "left and right strands must be of equal length"
+  in
+  match (l1, l2) with
+  | [], [] -> Ok 0
+  | [], _ -> Error "left strand must not be empty"
+  | _, [] -> Error "right strand must not be empty"
+  | _ -> go 0 l1 l2
