37f4f7137087c13abe854366e9b2bb891ad1d996
Eric Bailey <eric@ericb.me>
2023-11-28 18:40:04 -0600

    [ocaml] triangle: submit

--- ocaml/triangle/triangle.ml
+++ ocaml/triangle/triangle.ml
@@ -1,8 +1,6 @@
-let is_equilateral _ _ _ =
-    failwith "'is_equilateral' is missing"
+let is_triangle x y z =
+  x > 0 && y > 0 && z > 0 && x + y > z && x + z > y && y + z > x
 
-let is_isosceles _ _ _ =
-    failwith "'is_isosceles' is missing"
-
-let is_scalene _ _ _ =
-    failwith "'is_scalene' is missing"
+let is_equilateral x y z = is_triangle x y z && x = y && y = z
+let is_isosceles x y z = is_triangle x y z && (x = y || x = z || y = z)
+let is_scalene x y z = is_triangle x y z && x <> y && x <> z && y <> z
