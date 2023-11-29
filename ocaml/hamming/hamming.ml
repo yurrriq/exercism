2fc1b2005f4c342698f9189288cbacbfa18696c6
Eric Bailey <eric@ericb.me>
2023-11-28 22:54:12 -0600

    [ocaml] hamming: resubmit

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,14 +1,12 @@
 type nucleotide = A | C | G | T
 
-let hamming_distance l1 l2 =
-  let rec go acc xs ys =
-    match (xs, ys) with
-    | [], [] -> Ok acc
-    | x :: xs, y :: ys -> go (acc + Bool.to_int (x <> y)) xs ys
-    | _ -> Error "left and right strands must be of equal length"
-  in
-  match (l1, l2) with
+let hamming_distance xs ys =
+  match (xs, ys) with
   | [], [] -> Ok 0
   | [], _ -> Error "left strand must not be empty"
   | _, [] -> Error "right strand must not be empty"
-  | _ -> go 0 l1 l2
+  | _ -> (
+      try
+        List.fold_left2 (fun acc x y -> acc + Bool.to_int (x <> y)) 0 xs ys
+        |> Result.ok
+      with _ -> Error "left and right strands must be of equal length")
