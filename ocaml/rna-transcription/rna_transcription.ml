6073038f78dae601e8991e714a113bb823954cf9
Eric Bailey <eric@ericb.me>
2023-11-29 21:54:44 -0600

    [ocaml] rna-transcription: submit

--- ocaml/rna-transcription/rna_transcription.ml
+++ ocaml/rna-transcription/rna_transcription.ml
@@ -1,5 +1,18 @@
 type dna = [ `A | `C | `G | `T ]
 type rna = [ `A | `C | `G | `U ]
 
-let to_rna _ =
-    failwith "'to_rna' is missing"
+let rec map_option f = function
+  | [] -> []
+  | x :: xs -> (
+      let rs = map_option f xs in
+      match f x with None -> rs | Some r -> r :: rs)
+
+let to_rna =
+  let transcribe = function
+    | `G -> Some `C
+    | `C -> Some `G
+    | `T -> Some `A
+    | `A -> Some `U
+    | _ -> None
+  in
+  map_option transcribe
