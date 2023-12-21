bab05dec68ae4295b73154c2a60bb7a291c879bc
Eric Bailey <eric@ericb.me>
2023-12-21 12:29:20 -0500

    [go] proverb: resubmit

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -5,16 +5,24 @@ import (
 	"fmt"
 )
 
-// Proverb should have a comment documenting it.
+const (
+	lineIntermediate string = "For want of a %s the %s was lost."
+	lineLast         string = "And all for the want of a %s."
+)
+
+// Proverb takes a list of inputs and returns the relevant proverb.
 func Proverb(rhyme []string) []string {
 	k := len(rhyme)
-	lines := make([]string, k)
-	for i, want := range rhyme {
-		if i == k-1 {
-			lines[i] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
-		} else {
-			lines[i] = fmt.Sprintf("For want of a %s the %s was lost.", want, rhyme[i+1])
-		}
+	if k == 0 {
+		return []string{}
 	}
+
+	lines := make([]string, k)
+	lines[k-1] = fmt.Sprintf(lineLast, rhyme[0])
+
+	for i, want := range rhyme[:k-1] {
+		lines[i] = fmt.Sprintf(lineIntermediate, want, rhyme[i+1])
+	}
+
 	return lines
 }
