4044ac683bf221d12d748f8f85e87280c8b319ee
Eric Bailey <eric@ericb.me>
2023-12-21 13:30:52 -0500

    [go] word-count: submit

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,7 +1,25 @@
+// Package wordcount implements the Word Count exercism from Exercism.
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
+// Frequency is a map from word to the number of occurrences.
 type Frequency map[string]int
 
+// WordCount computes the frequency of words in a given a phrase.
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	nonAlphanumeric := regexp.MustCompile(`[^[:alpha:][:digit:]']+`)
+	words := nonAlphanumeric.Split(strings.ToLower(phrase), -1)
+	frequency := Frequency{}
+	for _, word := range words {
+		normalizedWord := strings.Trim(strings.ToLower(word), `'`)
+		if len(normalizedWord) == 0 {
+			continue
+		}
+		frequency[normalizedWord]++
+	}
+	return frequency
 }
