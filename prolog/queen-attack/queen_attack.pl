4c742e14c14888ecafaee40b543c0752ff049866
Eric Bailey <eric@ericb.me>
2024-01-13 01:07:25 -0600

    [prolog] queen-attack: submit

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,13 +1,60 @@
-%! create(+DimTuple)
-%
-% The create/1 predicate succeeds if the DimTuple contains valid chessboard 
-% dimensions, e.g. (0,0) or (2,4).
-create((DimX, DimY)) :-
-	fail.
+/*
 
-%! attack(+FromTuple, +ToTuple)
-%
-% The attack/2 predicate succeeds if a queen positioned on ToTuple is 
-% vulnerable to an attack by another queen positioned on FromTuple.
-attack((FromX, FromY), (ToX, ToY)):-
-	fail.
+\PrologDialect{swiprolog}
+
+Use the \href{https://www.swi-prolog.org/man/clpfd.html}{Constraint Logic Programming over Finite Domains} library.
+
+\PL*/
+:- use_module(library(clpfd)).
+/*PL
+
+\Predicate create/1(+Tuple).
+
+\emph{(Row, Col)} represents a valid chessboard position.
+
+\PL*/
+create((Row, Col)) :-
+/*PL
+
+\emph{Row} and \emph{Col} are both elements of $0..7$.
+
+\PL*/
+    [Row, Col] ins 0..7.
+/*PL
+
+\Predicate attack/2(+Tuple, +Tuple).
+
+A queen positioned at \emph{(Row1, Col1)} is vulnerable to an attack by another
+queen positioned at \emph{(Row2, Col2)}.
+
+\PL*/
+attack((Row1, Col1), (Row2, Col2)) :-
+/*PL
+
+Ensure both positions are valid.
+
+\PL*/
+    create((Row1, Col1)),
+    create((Row2, Col2)),
+/*PL
+
+Queens in the same row can attack each other.
+
+\PL*/
+    ( Row1 #= Row2
+/*PL
+
+Queens in the same column can attack each other.
+
+\PL*/
+    ; Col1 #= Col2
+/*PL
+
+Queens that share the same diagonal can attack each other.
+
+\PL*/
+    ; abs(Row1 - Row2) #= abs(Col1 - Col2)
+    ).
+/*PL
+
+\EndProlog*/
