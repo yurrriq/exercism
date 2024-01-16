c43ca157dffa2312694cb5104f39fa27f30ddd85
Eric Bailey <eric@ericb.me>
2024-01-16 02:17:27 -0600

    [clojure] cars-assemble: submit

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,23 @@
 (ns cars-assemble)
 
 (defn production-rate
-  "Returns the assembly line's production rate per hour,
-   taking into account its success rate"
+  "Given an assembly line's `speed` (an integer between 0 and 10 inclusive),
+  compute the hourly production rate, taking into account the success rate."
   [speed]
-  )
+  {:pre [(<= 0 speed) (<= speed 10)]}
+  (-> (cond
+        (= 0 speed) 0.0
+        (and (<= 1 speed) (<= speed 4)) 1.0
+        (and (<= 5 speed) (<= speed 8)) 0.9
+        (= 9 speed) 0.8
+        (= 10 speed) 0.77)
+      (* speed 221)))
 
 (defn working-items
-  "Calculates how many working cars are produced per minute"
+  "Calculate how many working cars are produced per minute."
   [speed]
-  )
+  {:pre [(int? speed)]}
+  (-> speed
+      production-rate
+      (/ 60)
+      int))
