007e3c8b64ccfa8cf4dd7fde0881bd79e6c8d9d4
Eric Bailey <eric@ericb.me>
2024-01-18 01:41:53 -0600

    feat(clojure): submit interest-is-interesting

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,29 @@
-(ns interest-is-interesting)
+(ns interest-is-interesting
+  "The Interest is Interesting exercise
+
+  https://exercism.org/tracks/clojure/exercises/interest-is-interesting"
+  (:require [clojure.math :as math]))
 
 (defn interest-rate
-  "Returns the interest rate based on the specified balance."
+  "Return the interest rate based on the specified balance."
   [balance]
-  )
+  (condp > balance
+    0 -3.213
+    1000 0.5
+    5000 1.621
+    2.475))
 
 (defn annual-balance-update
-  "Returns the annual balance update, taking into account the interest rate."
+  "Return the annual balance update, taking into account the interest rate."
   [balance]
-  )
+  (-> balance
+      (* (/ (bigdec (abs (interest-rate balance))) 100M))
+      (+ balance)
+      bigdec))
 
 (defn amount-to-donate
-  "Returns how much money to donate based on the balance and the tax-free percentage."
+  "Return the amount to donate based on the balance and tax-free percentage."
   [balance tax-free-percentage]
-  )
+  (if (pos? balance)
+    (-> balance (* 2M (/ tax-free-percentage 100)) math/floor int)
+    0))
