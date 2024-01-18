c444804bab7cbfea831f13807ff86cd69b176cef
Eric Bailey <eric@ericb.me>
2024-01-18 00:31:21 -0600

    chore(clojure): download interest-is-interesting

---
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -0,0 +1,16 @@
+(ns interest-is-interesting)
+
+(defn interest-rate
+  "Returns the interest rate based on the specified balance."
+  [balance]
+  )
+
+(defn annual-balance-update
+  "Returns the annual balance update, taking into account the interest rate."
+  [balance]
+  )
+
+(defn amount-to-donate
+  "Returns how much money to donate based on the balance and the tax-free percentage."
+  [balance tax-free-percentage]
+  )
