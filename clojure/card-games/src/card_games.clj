dd484399f76b50897851993e9d7f156f37979e29
Eric Bailey <eric@ericb.me>
2024-01-19 01:05:57 -0600

    feat(clojure): submit card-games

--- clojure/card-games/src/card_games.clj
+++ clojure/card-games/src/card_games.clj
@@ -1,44 +1,44 @@
 (ns card-games)
 
 (defn rounds
-  "Takes the current round number and returns 
-   a `list` with that round and the _next two_."
+  "Return a list with a given round number and the next two."
   [n]
-  )
+  (take 3 (iterate inc n)))
 
-(defn concat-rounds 
-  "Takes two lists and returns a single `list` 
-   consisting of all the rounds in the first `list`, 
-   followed by all the rounds in the second `list`"
-  [l1 l2]
-  )
+(defn concat-rounds
+  "Concatenate two lists of rounds."
+  [xs ys]
+  (concat xs ys))
 
-(defn contains-round? 
-  "Takes a list of rounds played and a round number.
-   Returns `true` if the round is in the list, `false` if not."
-  [l n]
-  )
+(defn contains-round?
+  "Determine if a given list of rounds contains a given round."
+  [rounds round]
+  (boolean (some #{round} rounds)))
 
 (defn card-average
-  "Returns the average value of a hand"
+  "Compute the average value of a hand."
   [hand]
-  )
+  (double (/ (apply + hand) (count hand))))
 
 (defn approx-average?
-  "Returns `true` if average is equal to either one of:
-  - Take the average of the _first_ and _last_ number in the hand.
-  - Using the median (middle card) of the hand."
+  "Determine if the average value of a hand is equal to either of:
+  - the average of the _first_ and _last_ card
+  - the median (middle card)"
   [hand]
-  )
+  (let [actual-average (card-average hand)]
+    (or (= actual-average (card-average [(first hand) (last hand)]))
+        (= actual-average (double (nth hand (/ (count hand) 2)))))))
 
 (defn average-even-odd?
-  "Returns true if the average of the cards at even indexes 
-   is the same as the average of the cards at odd indexes."
+  "Determine if the average of the even-indexed cards is equal to the
+  average of the odd-indexed cards."
   [hand]
-  )
+  (= (card-average (take-nth 2 hand))
+     (card-average (take-nth 2 (rest hand)))))
 
 (defn maybe-double-last
-  "If the last card is a Jack (11), doubles its value
-   before returning the hand."
+  "If the last card is a Jack (11), double its value."
   [hand]
-  )
+  (if (= 11 (last hand))
+    (concat (butlast hand) [22])
+    hand))
