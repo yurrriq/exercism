818cf3ff19f4bfd48f443ccb07d7d11992180b37
Eric Bailey <eric@ericb.me>
2024-01-20 23:41:07 -0600

    feat(clojure): submit squeaky-clean

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -2,6 +2,15 @@
   (:require [clojure.string :as str]))
 
 (defn clean
-  "TODO: add docstring"
-  [s]
-  )
+  "Return a squeaky clean copy of a given string."
+  [dirty-str]
+  (-> dirty-str
+      ;; 1. Replace any spaces encountered with underscores
+      (str/replace #"\s" "_")
+      ;; 2. Replace control characters with the upper case string "CTRL"
+      (str/replace #"\p{IsControl}" "CTRL")
+      ;; 3. Convert kebab-case to camelCase
+      (str/replace #"-(\p{IsLowercase})" #(str/upper-case (%1 1)))
+      ;; 4. Omit characters that are not letters
+      ;; 5. Omit Greek lower case letters
+      (str/replace #"[^_\p{IsLetter}]|[\p{IsGreek}&&\p{IsLowerCase}]" "")))
