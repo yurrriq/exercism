9dc5bb8943f2c5704b31001483421b99f68cf868
Eric Bailey <eric@ericb.me>
2024-01-20 20:33:33 -0600

    feat(clojure): submit log-levels

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -2,19 +2,23 @@
   (:require [clojure.string :as str]))
 
 (defn message
-  "Takes a string representing a log line
-   and returns its message with whitespace trimmed."
-  [s]
-  )
+  "Given a string representing a log line,
+  return its message with whitespace trimmed."
+  [log-line]
+  (let [[_ msg] (str/split log-line #":" 2)]
+    (str/trim msg)))
 
 (defn log-level
-  "Takes a string representing a log line
-   and returns its level in lower-case."
-  [s]
-  )
+  "Given a string representing a log line,
+   return its level in lower-case."
+  [log-line]
+  (let [[_ level] (re-find #"\[(\w+)\]" log-line)]
+    (str/lower-case level)))
 
 (defn reformat
-  "Takes a string representing a log line and formats it
-   with the message first and the log level in parentheses."
-  [s]
-  )
+  "Given a string representing a log line,
+  format it with the message first and the log level in parentheses."
+  [log-line]
+  (let [msg (message log-line)
+        level (log-level log-line)]
+    (format "%s (%s)" msg level)))
