feaeb9fabb4491bd87ff41d3a3204f6901e64bf2
Eric Bailey <eric@ericb.me>
2024-01-21 22:29:16 -0600

    feat(clojure): submit armstrong-numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,20 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  "The Armstrong Numbers exercise from Exercism"
+  (:require [clojure.math :as math]))
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- armstrong-accum
+  [k]
+  (fn [acc c]
+    (-> (Character/digit c 10)
+        (math/pow k)
+        (+ acc))))
+
+(defn armstrong?
+  "Determine whether `n` is an Armstrong number."
+  [n]
+  (or (zero? n)
+      (-> (inc (int (math/floor (math/log10 n))))
+          armstrong-accum
+          (reduce 0 (str n))
+          int
+          (= n))))
