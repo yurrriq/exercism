0252e7ab322edc24aa19e20c2f96bb5fbeb7aadf
Eric Bailey <eric@ericb.me>
2024-01-21 21:39:39 -0600

    feat(clojure): submit say

--- clojure/say/project.clj
+++ clojure/say/project.clj
@@ -1,4 +1,4 @@
-(defproject say "0.1.0-SNAPSHOT"
-  :description "say exercise."
+(defproject say "0.1.0"
+  :description "The Say exercise from Exercism"
   :url "https://github.com/exercism/clojure/tree/master/exercises/say"
   :dependencies [[org.clojure/clojure "1.10.0"]])
--- clojure/say/src/say.clj
+++ clojure/say/src/say.clj
@@ -1,5 +1,13 @@
-(ns say)
+(ns say
+  "The Say exercise from Exercism"
+  (:require [clojure.pprint :as pprint]
+            [clojure.string :as string]))
 
-(defn number [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn number
+  "Given an integer `n`, return a string with `n` as a cardinal English number."
+  [n]
+  (if (<= 0 n 999999999999)
+    ;; https://www.lispworks.com/documentation/HyperSpec/Body/22_cba.htm
+    (-> (pprint/cl-format nil "~R" n)
+        (string/replace "," ""))
+    (throw (IllegalArgumentException. "n must be within (0, 999999999999]"))))
