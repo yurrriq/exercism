7d6e9727a1db73cbd744be49c07c8d12e5766bac
Eric Bailey <eric@ericb.me>
2024-01-22 22:02:33 -0600

    feat(clojure): submit accumulate

--- clojure/accumulate/project.clj
+++ clojure/accumulate/project.clj
@@ -1,4 +1,4 @@
-(defproject accumulate "0.1.0-SNAPSHOT"
-  :description "accumulate exercise."
-  :url "https://github.com/exercism/clojure/tree/master/exercises/accumulate"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+(defproject accumulate "0.1.0"
+  :description "The Accumulate exercise from Exercism"
+  :url "https://github.com/yurrriq/exercism/tree/main/clojure/accumulate"
+  :dependencies [[org.clojure/clojure "1.11.1"]])
--- clojure/accumulate/src/accumulate.clj
+++ clojure/accumulate/src/accumulate.clj
@@ -1,5 +1,10 @@
-(ns accumulate)
+(ns accumulate
+  "The Accumulate exercise from Exercism")
 
-(defn accumulate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn accumulate
+  "Return a lazy sequence consisting of the result of applying `f` to
+  each item in `coll`."
+  [f coll]
+  (lazy-seq
+   (when-let [[x & xs] (seq coll)]
+     (cons (f x) (accumulate f xs)))))
