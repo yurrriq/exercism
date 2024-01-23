2f03631b420df413dea51400aed94d7ec7398680
Eric Bailey <eric@ericb.me>
2024-01-22 21:37:12 -0600

    feat(clojure): submit reverse-string

--- clojure/reverse-string/project.clj
+++ clojure/reverse-string/project.clj
@@ -1,4 +1,4 @@
-(defproject reverse-string "0.1.0-SNAPSHOT"
-  :description "reverse-string exercise."
-  :url "https://github.com/exercism/clojure/tree/master/exercises/reverse-string"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+(defproject reverse-string "0.1.0"
+  :description "The Reverse String exercise from Exercism"
+  :url "https://github.com/yurrriq/exercism/tree/main/clojure/reverse-string"
+  :dependencies [[org.clojure/clojure "1.11.1"]])
--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,5 +1,10 @@
-(ns reverse-string)
+(ns reverse-string
+  "The Reverse String exercise from Exercism")
 
-(defn reverse-string [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn reverse-string
+  "Return a string with the characters in a given string in reverse order."
+  [s]
+  (loop [chars s, reversed '()]
+    (if-let [[c & cs] chars]
+      (recur cs (cons c reversed))
+      (apply str reversed))))
