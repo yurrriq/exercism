631fda3a636f47f35b476b423e576cc5e86aae5f
Eric Bailey <eric@ericb.me>
2024-01-24 10:44:49 -0600

    feat(javascript): submit reverse-string
    https://rosettacode.org/wiki/Reverse_a_string#Split_graphemes_=

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const reverseString = () => {
-  throw new Error('Remove this statement and implement this function');
+export const reverseString = (string) => {
+  return [...new Intl.Segmenter().segment(string)].map(x => x.segment).reverse().join('');
 };
