4bcad759501e62e3accd06fecccf3128e9845db5
Eric Bailey <eric@ericb.me>
2024-01-30 17:35:53 -0600

    feat(common-lisp): submit raindrops

--- common-lisp/raindrops/raindrops.lisp
+++ common-lisp/raindrops/raindrops.lisp
@@ -4,6 +4,22 @@
 
 (in-package :raindrops)
 
-(defun convert (n)
-  "Converts a number to a string of raindrop sounds."
-  )
+(defconstant number->sound
+  '((3 . "Pling")
+    (5 . "Plang")
+    (7 . "Plong")))
+
+(defun divides-p (d n)
+  (zerop (rem n d)))
+
+(defun convert (number)
+  "Convert a number to a string of raindrop sounds."
+  (let ((sounds (reduce (lambda (sounds divisor-sound)
+                          (if (divides-p (car divisor-sound) number)
+                              (concatenate 'string sounds (cdr divisor-sound))
+                              sounds))
+                        number->sound
+                        :initial-value "")))
+    (if (= 0 (length sounds))
+        (format nil "~d" number)
+        sounds)))
