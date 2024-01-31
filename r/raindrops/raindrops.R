6e4b3df86c44ae5d20fb179c9b53857b8d3be90f
Eric Bailey <eric@ericb.me>
2024-01-31 11:44:56 -0600

    feat(r): submit raindrops

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,21 @@
 raindrops <- function(number) {
-  
+    sounds <- vector()
+
+    if (number%%3 == 0) {
+        sounds <- append(sounds, "Pling")
+    }
+
+    if (number%%5 == 0) {
+        sounds <- append(sounds, "Plang")
+    }
+
+    if (number%%7 == 0) {
+        sounds <- append(sounds, "Plong")
+    }
+
+    if (length(sounds) == 0) {
+        sprintf("%d", number)
+    } else {
+        paste(sounds, collapse = "")
+    }
 }
