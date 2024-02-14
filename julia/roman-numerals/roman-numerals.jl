7b76bccde50e9c3b3d4268677f016cc84ee5c426
Eric Bailey <eric@ericb.me>
2024-02-13 22:40:47 -0600

    feat(julia): resubmit roman-numerals

--- julia/roman-numerals/roman-numerals.jl
+++ julia/roman-numerals/roman-numerals.jl
@@ -1,24 +1,24 @@
-using OrderedCollections, Printf
+import Printf: @sprintf
 
-const conversions = OrderedDict(
-    1000 => "M",
-    900 => "CM",
-    500 => "D",
-    400 => "CD",
-    100 => "C",
-    90 => "XC",
-    50 => "L",
-    40 => "XL",
-    10 => "X",
-    9 => "IX",
-    5 => "V",
-    4 => "IV",
-    1 => "I"
+const conversions = (
+    (1000, "M"),
+    (900,  "CM"),
+    (500, "D"),
+    (400,  "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I")
 )
 
 function to_roman(number)
-    if number < 1 || number > 3999
-        error(@sprintf("%d is not within 1 and 3999, inclusive", number))
+    if number ∉ 1:3999
+        error(@sprintf("%d ∉ 1:3999", number))
     end
 
     converted, toconvert = "", number
