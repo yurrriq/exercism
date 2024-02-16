e9213b3dc65a8f4f237c99dc939bf05ba1b1582a
Eric Bailey <eric@ericb.me>
2024-02-15 23:28:37 -0600

    feat(fsharp): submit protein-translation

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,3 +1,26 @@
 module ProteinTranslation
 
-let proteins rna = failwith "You need to implement this function."
+let proteins (rna: string) : string list =
+    let translateCodon (rna: string) : (string * string) option =
+        match rna[0..2] with
+        | "AUG" -> Some("Methionine", rna[3..])
+        | "UGG" -> Some("Tryptophan", rna[3..])
+        | "UUU"
+        | "UUC" -> Some("Phenylalanine", rna[3..])
+        | "UUA"
+        | "UUG" -> Some("Leucine", rna[3..])
+        | "UCU"
+        | "UCC"
+        | "UCA"
+        | "UCG" -> Some("Serine", rna[3..])
+        | "UAU"
+        | "UAC" -> Some("Tyrosine", rna[3..])
+        | "UGU"
+        | "UGC" -> Some("Cysteine", rna[3..])
+        | "UAA"
+        | "UAG"
+        | "UGA"
+        | "" -> None
+        | _ -> failwith "Unknown coding"
+
+    List.unfold translateCodon rna
--- fsharp/protein-translation/ProteinTranslationTests.fs
+++ fsharp/protein-translation/ProteinTranslationTests.fs
@@ -6,106 +6,102 @@ open Xunit
 open ProteinTranslation
 
 [<Fact>]
-let ``Empty RNA sequence results in no proteins`` () =
-    proteins "" |> should be Empty
+let ``Empty RNA sequence results in no proteins`` () = proteins "" |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Methionine RNA sequence`` () =
-    proteins "AUG" |> should equal ["Methionine"]
+    proteins "AUG" |> should equal [ "Methionine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Phenylalanine RNA sequence 1`` () =
-    proteins "UUU" |> should equal ["Phenylalanine"]
+    proteins "UUU" |> should equal [ "Phenylalanine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Phenylalanine RNA sequence 2`` () =
-    proteins "UUC" |> should equal ["Phenylalanine"]
+    proteins "UUC" |> should equal [ "Phenylalanine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Leucine RNA sequence 1`` () =
-    proteins "UUA" |> should equal ["Leucine"]
+    proteins "UUA" |> should equal [ "Leucine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Leucine RNA sequence 2`` () =
-    proteins "UUG" |> should equal ["Leucine"]
+    proteins "UUG" |> should equal [ "Leucine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Serine RNA sequence 1`` () =
-    proteins "UCU" |> should equal ["Serine"]
+    proteins "UCU" |> should equal [ "Serine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Serine RNA sequence 2`` () =
-    proteins "UCC" |> should equal ["Serine"]
+    proteins "UCC" |> should equal [ "Serine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Serine RNA sequence 3`` () =
-    proteins "UCA" |> should equal ["Serine"]
+    proteins "UCA" |> should equal [ "Serine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Serine RNA sequence 4`` () =
-    proteins "UCG" |> should equal ["Serine"]
+    proteins "UCG" |> should equal [ "Serine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Tyrosine RNA sequence 1`` () =
-    proteins "UAU" |> should equal ["Tyrosine"]
+    proteins "UAU" |> should equal [ "Tyrosine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Tyrosine RNA sequence 2`` () =
-    proteins "UAC" |> should equal ["Tyrosine"]
+    proteins "UAC" |> should equal [ "Tyrosine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Cysteine RNA sequence 1`` () =
-    proteins "UGU" |> should equal ["Cysteine"]
+    proteins "UGU" |> should equal [ "Cysteine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Cysteine RNA sequence 2`` () =
-    proteins "UGC" |> should equal ["Cysteine"]
+    proteins "UGC" |> should equal [ "Cysteine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Tryptophan RNA sequence`` () =
-    proteins "UGG" |> should equal ["Tryptophan"]
+    proteins "UGG" |> should equal [ "Tryptophan" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``STOP codon RNA sequence 1`` () =
-    proteins "UAA" |> should be Empty
+[<Fact>]
+let ``STOP codon RNA sequence 1`` () = proteins "UAA" |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``STOP codon RNA sequence 2`` () =
-    proteins "UAG" |> should be Empty
+[<Fact>]
+let ``STOP codon RNA sequence 2`` () = proteins "UAG" |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``STOP codon RNA sequence 3`` () =
-    proteins "UGA" |> should be Empty
+[<Fact>]
+let ``STOP codon RNA sequence 3`` () = proteins "UGA" |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Sequence of two protein codons translates into proteins`` () =
-    proteins "UUUUUU" |> should equal ["Phenylalanine"; "Phenylalanine"]
+    proteins "UUUUUU" |> should equal [ "Phenylalanine"; "Phenylalanine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Sequence of two different protein codons translates into proteins`` () =
-    proteins "UUAUUG" |> should equal ["Leucine"; "Leucine"]
+    proteins "UUAUUG" |> should equal [ "Leucine"; "Leucine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Translate RNA strand into correct protein list`` () =
-    proteins "AUGUUUUGG" |> should equal ["Methionine"; "Phenylalanine"; "Tryptophan"]
+    proteins "AUGUUUUGG"
+    |> should equal [ "Methionine"; "Phenylalanine"; "Tryptophan" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Translation stops if STOP codon at beginning of sequence`` () =
-    proteins "UAGUGG" |> should be Empty
+[<Fact>]
+let ``Translation stops if STOP codon at beginning of sequence`` () = proteins "UAGUGG" |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Translation stops if STOP codon at end of two-codon sequence`` () =
-    proteins "UGGUAG" |> should equal ["Tryptophan"]
+    proteins "UGGUAG" |> should equal [ "Tryptophan" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Translation stops if STOP codon at end of three-codon sequence`` () =
-    proteins "AUGUUUUAA" |> should equal ["Methionine"; "Phenylalanine"]
+    proteins "AUGUUUUAA" |> should equal [ "Methionine"; "Phenylalanine" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Translation stops if STOP codon in middle of three-codon sequence`` () =
-    proteins "UGGUAGUGG" |> should equal ["Tryptophan"]
+    proteins "UGGUAGUGG" |> should equal [ "Tryptophan" ]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Translation stops if STOP codon in middle of six-codon sequence`` () =
-    proteins "UGGUGUUAUUAAUGGUUU" |> should equal ["Tryptophan"; "Cysteine"; "Tyrosine"]
-
+    proteins "UGGUGUUAUUAAUGGUUU"
+    |> should equal [ "Tryptophan"; "Cysteine"; "Tyrosine" ]
