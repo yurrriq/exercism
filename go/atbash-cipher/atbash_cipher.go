684101abca5b4a63c481451e44abadb7b7618a92
Eric Bailey <eric@ericb.me>
2024-04-18 23:11:04 -0500

    feat(go): submit atbash-cipher

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,40 @@
+// Package atbash implements the Atbash exercise from Exercism.
 package atbash
 
-func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+import (
+	"strings"
+	"unicode"
+)
+
+// Atbash encodes a given message using the Atbash cipher.
+func Atbash(message string) string {
+	var builder strings.Builder
+	cursor := 0
+
+	for _, roon := range message {
+		if !(unicode.IsLetter(roon) || unicode.IsNumber(roon)) {
+			continue
+		}
+
+		builder.WriteRune(ciper(roon))
+
+		cursor++
+		if cursor%5 == 0 {
+			builder.WriteRune(' ')
+		}
+	}
+
+	return strings.TrimSpace(builder.String())
+}
+
+func ciper(roon rune) rune {
+	if !unicode.IsLetter(roon) {
+		return roon
+	}
+
+	if unicode.IsUpper(roon) {
+		return unicode.ToLower('Z' - (roon - 'A'))
+	}
+
+	return 'z' - (roon - 'a')
 }
