3d162907ae76025352c1212bc28811eb91ad9fee
Eric Bailey <eric@ericb.me>
2024-04-18 23:18:52 -0500

    refactor(go): resubmit atbash-cipher

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -9,32 +9,25 @@ import (
 // Atbash encodes a given message using the Atbash cipher.
 func Atbash(message string) string {
 	var builder strings.Builder
-	cursor := 0
+	clusterLength := 0
 
 	for _, roon := range message {
-		if !(unicode.IsLetter(roon) || unicode.IsNumber(roon)) {
+		if !(unicode.IsLetter(roon) || unicode.IsDigit(roon)) {
 			continue
 		}
 
-		builder.WriteRune(ciper(roon))
-
-		cursor++
-		if cursor%5 == 0 {
+		if clusterLength == 5 {
 			builder.WriteRune(' ')
+			clusterLength = 0
 		}
+
+		if unicode.IsLetter(roon) {
+			roon = 'a' + 'z' - unicode.ToLower(roon)
+		}
+
+		builder.WriteRune(roon)
+		clusterLength++
 	}
 
 	return strings.TrimSpace(builder.String())
-}
-
-func ciper(roon rune) rune {
-	if !unicode.IsLetter(roon) {
-		return roon
-	}
-
-	if unicode.IsUpper(roon) {
-		return unicode.ToLower('Z' - (roon - 'A'))
-	}
-
-	return 'z' - (roon - 'a')
 }
