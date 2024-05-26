4dc6f4d9ea5eded5e6401a97667360a5b0108323
Eric Bailey <eric@ericb.me>
2024-05-26 00:29:12 -0500

    refactor(rust): resubmit collatz-conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,8 +1,11 @@
 pub fn collatz(n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+
     let mut n = n;
     for steps in 0.. {
         match n {
-            0 => return None,
             1 => return Some(steps),
             _ if 0 == n & 1 => n = n.checked_div(2)?,
             _ => n = n.checked_mul(3)?.checked_add(1)?,
