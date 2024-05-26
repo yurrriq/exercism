fb3ff0af09e884ff2c3d6cbedaa21522a808205c
Eric Bailey <eric@ericb.me>
2024-05-25 23:18:59 -0500

    feat(go): submit phone-number

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -1,13 +1,75 @@
+// Package phonenumber implements the Phone Number exercise from Exercism.
 package phonenumber
 
-func Number(phoneNumber string) (string, error) {
-	panic("Please implement the Number function")
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+// Number attempts to sanitizes a phone number.
+func Number(phoneNumber string) (sanitizedNumber string, err error) {
+	re := regexp.MustCompile(`\D`)
+	digits := re.ReplaceAllString(phoneNumber, "")
+
+	switch len(digits) {
+	case 10:
+		var result strings.Builder
+		for _, roon := range phoneNumber {
+			if unicode.IsDigit(roon) {
+				result.WriteRune(roon)
+			}
+		}
+		sanitizedNumber = result.String()
+	case 11:
+		if digits[0] != '1' {
+			return "", errors.New("Unknown country code")
+		}
+
+		sanitizedNumber, err = Number(digits[1:])
+		if err != nil {
+			return "", err
+		}
+	default:
+		return "", errors.New("Invalid length")
+	}
+
+	switch sanitizedNumber[0] {
+	case '0', '1':
+		return "", errors.New("Invalid area code")
+	}
+
+	switch sanitizedNumber[3] {
+	case '0', '1':
+		return "", errors.New("Invalid exchange code")
+	}
+
+	return sanitizedNumber, nil
 }
 
+// AreaCode attempts to extract the area code of a phone number.
 func AreaCode(phoneNumber string) (string, error) {
-	panic("Please implement the AreaCode function")
+	sanitizedNumber, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+
+	return sanitizedNumber[0:3], nil
 }
 
+// Format attempts to normlize a phone number.
 func Format(phoneNumber string) (string, error) {
-	panic("Please implement the Format function")
+	sanitizedNumber, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+
+	sanitizedNumber = fmt.Sprintf("(%s) %s-%s",
+		sanitizedNumber[0:3],
+		sanitizedNumber[3:6],
+		sanitizedNumber[6:10],
+	)
+	return sanitizedNumber, err
 }
