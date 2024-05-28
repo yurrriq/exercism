d7d38cdc08abd0fcf078039973b61e54b649666d
Eric Bailey <eric@ericb.me>
2024-05-27 21:42:36 -0500

    feat(haskell): submit darts

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,4 +1,13 @@
-module Darts (score) where
+module Darts
+  ( score,
+  )
+where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y
+  | distance > 10 = 0
+  | distance > 5 = 1
+  | distance > 1 = 5
+  | otherwise = 10
+  where
+    distance = sqrt (x * x + y * y)
