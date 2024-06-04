d71c84a7bdbac105ecda88f3db455537a0102a61
Eric Bailey <eric@ericb.me>
2024-06-03 23:35:29 -0500

    refactor(go): resubmit card-tricks
    Appease revive.

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -1,43 +1,46 @@
+// Package cards implements the [Card Tricks] exercise.
+//
+// [Card Tricks]: https://exercism.org/tracks/go/exercises/card-tricks
 package cards
 
-// Return a slice with the cards 2, 6 and 9 in that order.
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
 	return []int{2, 6, 9}
 }
 
-// Retrieve an item from a slice at given position.
-// If the index is out of range, return -1.
+// GetItem retrieves an item from a slice at given position.  If the index is
+// out of range, returns -1.
 func GetItem(slice []int, index int) int {
 	if index < 0 || index >= len(slice) {
 		return -1
-	} else {
-		return slice[index]
 	}
+
+	return slice[index]
 }
 
-// Write an item to a slice at given position overwriting an existing value.
-// If the index is out of range the value needs to be appended.
+// SetItem writes an item to a slice at given position, overwriting any existing
+// value.  If the index is out of range, appends the value.
 func SetItem(slice []int, index, value int) []int {
 	if index < 0 || index >= len(slice) {
 		return append(slice, value)
-	} else {
-		slice[index] = value
-		return slice
 	}
+
+	slice[index] = value
+	return slice
 }
 
-// Add an arbitrary number of values at the front of a slice.
+// PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, value ...int) []int {
 	return append(value, slice...)
 }
 
-// Remove an item from a slice by modifying the existing slice.
+// RemoveItem removes an item from a slice by modifying it in place.
 func RemoveItem(slice []int, index int) []int {
 	if index < 0 || index >= len(slice) {
 		return slice
 	} else if index == 0 {
 		return slice[1:]
-	} else {
-		return append(slice[:index], slice[index+1:]...)
 	}
+
+	return append(slice[:index], slice[index+1:]...)
 }
