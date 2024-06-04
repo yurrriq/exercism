8b5d783aa87ba96696166a0b2c7d76be525ba34d
Eric Bailey <eric@ericb.me>
2024-06-03 23:57:25 -0500

    refactor(go): resubmit elons-toys
    Appease revive.

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,7 +1,14 @@
+// Package elon implements the [Elon's Toys] exercise.
+//
+// [Elon's Toys]: https://exercism.org/tracks/go/exercises/elons-toys
 package elon
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// Drive updates the number of meters driven based on the car's speed, and
+// reduces the battery according to the battery drainage.
 func (car *Car) Drive() {
 	if car.battery >= car.batteryDrain {
 		car.battery -= car.batteryDrain
@@ -9,15 +16,18 @@ func (car *Car) Drive() {
 	}
 }
 
+// DisplayDistance returns the distance as displayed on the LED display.
 func (car *Car) DisplayDistance() string {
 	return fmt.Sprintf("Driven %d meters", car.distance)
 }
 
+// DisplayBattery returns the battery percentage as displayed on the LED
+// display.
 func (car *Car) DisplayBattery() string {
 	return fmt.Sprintf("Battery at %d%%", car.battery)
 }
 
-// Check if a car is able to finish a certain track.
+// CanFinish checks if a car is able to finish a certain track.
 func (car *Car) CanFinish(trackDistance int) bool {
 	return (car.battery/car.batteryDrain)*car.speed >= trackDistance
 }
