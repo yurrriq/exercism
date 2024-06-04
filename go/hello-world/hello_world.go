bb056cc05748acb5b7bc391dfc581322a0917aa1
Eric Bailey <eric@ericb.me>
2024-06-04 00:17:40 -0500

    refactor(go): resubmit hello-world
    Appease revive.

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -1,3 +1,6 @@
+// Package greeting implements the [Hello World] exercise.
+//
+// [Hello World]: https://exercism.org/tracks/go/exercises/hello-world
 package greeting
 
 // HelloWorld greets the world.
