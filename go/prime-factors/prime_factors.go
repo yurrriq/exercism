cb1c576ac44419e46402f2b83d656bace03e3f93
Eric Bailey <eric@ericb.me>
2024-06-03 22:29:50 -0500

    feat(go): submit prime-factors

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,5 +1,21 @@
+// Package prime implements the [Prime Factors] exercise from Exercism.
+//
+// [Prime Factors]: https://exercism.org/tracks/go/exercises/prime-factors
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+// Factors computes the primte factors of a given natural number.
+func Factors(n int64) (factors []int64) {
+	var p int64 = 2
+	for {
+		if n%p == 0 {
+			factors = append(factors, p)
+			n /= p
+		} else if n == 1 {
+			break
+		} else {
+			p++
+		}
+	}
+
+	return factors
 }
