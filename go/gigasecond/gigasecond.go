0cfe7e42f69f7ef56c76daa1049eb1d4e2231dd7
Eric Bailey <eric@ericb.me>
2024-06-04 00:05:36 -0500

    refactor(go): resubmit gigasecond
    Appease revive.

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,14 +1,17 @@
-// Calculating gigasecond anniversaries.
+// Package gigasecond implements the [Gigasecond] exercise.
+//
+// [Gigasecond]: https://exercism.org/tracks/go/exercises/gigasecond
 package gigasecond
 
 import (
 	"time"
 )
 
-// A gigasecond in nanoseconds.
+// Gigasecond is a billion seconds in nanoseconds.
 const Gigasecond time.Duration = 1e9 * 1e9
 
-// Given a time.Time, return the time.Time a gigasecond later.
+// AddGigasecond returns the time.Time a gigasecond later than a given
+// time.Time.
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(Gigasecond)
 }
