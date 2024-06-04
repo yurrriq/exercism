06c3157476de7d5806f3cdf1b073fcf8dc4ba48f
Eric Bailey <eric@ericb.me>
2024-06-03 23:38:51 -0500

    refactor(go): resubmit cars-assemble
    Appease revive.

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,19 +1,24 @@
+// Package cars implements the [Cars Assemble] exercise.
+//
+// [Cars Assemble]: https://exercism.org/tracks/go/exercises/cars-assemble
 package cars
 
 import "math/bits"
 
-// Calculate how many working cars are produced by the assembly line every hour.
+// CalculateWorkingCarsPerHour calculates how many working cars are produced by
+// the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
 	return float64(productionRate) * (successRate / 100)
 }
 
-// Calculate how many working cars are produced by the assembly line every minute.
+// CalculateWorkingCarsPerMinute calculates how many working cars are produced
+// by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
 	workingCarsPerHour := CalculateWorkingCarsPerHour(productionRate, successRate)
 	return int(workingCarsPerHour / 60)
 }
 
-// Calculate the cost of producing the given number of cars.
+// CalculateCost calculates the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
 	quotient, remainder := bits.Div(0, uint(carsCount), 10)
 	return (quotient * 95000) + (remainder * 10000)
