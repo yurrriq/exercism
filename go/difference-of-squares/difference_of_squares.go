9e7fe04b380b0eb7a15d67603f403863a07b980d
Eric Bailey <eric@ericb.me>
2024-06-03 23:50:06 -0500

    refactor(go): resubmit difference-of-squares
    Appease revive.

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,7 +1,11 @@
 //go:build !closedform
 
+// Package diffsquares implements the [Difference of Squares] exercise.
+//
+// [Difference of Squares]: https://exercism.org/tracks/go/exercises/difference-of-squares
 package diffsquares
 
+// SquareOfSum returns the square of the sum of the first n natural numbers.
 func SquareOfSum(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -11,6 +15,7 @@ func SquareOfSum(n int) int {
 	return sum * sum
 }
 
+// SumOfSquares returns the sum of the squares of the first n natural numbers.
 func SumOfSquares(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -20,6 +25,8 @@ func SumOfSquares(n int) int {
 	return sum
 }
 
+// Difference returns the difference between the square of the sum of the first
+// n numbers and the sum of the squares of the first n natural numbers.
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
