174c05b84d78e8a28e35dfb6467b54fd2e905c83
Eric Bailey <eric@ericb.me>
2024-06-04 00:16:18 -0500

    refactor(go): resubmit hamming
    Appease revive.

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,13 +1,20 @@
+// Package hamming implements the [Hamming] exercise.
+//
+// [Hamming]: https://exercism.org/tracks/go/exercises/hamming
 package hamming
 
+// ErrInvalidStrands is returned when trying to compute the Hamming distance
+// between two strands of unequal length.
 const ErrInvalidStrands HammingError = "Can't compute Hamming distance for strands of unequal length"
 
-type HammingError string
+// A DistanceError is a string.
+type DistanceError string
 
-func (err HammingError) Error() string {
+func (err DistanceError) Error() string {
 	return string(err)
 }
 
+// Distance calculates the Hamming distance between two DNA strands.
 func Distance(strandA, strandB string) (distance int, err error) {
 	if len(strandA) != len(strandB) {
 		return -1, ErrInvalidStrands
