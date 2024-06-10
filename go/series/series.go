1710d6519be8b91eda53f773f9280a410d1961a9
Eric Bailey <eric@ericb.me>
2024-06-10 00:44:19 -0500

    feat(go): submit series

--- go/series/series.go
+++ go/series/series.go
@@ -1,9 +1,18 @@
+// Package series implements [the Series exercise] on Exercism.
+//
+// [the Series exercise]: https://exercism.org/tracks/go/exercises/series
 package series
 
+// All returns all substrings of of s with length n.
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	if len(s) < n {
+		return []string{}
+	}
+
+	return append([]string{UnsafeFirst(n, s)}, All(n, s[1:])...)
 }
 
+// UnsafeFirst returns the first substring of s with length n.
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	return s[:n]
 }
