c59d716391c68dde8c52301d752e9c41792e8cd3
Eric Bailey <eric@ericb.me>
2024-06-26 00:44:43 -0500

    feat(haskell): submit food-chain

--- haskell/food-chain/src/FoodChain.hs
+++ haskell/food-chain/src/FoodChain.hs
@@ -1,54 +1,63 @@
-module FoodChain (song) where
+module FoodChain
+  ( song,
+  )
+where
+
+import Data.List (intercalate)
 
 song :: String
 song =
-  "I know an old lady who swallowed a fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a spider.\n\
-  \It wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a bird.\n\
-  \How absurd to swallow a bird!\n\
-  \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a cat.\n\
-  \Imagine that, to swallow a cat!\n\
-  \She swallowed the cat to catch the bird.\n\
-  \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a dog.\n\
-  \What a hog, to swallow a dog!\n\
-  \She swallowed the dog to catch the cat.\n\
-  \She swallowed the cat to catch the bird.\n\
-  \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a goat.\n\
-  \Just opened her throat and swallowed a goat!\n\
-  \She swallowed the goat to catch the dog.\n\
-  \She swallowed the dog to catch the cat.\n\
-  \She swallowed the cat to catch the bird.\n\
-  \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a cow.\n\
-  \I don't know how she swallowed a cow!\n\
-  \She swallowed the cow to catch the goat.\n\
-  \She swallowed the goat to catch the dog.\n\
-  \She swallowed the dog to catch the cat.\n\
-  \She swallowed the cat to catch the bird.\n\
-  \She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-  \She swallowed the spider to catch the fly.\n\
-  \I don't know why she swallowed the fly. Perhaps she'll die.\n\
-  \\n\
-  \I know an old lady who swallowed a horse.\n\
-  \She's dead, of course!\n"
+  intercalate
+    "\n"
+    [ stanza ["fly"],
+      stanza ["spider", "fly"],
+      stanza ["bird", "spider", "fly"],
+      stanza ["cat", "bird", "spider", "fly"],
+      stanza ["dog", "cat", "bird", "spider", "fly"],
+      stanza ["goat", "dog", "cat", "bird", "spider", "fly"],
+      stanza ["cow", "goat", "dog", "cat", "bird", "spider", "fly"],
+      stanza ["horse"]
+    ]
+
+stanza :: [String] -> String
+stanza [] = []
+stanza (food : foods) = unlines $ firstLine food <> go (food : foods)
+  where
+    go ["fly"] = ["I don't know why she swallowed the fly. Perhaps she'll die."]
+    go (this : that : rest)
+      | that == "spider" = "She swallowed the " <> this <> " to catch the spider that wriggled and jiggled and tickled inside her." : go (that : rest)
+      | otherwise = "She swallowed the " <> this <> " to catch the " <> that <> "." : go (that : rest)
+    go _ = []
+
+firstLine :: String -> [String]
+firstLine "fly" =
+  ["I know an old lady who swallowed a fly."]
+firstLine "spider" =
+  [ "I know an old lady who swallowed a spider.",
+    "It wriggled and jiggled and tickled inside her."
+  ]
+firstLine "bird" =
+  [ "I know an old lady who swallowed a bird.",
+    "How absurd to swallow a bird!"
+  ]
+firstLine "cat" =
+  [ "I know an old lady who swallowed a cat.",
+    "Imagine that, to swallow a cat!"
+  ]
+firstLine "dog" =
+  [ "I know an old lady who swallowed a dog.",
+    "What a hog, to swallow a dog!"
+  ]
+firstLine "horse" =
+  [ "I know an old lady who swallowed a horse.",
+    "She's dead, of course!"
+  ]
+firstLine "cow" =
+  [ "I know an old lady who swallowed a cow.",
+    "I don't know how she swallowed a cow!"
+  ]
+firstLine "goat" =
+  [ "I know an old lady who swallowed a goat.",
+    "Just opened her throat and swallowed a goat!"
+  ]
+firstLine _ = undefined
