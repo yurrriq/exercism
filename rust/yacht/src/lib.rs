9f7b952150e75750c3abe6172bc9d9054d5694da
Eric Bailey <eric@ericb.me>
2024-07-01 22:46:43 -0500

    chore(rust): download yacht

---
+++ rust/yacht/src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+pub enum Category {
+    Ones,
+    Twos,
+    Threes,
+    Fours,
+    Fives,
+    Sixes,
+    FullHouse,
+    FourOfAKind,
+    LittleStraight,
+    BigStraight,
+    Choice,
+    Yacht,
+}
+
+type Dice = [u8; 5];
+
+pub fn score(dice: Dice, category: Category) -> u8 {
+    todo!("determine the score for {dice:?} in the {category:?}");
+}
