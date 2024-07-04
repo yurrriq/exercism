3855013fe12c53be15c3bf3a3c6282f4c80f8348
Eric Bailey <eric@ericb.me>
2024-07-04 00:40:02 -0500

    chore(rust): download nth-prime

---
+++ rust/nth-prime/tests/nth-prime.rs
@@ -0,0 +1,32 @@
+use nth_prime::*;
+
+#[test]
+fn first_prime() {
+    let output = nth(0);
+    let expected = 2;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn second_prime() {
+    let output = nth(1);
+    let expected = 3;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn sixth_prime() {
+    let output = nth(5);
+    let expected = 13;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn big_prime() {
+    let output = nth(10000);
+    let expected = 104743;
+    assert_eq!(output, expected);
+}
