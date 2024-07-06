ca5040f7f52508a9aeafd63a8c360160e30dedfe
Eric Bailey <eric@ericb.me>
2024-07-06 01:43:30 -0500

    feat(ocaml): submit binary-search

--- ocaml/binary-search/binary_search.ml
+++ ocaml/binary-search/binary_search.ml
@@ -1,2 +1,13 @@
-let find _ _ =
-    failwith "'find' is missing"
+let rec find haystack needle =
+  let len = Array.length haystack in
+  if len == 0 then Error "value not in array"
+  else
+    let pivot = Array.length haystack / 2 in
+    let middle = haystack.(pivot) in
+    if needle < middle then find (Array.sub haystack 0 pivot) needle
+    else if needle > middle then
+      let pos = pivot + 1 in
+      Array.sub haystack pos (len - pos)
+      |> (Fun.flip find) needle
+      |> Result.map (( + ) (pivot + 1))
+    else Ok pivot
--- ocaml/binary-search/binary_search.mli
+++ ocaml/binary-search/binary_search.mli
@@ -1,4 +1,4 @@
 open Base
 
-(* Finds an int in an array of ints via binary search *)
 val find : int array -> int -> (int, string) Result.t
+(** Find an [int] in an [int array] via binary search. *)
