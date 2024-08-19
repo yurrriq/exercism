c6b841b2bad9746038096a5a4361d6656117ce9f
Eric Bailey <eric@ericb.me>
2024-08-19 00:35:48 -0500

    refactor(julia): resubmit difference-of-squares
    Use Horner's method.
    
    https://docs.julialang.org/en/v1/base/math/#Base.Math.evalpoly

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,8 +1,45 @@
-"Square the sum of the first `n` positive integers"
-square_of_sum(n::Integer)::Integer = ((n * (n + 1)) ÷ 2)^2
+@doc raw"""
+    square_of_sum(n::Integer)::Integer
 
-"Sum the squares of the first `n` positive integers"
-sum_of_squares(n::Integer)::Integer = (n * (n + 1) * (2 * n + 1)) ÷ 6
+Square the sum of the first `n` positive integers.
 
-"Subtract the sum of squares from square of the sum of the first `n` positive ints"
-difference(n::Integer)::Integer = square_of_sum(n) - sum_of_squares(n)
+```math
+\begin{align*}
+  \left(\sum_{x=1}^n x\right)^2
+  &= \left(\frac{n(n + 1)}{2}\right)^2 \\
+  &= \frac{n^2 + 2n^3 + n^4}{4}
+\end{align*}
+```
+"""
+square_of_sum(n::Integer)::Integer = evalpoly(n, (0, 0, 1, 2, 1)) ÷ 4
+
+@doc raw"""
+    sum_of_squares(n::Integer)::Integer
+
+Sum the squares of the first `n` positive integers.
+
+```math
+\begin{align*}
+  \sum_{x=1}^n x^2
+  &= \frac{n(n + 1)(2n + 1)}{6} \\
+  &= \frac{n + 3n^2 + 2n^3}{6}
+\end{align*}
+```
+"""
+sum_of_squares(n::Integer)::Integer = evalpoly(n, (0, 1, 3, 2)) ÷ 6
+
+@doc raw"""
+    difference(n::Integer)::Integer
+
+Subtract the sum of squares from square of the sum of the first `n` positive integers.
+
+```math
+\begin{align*}
+  \left(\sum_{x=1}^n x\right)^2 - \sum_{x=1}^n x^2
+  &= \left(\frac{n(n + 1)}{2}\right)^2 - \frac{n(n + 1)(2n + 1)}{6} \\
+  &= \frac{n^2 + 2n^3 + n^4}{4} - \frac{n + 3n^2 + 2n^3}{6} \\
+  &= \frac{-2n - 3n^2 + 2n^3 + 3n^4}{12}
+\end{align*}
+```
+"""
+difference(n::Integer)::Integer = evalpoly(n, (0, -2, -3, 2, 3)) ÷ 12
