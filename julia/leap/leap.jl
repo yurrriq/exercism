bd0f808bd8220972b8e042ba7ad6b7d4022e2a25
Eric Bailey <eric@ericb.me>
2024-08-18 23:03:53 -0500

    refactor(julia): resubmit leap
    Prefer \nequiv.

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -4,5 +4,5 @@
 Return `true` if `year` is a leap year in the gregorian calendar.
 """
 function is_leap_year(year)
-    year % 400 ≡ 0 || (year % 4 ≡ 0 && year % 100 ≠ 0)
+    year % 400 ≡ 0 || (year % 4 ≡ 0 && year % 100 ≢ 0)
 end
