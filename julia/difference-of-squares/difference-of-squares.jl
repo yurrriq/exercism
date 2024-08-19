0c0d2ab6374a0faec85b25bd863a5b0872ca6736
Eric Bailey <eric@ericb.me>
2024-08-18 23:58:47 -0500

    feat(julia): submit difference-of-squares

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,14 +1,8 @@
 "Square the sum of the first `n` positive integers"
-function square_of_sum(n)
-
-end
+square_of_sum(n::Integer)::Integer = ((n * (n + 1)) ÷ 2)^2
 
 "Sum the squares of the first `n` positive integers"
-function sum_of_squares(n)
-
-end
+sum_of_squares(n::Integer)::Integer = (n * (n + 1) * (2 * n + 1)) ÷ 6
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
-function difference(n)
-
-end
+difference(n::Integer)::Integer = square_of_sum(n) - sum_of_squares(n)
