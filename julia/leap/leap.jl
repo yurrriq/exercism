baa778405b7121448e50a64ed22d1415d5e383f9
Eric Bailey <eric@ericb.me>
2024-08-18 23:06:52 -0500

    refactor(julia): resubmit leap
    Add type annotations.

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -3,6 +3,6 @@
 
 Return `true` if `year` is a leap year in the gregorian calendar.
 """
-function is_leap_year(year)
+function is_leap_year(year::Integer) :: Bool
     year % 400 ≡ 0 || year % 4 ≡ 0 && year % 100 ≢ 0
 end
