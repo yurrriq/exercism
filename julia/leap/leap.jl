10e5df2dc4c0b96fbebccabb812d2de14eef61ec
Eric Bailey <eric@ericb.me>
2024-08-18 23:01:37 -0500

    feat(julia): submit leap

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -2,9 +2,7 @@
     is_leap_year(year)
 
 Return `true` if `year` is a leap year in the gregorian calendar.
-
 """
 function is_leap_year(year)
-
+    year % 400 ≡ 0 || (year % 4 ≡ 0 && year % 100 ≠ 0)
 end
-
