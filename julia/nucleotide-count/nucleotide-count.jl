0eba13737f797e44611a23708e6b20cd73f2b1d3
Eric Bailey <eric@ericb.me>
2024-08-18 23:25:55 -0500

    feat(julia): submit nucleotide-count

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -4,8 +4,14 @@
 The count of each nucleotide within `strand` as a dictionary.
 
 Invalid strands raise a `DomainError`.
-
 """
-function count_nucleotides(strand)
-
+function count_nucleotides(strand::String)::Dict{Char,Integer}
+    counts = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    for nucleotide in strand
+        if nucleotide ∉ "ACGT"
+            throw(DomainError(nucleotide, "invalid nucleotide"))
+        end
+        counts[nucleotide] += 1
+    end
+    counts
 end
