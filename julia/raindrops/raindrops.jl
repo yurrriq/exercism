e9eee958dee53ec5c5535e8e94a6f6cd15e986d6
Eric Bailey <eric@ericb.me>
2024-08-23 01:05:31 -0500

    feat(julia): submit raindrops

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,3 +1,11 @@
-function raindrops(number)
+const words = ["Pling", "Plang", "Plong"]
 
+function raindrops(number)
+    result = ""
+    for (i, word) in enumerate(words)
+        if number % (2 * i + 1) ≡ 0
+            result *= word
+        end
+    end
+    isempty(result) ? string(number) : result
 end
