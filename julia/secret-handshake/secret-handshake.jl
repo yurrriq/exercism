f90a43b5dab92a8c74e7175939bc000e5c18265c
Eric Bailey <eric@ericb.me>
2024-08-23 02:41:51 -0500

    feat(julia): submit secret-handshake

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,3 +1,11 @@
-function secret_handshake(code)
+using .Iterators: reverse
 
+const actions = ["wink", "double blink", "close your eyes", "jump"]
+
+function secret_handshake(code)
+    handshake = [action for (i, action) in enumerate(actions) if bitset(i - 1, code)]
+    bitset(4, code) && reverse!(handshake)
+    handshake
 end
+
+bitset(k, n) = n & (1 << k) > 0
