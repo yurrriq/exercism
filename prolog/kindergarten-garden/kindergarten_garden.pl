b2273a4562ef460a31a9c217b2a7f7ced57d3d08
Eric Bailey <eric@ericb.me>
2025-06-29 04:41:49 -0500

    docs(prolog): add docstrings to kindergarten-garden

--- prolog/kindergarten-garden/kindergarten_garden.pl
+++ prolog/kindergarten-garden/kindergarten_garden.pl
@@ -1,39 +1,47 @@
-/*
+:- module(kindergarten_garden, [garden/3]).  /*
 
 \PrologDialect{swiprolog}
 
-\Predicate plant/2(?Plant, ?Encoding).
+\Predicate plant/2(+Plant:atom, -Encoding:char).
 
 Four different types of seeds are planted:
 
 \begin{table}[h!]
-  \begin{tabular}{ l l l }
-    \hline
-    Plant & Prolog encoding & Diagram encoding \\
-    \hline
-    Grass & \verb|grass| & G \\
-    Clover & \verb|clover| & C \\
-    Radish & \verb|radishes| & R \\
-    Violets & \verb|violet| & V
-  \end{tabular}
+    \begin{tabular}{ l l l }
+      \hline
+      Plant & Prolog encoding & Diagram encoding \\
+      \hline
+      Grass & \verb|grass| & G \\
+      Clover & \verb|clover| & C \\
+      Radish & \verb|radishes| & R \\
+      Violets & \verb|violet| & V
+    \end{tabular}
 \end{table}
 
 \PL*/
+%! plant(+Plant:atom, -Encoding:char) is det.
+%
+% Encode a plant name as a single character.
 plant(grass, 'G').
 plant(clover, 'C').
 plant(radishes, 'R').
 plant(violets, 'V').
 /*PL
 
-\Predicate find_child_plants/3(?Child, ?FirstRow, ?SecondRow, ?Children, ?Plants).
+\Predicate find_child_plants/3(+Child:atom, +FirstRow:list(char),
+    +SecondRow:list(char), +Children:list(atom), -Plants:list(atom)).
 
-Find \verb|Child|'s \verb|Plants|, given the two rows of plants and list of
+Find \verb|Child|'s \verb|Plants|, given two rows of plants and a list of
 \verb|Children|. \\
 
 If \verb|Child| is the first of \verb|Children|, their \verb|Plants| are the
 first two in each row.
 
 \PL*/
+%! find_child_plants(+Child:atom, +FirstRow:list(char), +SecondRow:list(char),
+%!     +Children:list(atom), -Plants:list(atom)) is det.
+%
+% Find =Child='s =Plants=, given two rows of plants and a list of =Children=.
 find_child_plants(Child, [P1, P2  |_], [P3, P4  |_],
                   [Child  |_], [P1, P2, P3, P4]) :-
     !.
@@ -47,12 +55,15 @@ find_child_plants(Child, [_, _  |FirstRow], [_, _  |SecondRow],
     find_child_plants(Child, FirstRow, SecondRow, Children, Plants).
 /*PL
 
-\Predicate garden/3(?Garden, ?Child, ?Plants).
+\Predicate garden/3(+Garden:string, +Child:atom, -Plants:list(atom)).
 
 Determine which \verb|Plants| in the \verb|Garden| belong to the given
 \verb|Child|.
 
 \PL*/
+%! garden(+Garden:string, +Child:atom, -Plants:list(atom)) is det.
+%
+% Determine which =Plants= in the =Garden= belong to the given =Child=.
 garden(Garden, Child, Plants) :-
 /*PL
 
