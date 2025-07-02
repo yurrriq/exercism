8399b80c0bc5e3c2d3923a4d8ad68cb8af61fdc9
Eric Bailey <eric@ericb.me>
2025-07-02 02:45:57 -0500

    refactor(prolog): resubmit rna-transcription

--- prolog/rna-transcription/rna_transcription.pl
+++ prolog/rna-transcription/rna_transcription.pl
@@ -1,9 +1,42 @@
-nucleotide_transcription('G', 'C').
-nucleotide_transcription('C', 'G').
-nucleotide_transcription('T', 'A').
-nucleotide_transcription('A', 'U').
+:- module(rna_transcription, [rna_transcription/2]).  /*
+
+\PrologDialect{swiprolog}
+
+\Predicate rna_complement/2(+Dna:char, -Rna:char).
+
+True if \verb|Rna| is the RNA complement of the DNA nucleotide \verb|Dna|.
+
+\PL*/
+:- semidet(rna_complement/2).
+
+%! rna_complement(+Dna:char, -Rna:char) is semidet.
+%
+% True if =Rna= is the RNA complement of the DNA nucleotide =Dna=.
+rna_complement('G', 'C').
+rna_complement('C', 'G').
+rna_complement('T', 'A').
+rna_complement('A', 'U').
+/*PL
+
+\PL*/
+:- semidet(rna_transcription/2).
+/*PL
+
+\Predicate rna_transcription/2(+Dna:string, -Rna:string).
+
+True if \verb|Rna| is the RNA complement of the DNA sequence \verb|Dna|.
+
+\PL*/
+%! rna_transcription(+Dna:string, -Rna:string) is semidet.
+%
+% True if =Rna= is the RNA complement of the DNA sequence =Dna=.
+%
+% @see rna_complement/2
 
 rna_transcription(Dna, Rna) :-
     string_chars(Dna, Strand),
-    maplist(nucleotide_transcription, Strand, Transcribed),
+    maplist(rna_complement, Strand, Transcribed),
     string_chars(Rna, Transcribed).
+/*PL
+
+\EndProlog*/
