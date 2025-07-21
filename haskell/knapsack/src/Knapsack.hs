c1bfd0eadb683d9ca8055bdfbbaf068cb70a5e65
Eric Bailey <eric@ericb.me>
2025-07-20 23:34:03 -0500

    feat(haskell): submit knapsack

--- haskell/knapsack/src/Knapsack.hs
+++ haskell/knapsack/src/Knapsack.hs
@@ -1,4 +1,24 @@
-module Knapsack (maximumValue) where
+module Knapsack
+  ( maximumValue,
+  )
+where
+
+import Control.Arrow ((***))
+import Data.Semigroup (Max (..))
 
 maximumValue :: Int -> [(Int, Int)] -> Int
-maximumValue n items = error "You need to implement this function."
+maximumValue maxWeight items =
+  getMax $
+    foldMap (Max . fst) $
+      knapsack maxWeight items
+
+knapsack :: (Ord a, Num a, Num b) => a -> [(a, b)] -> [(b, [(a, b)])]
+knapsack _ [] = [(0, [])]
+knapsack maxWeight ((weight, value) : items)
+  | weight > maxWeight = excluded
+  | otherwise = included ++ excluded
+  where
+    excluded = knapsack maxWeight items
+    included =
+      ((value +) *** ((weight, value) :))
+        <$> knapsack (maxWeight - weight) items
