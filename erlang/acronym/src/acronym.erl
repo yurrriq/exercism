9d49a270a67747ca4faec881edb0e27caf112702
Eric Bailey <eric@ericb.me>
2025-07-21 19:56:15 -0500

    feat(erlang): submit acronym

--- erlang/acronym/src/acronym.erl
+++ erlang/acronym/src/acronym.erl
@@ -2,4 +2,11 @@
 
 -export([abbreviate/1]).
 
-abbreviate(_Phrase) -> undefined.
+abbreviate(Phrase) ->
+    lists:flatmap(fun do_abbreviate/1, string:lexemes(Phrase, " _-")).
+
+do_abbreviate(Word) ->
+    case string:next_grapheme(Word) of
+        [Letter | _] -> string:uppercase([Letter]);
+        _ -> []
+    end.
