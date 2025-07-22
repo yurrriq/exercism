5e5da1f5230830a89616e0318f08360b813ad90b
Eric Bailey <eric@ericb.me>
2025-07-21 21:59:20 -0500

    chore(julia): download collatz-conjecture

---
+++ julia/collatz-conjecture/runtests.jl
@@ -0,0 +1,15 @@
+using Test
+
+include("collatz-conjecture.jl")
+
+@testset verbose = true "tests" begin
+    # canonical data
+    @testset "Canonical data" begin
+        @test collatz_steps(1) == 0
+        @test collatz_steps(16) == 4
+        @test collatz_steps(12) == 9
+        @test collatz_steps(1000000) == 152
+        @test_throws DomainError collatz_steps(0)
+        @test_throws DomainError collatz_steps(-15)
+    end
+end
