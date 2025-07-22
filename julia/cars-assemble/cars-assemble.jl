1163ed8dfcbb3ea723c9e2a77edf85c2e542647d
Eric Bailey <eric@ericb.me>
2025-07-21 21:05:52 -0500

    feat(julia): submit cars-assemble

--- julia/cars-assemble/cars-assemble.jl
+++ julia/cars-assemble/cars-assemble.jl
@@ -1,11 +1,12 @@
 function success_rate(speed)
-    
+   iszero(speed) && return zero(speed)
+   1 ≤ speed ≤ 4 && return 1
+   5 ≤ speed ≤ 8 && return 0.9
+   speed == 9 ? 0.8 : 0.77
 end
 
-function production_rate_per_hour(speed)
-    
-end
+production_rate_per_hour(speed) =
+    221speed * success_rate(speed)
 
-function working_items_per_minute(speed)
-    
-end
+working_items_per_minute(speed) =
+    Int(production_rate_per_hour(speed) ÷ 60)
