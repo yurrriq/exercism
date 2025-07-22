cd1895428c0609a30627f1e2c00871a94144e86b
Eric Bailey <eric@ericb.me>
2025-07-21 20:07:31 -0500

    chore(julia): download lasagna

---
+++ julia/lasagna/lasagna.jl
@@ -0,0 +1,8 @@
+# Define the `expected_bake_time` constant`
+
+# Define the `preparation_time(layers)` function.
+
+# Define the `remaining_time(time_in_oven)` function.
+
+# Define the `total_working_time(layers, time_in_oven)` function.
+
