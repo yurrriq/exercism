563083fe17feb4bb7dc7a01c3d23a9f2f1de8649
Eric Bailey <eric@ericb.me>
2025-07-21 20:33:55 -0500

    feat(julia): submit annalyns-infiltration

--- julia/annalyns-infiltration/annalyns-infiltration.jl
+++ julia/annalyns-infiltration/annalyns-infiltration.jl
@@ -1,11 +1,11 @@
-function can_do_fast_attack(knight_awake)
-end
+can_do_fast_attack(knight_awake) =
+    !knight_awake
 
-function can_spy(knight_awake, archer_awake, prisoner_awake)
-end
+can_spy(knight_awake, archer_awake, prisoner_awake) =
+    knight_awake || archer_awake || prisoner_awake
 
-function can_signal_prisoner(archer_awake, prisoner_awake)
-end
+can_signal_prisoner(archer_awake, prisoner_awake) =
+    prisoner_awake && !archer_awake
 
-function can_free_prisoner(knight_awake, archer_awake, prisoner_awake, dog_present)
-end
+can_free_prisoner(knight_awake, archer_awake, prisoner_awake, dog_present) =
+    dog_present ? !archer_awake : (prisoner_awake && !(knight_awake || archer_awake))
