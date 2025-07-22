eb2e0db40c8cd622d3145bb88f3198fa308f9450
Eric Bailey <eric@ericb.me>
2025-07-21 21:39:22 -0500

    feat(julia): submit perfect-numbers

--- julia/perfect-numbers/perfect-numbers.jl
+++ julia/perfect-numbers/perfect-numbers.jl
@@ -1,1 +1,24 @@
+@enum Classification begin
+    deficient
+    perfect
+    abundant
+end
 
+aliquotsum(n) =
+    sum(d for d ∈ 1:(n-1) if iszero(n % d))
+
+function classify(n)
+    n ≤ 0 && throw(DomainError(n, "must be positive"))
+    asum = aliquotsum(n)
+    asum < n && return deficient::Classification
+    asum ≡ n ? perfect::Classification : abundant::Classification
+end
+
+isdeficient(n) =
+    classify(n) ≡ deficient::Classification
+
+isperfect(n) =
+    classify(n) ≡ perfect::Classification
+
+isabundant(n) =
+    classify(n) ≡ abundant::Classification
