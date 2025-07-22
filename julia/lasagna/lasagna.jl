3a8fccd6c3ce8d5f89827f2d32d9342abb1e4675
Eric Bailey <eric@ericb.me>
2025-07-21 20:23:03 -0500

    feat(julia): submit lasagna

--- julia/lasagna/lasagna.jl
+++ julia/lasagna/lasagna.jl
@@ -1,8 +1,8 @@
-# Define the `expected_bake_time` constant`
+const expected_bake_time = 60
 
-# Define the `preparation_time(layers)` function.
+preparation_time = Base.Fix1(*, 2)
 
-# Define the `remaining_time(time_in_oven)` function.
+remaining_time = Base.Fix1(-, expected_bake_time) 
 
-# Define the `total_working_time(layers, time_in_oven)` function.
-
+total_working_time(layers, time_in_oven) =
+    preparation_time(layers) + time_in_oven
