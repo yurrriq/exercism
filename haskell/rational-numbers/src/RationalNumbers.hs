3c341040855734edf07d979eca751eb41f8f41fd
Eric Bailey <eric@ericb.me>
2025-07-27 22:50:46 -0500

    feat(haskell): submit rational-numbers

--- haskell/rational-numbers/src/RationalNumbers.hs
+++ haskell/rational-numbers/src/RationalNumbers.hs
@@ -17,39 +17,58 @@ where
 import Prelude hiding (Rational, abs, div)
 
 -- Data definition -------------------------------------------------------------
-data Rational a = Dummy deriving (Eq, Show)
+
+data Rational a = !a :% !a deriving (Eq, Show)
 
 rational :: (Integral a) => (a, a) -> Rational a
-rational = error "You need to implement this function"
+rational (n, d) =
+  if d' < 0
+    then
+      negate n' :% negate d'
+    else
+      n' :% d'
+  where
+    n' = n `quot` k
+    d' = d `quot` k
+    k = gcd n d
 
 -- unary operators -------------------------------------------------------------
+
 abs :: (Integral a) => Rational a -> Rational a
-abs = error "You need to implement this function"
+abs (n :% d) =
+  case (n >= 0, d >= 0) of
+    (True, True) -> n :% d
+    (True, False) -> n :% negate d
+    (False, True) -> negate n :% d
+    (False, False) -> negate n :% negate d
 
 numerator :: (Integral a) => Rational a -> a
-numerator = error "You need to implement this function"
+numerator (n :% _) = n
 
 denominator :: (Integral a) => Rational a -> a
-denominator = error "You need to implement this function"
+denominator (_ :% d) = d
 
 -- binary operators ------------------------------------------------------------
+
 add :: (Integral a) => Rational a -> Rational a -> Rational a
-add = error "You need to implement this function"
+add (n1 :% d1) (n2 :% d2) = rational (n1 * d2 + n2 * d1, d1 * d2)
 
 sub :: (Integral a) => Rational a -> Rational a -> Rational a
-sub = error "You need to implement this function"
+sub (n1 :% d1) (n2 :% d2) = rational (n1 * d2 - n2 * d1, d1 * d2)
 
 mul :: (Integral a) => Rational a -> Rational a -> Rational a
-mul = error "You need to implement this function"
+mul (n1 :% d1) (n2 :% d2) = rational (n1 * n2, d1 * d2)
 
 div :: (Integral a) => Rational a -> Rational a -> Rational a
-div = error "You need to implement this function"
+div (n1 :% d1) (n2 :% d2) = rational (n1 * d2, n2 * d1)
 
 pow :: (Integral a) => Rational a -> a -> Rational a
-pow = error "You need to implement this function"
+pow (n :% d) e
+  | e >= 0 = rational (n ^ e, d ^ e)
+  | otherwise = rational (d ^ negate e, n ^ negate e)
 
-expRational :: (Integral a) => (Floating b) => Rational a -> b -> b
-expRational = error "You need to implement this function"
+expRational :: (Integral a, Floating b) => Rational a -> b -> b
+expRational (n :% d) e = (fromIntegral n ** e) / (fromIntegral d ** e)
 
-expReal :: (Floating a) => (Integral b) => a -> Rational b -> a
-expReal = error "You need to implement this function"
+expReal :: (Floating a, Integral b) => a -> Rational b -> a
+expReal x (n :% d) = x ** (fromIntegral n / fromIntegral d)
