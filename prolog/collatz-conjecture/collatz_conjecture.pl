5b833e105002194993eb5175d5e76252bd2dc0f9
Eric Bailey <eric@ericb.me>
2025-07-29 00:57:22 -0500

    feat(prolog): submit collatz-conjecture

--- prolog/collatz-conjecture/collatz_conjecture.pl
+++ prolog/collatz-conjecture/collatz_conjecture.pl
@@ -1,1 +1,15 @@
-collatz_steps(N, Steps).
+:- module(collatz_conjecture, [collatz_steps/2]).
+
+%!  collatz_steps(+N:int, -Steps:int) is det.
+%
+%   Compute the number of =Steps= required to reach 1.
+collatz_steps(1, 0) :- !.
+collatz_steps(N, Steps) :-
+    must_be(integer, N),
+    N > 1,
+    (  0 is N mod 2
+    -> Next is N // 2
+    ;  Next is 3 * N + 1
+    ),
+    collatz_steps(Next, NextSteps),
+    Steps is 1 + NextSteps.
