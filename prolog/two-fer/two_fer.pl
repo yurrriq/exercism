1844d83b1bb57e3fb3f4ac445375feda67e7946c
Eric Bailey <eric@ericb.me>
2025-07-29 00:25:06 -0500

    feat(prolog): submit two-fer

--- prolog/two-fer/two_fer.pl
+++ prolog/two-fer/two_fer.pl
@@ -1,3 +1,7 @@
-two_fer(Name, Dialogue).
+:- module(two_fer, [two_fer/1, two_fer/2]).
 
-two_fer(Dialogue).
+two_fer(Dialogue) :-
+    two_fer('you', Dialogue).
+
+two_fer(Name, Dialogue) :-
+    format(string(Dialogue), 'One for ~s, one for me.', [Name]).
