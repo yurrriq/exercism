f6d0f84eee0569bfd4593a88f6a5b467634a7766
Eric Bailey <eric@ericb.me>
2025-07-29 01:11:32 -0500

    refactor(prolog): resubmit collatz-conjecture
    Pull out even/1.

--- prolog/collatz-conjecture/collatz_conjecture.pl
+++ prolog/collatz-conjecture/collatz_conjecture.pl
@@ -7,9 +7,14 @@ collatz_steps(1, 0) :- !.
 collatz_steps(N, Steps) :-
     must_be(integer, N),
     N > 1,
-    (  0 is N mod 2
+    (  even(N)
     -> Next is N // 2
     ;  Next is 3 * N + 1
     ),
     collatz_steps(Next, NextSteps),
     Steps is 1 + NextSteps.
+
+%!  even(+N:int) is det.
+%   True if =N= is even.
+even(N) :-
+    0 is N mod 2.
