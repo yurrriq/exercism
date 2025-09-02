38ff64e457ede57bfc7b7b11b6490011717539aa
Eric Bailey <eric@ericb.me>
2025-09-01 19:53:14 -0500

    feat(haskell): submit spiral-matrix

--- haskell/spiral-matrix/src/Spiral.hs
+++ haskell/spiral-matrix/src/Spiral.hs
@@ -1,4 +1,20 @@
-module Spiral (spiral) where
+{-# LANGUAGE BlockArguments #-}
+
+module Spiral
+  ( spiral,
+  )
+where
+
+import Data.Functor ((<&>))
 
 spiral :: Int -> [[Int]]
-spiral size = error "You need to implement this function."
+spiral size =
+  let maxIndex = size - 1
+   in [0 .. maxIndex] <&> \x ->
+        [0 .. maxIndex] <&> \y ->
+          (\(layer, offset) -> 4 * layer * (size - layer) + offset + 1)
+            case (y >= x, y < maxIndex - x) of
+              (True, True) -> (x, y - x)
+              (True, False) -> (maxIndex - y, x + 3 * y - 2 * maxIndex)
+              (False, False) -> (maxIndex - x, 5 * x - y - 2 * maxIndex)
+              (False, True) -> (y, 4 * maxIndex - x - 7 * y)
