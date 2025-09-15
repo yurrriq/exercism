8c65ad49348683d6a2cf43bf8db42e75e694296c
Eric Bailey <eric@ericb.me>
2025-09-15 00:14:08 -0500

    feat(haskell): solve book-store

--- haskell/book-store/src/BookStore.hs
+++ haskell/book-store/src/BookStore.hs
@@ -1,6 +1,65 @@
-module BookStore (total, Book (..)) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
 
-data Book = First | Second | Third | Fourth | Fifth
+module BookStore
+  ( Book (..),
+    total,
+  )
+where
 
-total :: [Book] -> Int
-total basket = error "You need to implement this function."
+import Algorithm.Search (dijkstra)
+import Data.Finitary (Finitary (Cardinality), toFinite)
+import Data.Finite (getFinite)
+import Data.IntMultiSet (IntMultiSet, (\\))
+import Data.IntMultiSet qualified as IMS
+import GHC.Generics (Generic)
+
+data Book
+  = First
+  | Second
+  | Third
+  | Fourth
+  | Fifth
+  deriving stock (Eq, Generic, Show)
+  deriving anyclass (Finitary)
+
+total :: forall a. (Finitary a, Cardinality a ~ 5) => [a] -> Int
+total =
+  maybe 0 fst
+    . dijkstra purchases cost IMS.null
+    . fromBooks
+  where
+    purchases basket = map (basket \\) allSets
+    cost from to =
+      case IMS.distinctSize (from \\ to) of
+        1 -> 800
+        2 -> 1520
+        3 -> 2160
+        4 -> 2560
+        5 -> 3000
+        k ->
+          error $
+            unlines
+              [ "Invalid number of different books: " <> show k,
+                "Must be one of 1, 2, 3, 4, or 5."
+              ]
+
+allSets :: [IntMultiSet]
+allSets =
+  IMS.fromDistinctAscList [1 .. 5]
+    : concatMap
+      (map IMS.fromDistinctAscList . flip choose [1 .. 5])
+      [2, 3, 4]
+
+-- | All \(k\)-combinations of the natural numbers \(1\) through \(n\).
+choose :: Int -> [Int] -> [[Int]]
+choose 0 _ = [[]]
+choose _ [] = []
+choose k (x : xs) = map (return x `mappend`) (choose (k - 1) xs) ++ choose k xs
+
+fromBooks :: forall a. (Finitary a, Cardinality a ~ 5) => [a] -> IntMultiSet
+fromBooks = IMS.fromList . map toInt
+
+toInt :: forall a. (Finitary a, Cardinality a ~ 5) => a -> Int
+toInt = (+ 1) . fromIntegral . getFinite . toFinite
